cf58e78ce846e741cf376ad20a36cfdbb5b8cb19
Scott Aubrey <scott@aubrey.org.uk>
2022-08-23 16:38:57 +0100

    fix linting issues

--- src/components/atoms/authors/authors.tsx
+++ src/components/atoms/authors/authors.tsx
@@ -8,20 +8,20 @@ type Author = {
 
 const authorLimit = 10;
 
-export const Authors = ({ authors }: {authors: Author[]}): JSX.Element => {
+export const Authors = ({ authors }: { authors: Author[] }): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
 
   return (
     <div>
       <ol className="authors-list">
-        { authors.slice(0, expanded ? authors.length : authorLimit).map(({givenNames, familyNames}, index) => (
+        { authors.slice(0, expanded ? authors.length : authorLimit).map(({ givenNames, familyNames }, index) => (
           <li className="authors-list__item" key={index}>
             {givenNames.join(' ')} {familyNames.join(' ')}
           </li>
         ))}
       </ol>
-      { authors.length > authorLimit ?
-        <span className="authors-list__expansion" onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${authors.length - authorLimit} more`}</span> : ''}
+      { authors.length > authorLimit
+        ? <span className="authors-list__expansion" onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${authors.length - authorLimit} more`}</span> : ''}
     </div>
   );
-}
+};
