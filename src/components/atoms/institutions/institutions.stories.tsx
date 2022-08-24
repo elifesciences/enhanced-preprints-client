b2eb1ac9d8a2cc11f40b8d5746a7aa291d008202
Will Byrne <will.alex.byrne@gmail.com>
2022-08-24 14:11:48 +0100

    add story, component and styles

---
+++ src/components/atoms/institutions/institutions.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Institutions } from './institutions';
+
+export default {
+  title: 'Atoms/Institutions',
+  component: Institutions,
+} as ComponentMeta<typeof Institutions>;
+
+const Template: ComponentStory<typeof Institutions> = (args) => <Institutions {...args} />;
+
+export const InstitutionList = Template.bind({});
+InstitutionList.args = {
+  institutions: [
+    { name: 'Charles Xavier\'s School for Gifted Youngsters', address: { addressCountry: 'West Chester',}},
+    { name: 'Star Labs', address: { addressCountry: 'Star City'}},
+    { name: 'Avengers Tower', address: { addressCountry: 'New York'}},
+    { name: 'Bat Cave', address: { addressCountry: 'Gotham'}},
+    { name: 'Arrow Cave', address: { addressCountry: 'Central City'}},
+  ],
+};
---
+++ src/components/atoms/institutions/institutions.tsx
@@ -0,0 +1,29 @@
+import './institutions.scss';
+import { useState } from 'react';
+
+type Institution = {
+  name: string,
+  address?: {
+    addressCountry: string,
+  },
+};
+
+const institutionLimit = 3;
+
+export const Institutions = ({ institutions }: { institutions: Institution[] }): JSX.Element => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div>
+      <ol className="institutions-list">
+        { institutions.slice(0, expanded ? institutions.length : institutionLimit).map(({ name, address }, index) => (
+          <li className="institutions-list__item" key={index}>
+            {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address>: ''}
+          </li>
+        ))}
+      </ol>
+      { institutions.length > institutionLimit
+        ? <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${institutions.length - institutionLimit} more`}</span> : ''}
+    </div>
+  );
+};
