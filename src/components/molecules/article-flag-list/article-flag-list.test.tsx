aca28ae05e7e74d94639774d9979dbc5185fc109
Will Byrne <will.alex.byrne@gmail.com>
2022-08-25 11:35:09 +0100

    fix broken test

--- src/components/molecules/article-flag-list/article-flag-list.test.tsx
+++ src/components/molecules/article-flag-list/article-flag-list.test.tsx
@@ -19,9 +19,9 @@ describe('ArticleFlagList', () => {
   it('should correctly pass in the msa flag', () => {
     render(<ArticleFlagList msas={msas} importance="Landmark" strengthOfEvidence="Tour-de-force"/>);
 
-    expect(screen.getByText(msas[0])).toHaveClass('article-flags__link-msa');
-    expect(screen.getByText(msas[1])).toHaveClass('article-flags__link-msa');
-    expect(screen.getByText('Landmark')).not.toHaveClass('article-flags__link-msa');
-    expect(screen.getByText('Tour-de-force')).not.toHaveClass('article-flags__link-msa');
+    expect(screen.getByText(msas[0])).toHaveClass('article-flag__link-msa');
+    expect(screen.getByText(msas[1])).toHaveClass('article-flag__link-msa');
+    expect(screen.getByText('Landmark')).not.toHaveClass('article-flag__link-msa');
+    expect(screen.getByText('Tour-de-force')).not.toHaveClass('article-flag__link-msa');
   });
 });
