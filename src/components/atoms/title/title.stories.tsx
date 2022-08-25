7ae71ca2bee0fa737e05ccd9beb1cfa24611104c
Scott Aubrey <scott@aubrey.org.uk>
2022-08-25 16:49:44 +0100

    use Encoda's content formatted JSON as a basis for formatted text format for the title

--- src/components/atoms/title/title.stories.tsx
+++ src/components/atoms/title/title.stories.tsx
@@ -11,5 +11,35 @@ const Template: ComponentStory<typeof Title> = (args) => <Title {...args} />;
 
 export const ArticleTitle = Template.bind({});
 ArticleTitle.args = {
-  title: 'I am an article title!',
+  title: 'I am an article string title!',
+};
+
+export const ArticleTitle2 = Template.bind({});
+ArticleTitle2.args = {
+  title: ['I am an article ', 'title', ' array!'],
+};
+
+export const ArticleTitle3 = Template.bind({});
+ArticleTitle3.args = {
+  title: { type: 'Emphasis', content: 'I am an emphasised title' },
+};
+
+export const ArticleTitle4 = Template.bind({});
+ArticleTitle4.args = {
+  title: ['I am a ', { type: 'Emphasis', content: 'partially' }, ' emphasised title'],
+};
+
+export const ArticleTitle5 = Template.bind({});
+ArticleTitle5.args = {
+  title: ['I am a ', { type: 'Emphasis', content: [{ type: 'Superscript', content: 'partially emphasised superscript' }] }, 'title'],
+};
+
+export const ArticleTitle6 = Template.bind({});
+ArticleTitle6.args = {
+  title: ['I am a ', { type: 'Emphasis', content: ['partially emphasised ', { type: 'Superscript', content: 'superscript' }] }, ' title'],
+};
+
+export const ArticleTitle7 = Template.bind({});
+ArticleTitle7.args = {
+  title: ['I am a ', { type: 'Emphasis', content: ['partially emphasised ', { type: 'Superscript', content: [{ type: 'Superscript', content: 'superscript' }, { type: 'Subscript', content: 'text' }] }] }, ' title'],
 };
--- src/components/atoms/title/title.tsx
+++ src/components/atoms/title/title.tsx
@@ -1,5 +1,44 @@
 import './title.scss';
 
-export const Title = ({ title }: { title: string }): JSX.Element => (
-  <h1 className="title">{title}</h1>
+type DecoratedContent = {
+  content: string | DecoratedContent | Array<DecoratedContent | string>,
+  type: string,
+};
+
+type Content = string | DecoratedContent | Array<Content>;
+
+type JSXContentPart = string | JSX.Element | Array<JSXContentPart>;
+export type JSXContent = JSXContentPart | Array<JSXContentPart>;
+
+export const contentToJsx = (content: Content): JSXContent => {
+  if (typeof content === 'undefined') {
+    return '';
+  }
+  if (typeof content === 'string') {
+    return content;
+  }
+
+  // array of string or DecoratedContent, so just map back to this function
+  if (Array.isArray(content)) {
+    return content.map(contentToJsx);
+  }
+  switch (content.type) {
+    case 'Emphasis':
+      return <em>{contentToJsx(content.content)}</em>;
+    case 'Strong':
+      return <strong>{contentToJsx(content.content)}</strong>;
+    case 'Superscript':
+      return <sup>{contentToJsx(content.content)}</sup>;
+    case 'Subscript':
+      return <sub>{contentToJsx(content.content)}</sub>;
+    case 'Date':
+      return <time>{contentToJsx(content.content)}</time>;
+    default:
+      // TODO: log unimplemented type
+      return <span>{contentToJsx(content.content)}</span>;
+  }
+};
+
+export const Title = ({ title }: { title: Content }): JSX.Element => (
+  <h1 className="title">{contentToJsx(title)}</h1>
 );
