d565c87ade7c6718a6a7e01df18bfee98e0013b8
Will Byrne <will.alex.byrne@gmail.com>
2022-09-08 11:48:42 +0100

    change dates in story to differentiate the default and passed in events

--- src/components/molecules/timeline/timeline.stories.tsx
+++ src/components/molecules/timeline/timeline.stories.tsx
@@ -14,8 +14,8 @@ export const DefaultTimeline = Template.bind({});
 export const EventTimeline = Template.bind({});
 EventTimeline.args = {
   events: [
-    { name: 'event 3', date: new Date('2022-03-06') },
-    { name: 'event 2', date: new Date('2022-03-03') },
-    { name: 'event 1', date: new Date('2021-11-08') },
+    { name: 'event 3', date: new Date('1999-01-01') },
+    { name: 'event 2', date: new Date('2000-02-02') },
+    { name: 'event 1', date: new Date('2001-03-03') },
   ],
 };
