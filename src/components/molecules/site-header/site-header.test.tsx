55e7844e722b95e642a55443144c567be1ca07da
Scott Aubrey <scott@aubrey.org.uk>
2022-09-13 11:26:37 +0100

    add a test

---
+++ src/components/molecules/site-header/site-header.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { SiteHeader } from './site-header';
+
+describe('SiteHeader', () => {
+  it('renders the logo image with alt text', () => {
+    render(<SiteHeader />);
+
+    expect(screen.getByAltText('eLife logo', { exact: false }).getAttribute('src')?.endsWith('elife-logo.svg')).toStrictEqual(true);
+  });
+});
