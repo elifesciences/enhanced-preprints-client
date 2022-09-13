1889a97de215e272cfc77ec06eba000130c8979f
Scott Aubrey <scott@aubrey.org.uk>
2022-09-13 17:10:22 +0100

    add the Article to the main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+import { mockContent } from './components/atoms/article-content/mock-content';
+import { ArticlePage } from './components/pages/article/article';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArticlePage
+        msas={['Mad Science', 'Alchemy']}
+        importance={'Landmark'}
+        strengthOfEvidence={'Tour-de-force'}
+        title={'This is a title'}
+        doi={'bbc.co.uk'}
+        authors={[
+          { givenNames: ['Steve'], familyNames: ['Rogers'] },
+          { givenNames: ['Antony'], familyNames: ['Stark'] },
+          { givenNames: ['Natasha'], familyNames: ['Romanov'] },
+          { givenNames: ['Bruce'], familyNames: ['Banner'] },
+          { givenNames: ['Wanda'], familyNames: ['Maximof'] },
+          { givenNames: ['Bucky'], familyNames: ['Barnes'] },
+          { givenNames: ['Barry'], familyNames: ['Allen'] },
+          { givenNames: ['Jesse'], familyNames: ['Quick'] },
+          { givenNames: ['Kara'], familyNames: ['Zor-el'] },
+          { givenNames: ['Arthur'], familyNames: ['Curry'] },
+          { givenNames: ['Kal'], familyNames: ['El'] },
+          { givenNames: ['Oliver'], familyNames: ['Queen'] },
+        ]}
+        content={mockContent}
+        tweets={0} citations={0} views={0}
+      />
     </div>
   );
 }
