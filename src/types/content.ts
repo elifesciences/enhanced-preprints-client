860d9890b1c7b836a59d73771fc9750e45810336
Scott Aubrey <scott@aubrey.org.uk>
2022-09-14 08:56:19 +0100

    further refinements to types based on the mockContent

--- src/types/content.ts
+++ src/types/content.ts
@@ -17,6 +17,7 @@ type DateContent = DecoratedContent & {
 type LinkContent = DecoratedContent & {
   type: 'Link',
   target: string,
+  relation?: string,
 };
 
 type CiteContent = DecoratedContent & {
@@ -37,9 +38,10 @@ type FigureContent = DecoratedContent & {
   label: string,
 };
 
-type ImageObjectContent = DecoratedContent & {
+type ImageObjectContent = {
   type: 'ImageObject',
-  contentUrl: string,
+  contentUrl?: string,
+  content?: Content
   meta: {
     inline: boolean,
   },
