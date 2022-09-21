b605b1c6ec54cc1efa3a4875bf989d15817a0376
Scott Aubrey <scott@aubrey.org.uk>
2022-09-21 09:59:54 +0100

    fix dev setup so that it works again with nginx

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   rewrites: () => [
-    {
-      source: "/api/article/:path*",
-      destination: "http://api:3000/api/article/:path*",
-      basePath: false,
-    },
-    {
-      source: "/storybook/:path*",
-      destination: "http://storybook:6006/:path*",
-      basePath: false,
-    },
   ]
 }
 
