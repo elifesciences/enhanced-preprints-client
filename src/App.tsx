1254f98af2535a6714faf78987eefe8aa29978ea
Scott Aubrey <scott@aubrey.org.uk>
2022-09-21 10:01:22 +0100

    remove CRA guff

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { ArticlePage } from './components/pages/article/article';
-
-export const App = (): JSX.Element => (
-  <div className="App">
-    <ArticlePage doi="10.1101/2022.04.13.488149"/>
-  </div>
-);
-
-export default App;
