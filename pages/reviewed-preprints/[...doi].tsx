eca4623c89200bb0c44f43453fc1e6986be62f99
Scott Aubrey <scott@aubrey.org.uk>
2022-09-21 10:00:49 +0100

    fix server-rendered content

--- pages/reviewed-preprints/[...doi].tsx
+++ pages/reviewed-preprints/[...doi].tsx
@@ -8,13 +8,14 @@ import { ContextualData, ContextualDataProps } from '../../src/components/molecu
 import { SiteHeader } from '../../src/components/molecules/site-header/site-header';
 import { Tab, TabbedNavigation } from '../../src/components/molecules/tabbed-navigation';
 import { Timeline } from '../../src/components/molecules/timeline/timeline';
+import { Content } from '../../src/types/content';
 import styles from './article-page.module.scss';
 
 type MetaData = ContentHeaderProps & ContextualDataProps & {
   headings: JumpMenuHeading[]
 };
 
-export const ArticlePage = (metaData: MetaData): JSX.Element => (
+export const ArticlePage = ({ metaData, content }: { metaData: MetaData, content: Content }): JSX.Element => (
   <div className={`${styles['grid-container']} ${styles['article-page']}`}>
     <div className={styles['grid-header']}>
       <SiteHeader />
@@ -33,7 +34,7 @@ export const ArticlePage = (metaData: MetaData): JSX.Element => (
       <TabbedNavigation>
         <Tab label="Full text">
           <JumpToMenu active={1} headings={metaData.headings} />
-          <ArticleContent doi={metaData.doi} />
+          <ArticleContent doi={metaData.doi} content={content} />
         </Tab>
         <Tab label="Figures and data">
           <Heading id="figures" headingLevel={2} content="Figures and data" />
@@ -53,9 +54,13 @@ export const ArticlePage = (metaData: MetaData): JSX.Element => (
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
   const doi = Array.isArray(context.params?.doi) ? context.params?.doi?.join('/') ?? context.params?.doi : '';
-  const metaData = fetch(`http://localhost:3000/api/article/${doi}/metadata`).then((res) => res.json());
+  const metaData = await fetch(`http://api:3000/api/article/${doi}/metadata`).then((res) => res.json());
+  const content = await fetch(`http://api:3000/api/article/${doi}/content`).then((res) => res.json());
   return {
-    props: metaData,
+    props: {
+      metaData,
+      content,
+    }
   };
 };
 
--- src/components/atoms/article-content/article-content.tsx
+++ src/components/atoms/article-content/article-content.tsx
@@ -2,16 +2,9 @@ import { useEffect, useState } from 'react';
 import styles from './article-content.module.scss';
 import { contentToJsx } from '../../../utils/content-to-jsx';
 import { Content } from '../../../types/content';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
-export const ArticleContent = ({ doi }: { doi: string }): JSX.Element => {
-  const [content, setContent] = useState<Content>('Loading article...');
-  useEffect(() => {
-    fetch(`/api/article/${doi}/content`)
-      .then((res) => res.json())
-      .then((json) => setContent(json))
-      .catch(() => setContent('Unable to load article'));
-  }, [doi]);
-
+export const ArticleContent = ({ doi, content }: { doi: string, content: Content }): JSX.Element => {
   return (
     <article className={styles['article-body']}>{contentToJsx(content)}</article>
   );
