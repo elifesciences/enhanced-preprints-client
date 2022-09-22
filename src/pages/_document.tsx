e3ca572bde2b84661549c728196491703b2875a0
Will Byrne <will.alex.byrne@gmail.com>
2022-09-22 15:16:55 +0100

    add fonts to base page wraper

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;600&family=Noto+Serif" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  );
+};
+
