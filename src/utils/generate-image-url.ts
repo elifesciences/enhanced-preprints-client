363823dbc1a5182fcc06f57e7a3b866003e402b4
Scott Aubrey <scott@aubrey.org.uk>
2022-09-30 07:20:08 +0100

    encode the URI component when passing into the image server URL

--- src/utils/generate-image-url.ts
+++ src/utils/generate-image-url.ts
@@ -1,3 +1,3 @@
 import { config } from '../config';
 
-export const generateImageUrl = (contentUrl: string): string => (config.IMAGE_SERVER ? `/iiif/2/${contentUrl}/full/1024,/0/default.jpg` : contentUrl);
+export const generateImageUrl = (contentUrl: string): string => (config.IMAGE_SERVER ? `/iiif/2/${encodeURIComponent(contentUrl)}/full/1024,/0/default.jpg` : contentUrl);
