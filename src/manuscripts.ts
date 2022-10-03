cb59893d5d646b3b377a4d06de0f30833e36302e
Scott Aubrey <scott@aubrey.org.uk>
2022-10-03 08:42:15 +0100

    move msid -> preprint data into separate module

---
+++ src/manuscripts.ts
@@ -0,0 +1,51 @@
+import { ArticleStatusProps } from './components/pages/article/article-page';
+
+type PreprintConfig = {
+  doi: string,
+  status: ArticleStatusProps
+};
+
+const preprintConfigs: Record<string, PreprintConfig> = {
+  '10.1101/2022.03.04.482974': {
+    doi: '10.1101/2022.03.04.482974',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      timeline: [
+        { name: 'Peer review', date: '2022-04-07' },
+        { name: 'Preprint posted', date: '2022-03-05' },
+      ],
+    },
+  },
+  '10.1101/2022.06.24.497502': {
+    doi: '10.1101/2022.06.24.497502',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      timeline: [
+        { name: 'Peer review', date: '2022-09-06' },
+        { name: 'Preprint posted', date: '2022-06-26' },
+      ],
+    },
+  },
+  '10.1101/2022.07.26.501569': {
+    doi: '10.1101/2022.07.26.501569',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      timeline: [
+        { name: 'Peer review', date: '2022-09-05' },
+        { name: 'Preprint posted', date: '2022-07-28' },
+      ],
+    },
+  },
+};
+
+export const msids: Record<string, PreprintConfig> = {
+  '77558': preprintConfigs['10.1101/2022.03.04.482974'], // eslint-disable-line quote-props
+  '77558v1': preprintConfigs['10.1101/2022.03.04.482974'], // eslint-disable-line quote-props
+  '80494': preprintConfigs['10.1101/2022.06.24.497502'], // eslint-disable-line quote-props
+  '80494v1': preprintConfigs['10.1101/2022.06.24.497502'], // eslint-disable-line quote-props
+  '81926': preprintConfigs['10.1101/2022.07.26.501569'], // eslint-disable-line quote-props
+  '81926v1': preprintConfigs['10.1101/2022.07.26.501569'], // eslint-disable-line quote-props
+};
--- src/pages/reviewed-preprints/[msid].page.tsx
+++ src/pages/reviewed-preprints/[msid].page.tsx
@@ -1,64 +1,12 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
-import { TimelineEvent } from '../../components/molecules/timeline/timeline';
-import { ArticlePage, ArticlePageProps, ArticleStatus } from '../../components/pages/article/article-page';
+import { ArticlePage, ArticlePageProps, ArticleStatusProps } from '../../components/pages/article/article-page';
 import { config } from '../../config';
+import { msids } from '../../manuscripts';
 import { Content } from '../../types/content';
 
-export const Page = (props: { metaData: ArticlePageProps, content: Content, status: ArticleStatus }): JSX.Element => (
+export const Page = (props: { metaData: ArticlePageProps, content: Content, status: ArticleStatusProps }): JSX.Element => (
   <ArticlePage {...props}></ArticlePage>
 );
-
-type PreprintConfig = {
-  doi: string,
-  articleType: string,
-  status: string,
-  timeline: TimelineEvent[],
-};
-
-const preprintConfigs = {
-  '10.1101/2022.03.04.482974': {
-    doi: '10.1101/2022.03.04.482974',
-    status: {
-      type: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
-      timeline: [
-        { name: 'Peer review', date: '2022-04-07' },
-        { name: 'Preprint posted', date: '2022-03-05' },
-      ],
-    },
-  },
-  '10.1101/2022.06.24.497502': {
-    doi: '10.1101/2022.06.24.497502',
-    status: {
-      type: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
-      timeline: [
-        { name: 'Peer review', date: '2022-09-06' },
-        { name: 'Preprint posted', date: '2022-06-26' },
-      ],
-    },
-  },
-  '10.1101/2022.07.26.501569': {
-    doi: '10.1101/2022.07.26.501569',
-    status: {
-      type: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
-      timeline: [
-        { name: 'Peer review', date: '2022-09-05' },
-        { name: 'Preprint posted', date: '2022-07-28' },
-      ],
-    },
-  },
-};
-
-const msids: Record<string, PreprintConfig> = {
-  '77558': preprintConfigs['10.1101/2022.03.04.482974'], // eslint-disable-line quote-props
-  '77558v1': preprintConfigs['10.1101/2022.03.04.482974'], // eslint-disable-line quote-props
-  '80494': preprintConfigs['10.1101/2022.06.24.497502'], // eslint-disable-line quote-props
-  '80494v1': preprintConfigs['10.1101/2022.06.24.497502'], // eslint-disable-line quote-props
-  '81926': preprintConfigs['10.1101/2022.07.26.501569'], // eslint-disable-line quote-props
-  '81926v1': preprintConfigs['10.1101/2022.07.26.501569'], // eslint-disable-line quote-props
-};
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
   const msid = context.params?.msid;
