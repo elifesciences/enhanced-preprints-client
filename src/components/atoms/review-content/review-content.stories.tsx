9682a1cad053cc3fd8d91f13024f42d29de46f3e
Scott Aubrey <scott@aubrey.org.uk>
2022-10-03 14:18:05 +0100

    fix some missing styles and add a new review-content example with blockquote (since that's pretty crucial)

--- src/components/atoms/review-content/review-content.stories.tsx
+++ src/components/atoms/review-content/review-content.stories.tsx
@@ -16,3 +16,12 @@ Review.args = {
 
   With some body *emphasised*`,
 };
+
+export const ReviewBlockQuote = Template.bind({});
+ReviewBlockQuote.args = {
+  content: `# This is a review in markdown
+
+> With some body *emphasised*
+
+Some other comments here reference the quote above`,
+};
