373202b4287fa28606788dff27b790e9796812f5
Scott Aubrey <scott@aubrey.org.uk>
2022-10-06 12:38:40 +0100

    add more tests for assessment behaviour

--- src/components/atoms/review-content/review-content.test.tsx
+++ src/components/atoms/review-content/review-content.test.tsx
@@ -30,9 +30,16 @@ describe('ArticleContent', () => {
     expect(screen.getByText('convincing').tagName).toStrictEqual('STRONG');
   });
 
-  it('does not highlight unless term is exact', async () => {
+  it('does not highlight terms unless term is exact', async () => {
     render(<ReviewContent isAssessment={true} content="I am an important article that is very convincingly good."/>);
 
     expect(screen.queryByText('convincing')).not.toBeInTheDocument();
+  });
+
+  it('shows links to explain assessment terms', async () => {
+    render(<ReviewContent isAssessment={true} content="I have reviewed it, and it's good"/>);
+
+    expect(screen.getByText('Read the peer reviews')).toBeInTheDocument();
+    expect(screen.getByText('About eLife assessments')).toBeInTheDocument();
   });
 });
