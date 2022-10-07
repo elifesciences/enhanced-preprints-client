02c5b606f82ca732f173d8b7228650e8dd3a801a
Scott Aubrey <scott@aubrey.org.uk>
2022-10-07 11:01:50 +0100

    enable a new article with Maths in, and fix images to always serve the max size

--- src/utils/generate-image-url.ts
+++ src/utils/generate-image-url.ts
@@ -1,3 +1,3 @@
 import { config } from '../config';
 
-export const generateImageUrl = (contentUrl: string): string => (config.imageServer ? `${config.imageServer}/2/${encodeURIComponent(contentUrl)}/full/1024,/0/default.jpg` : contentUrl);
+export const generateImageUrl = (contentUrl: string): string => (config.imageServer ? `${config.imageServer}/2/${encodeURIComponent(contentUrl)}/full/max/0/default.jpg` : contentUrl);
