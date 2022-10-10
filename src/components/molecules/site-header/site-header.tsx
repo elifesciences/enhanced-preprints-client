72be17f66729d62db313c370f73b63ce5d096df2
Will Byrne <will.alex.byrne@gmail.com>
2022-10-10 18:19:40 +0100

    make menu content closer to journal and fix links

--- src/components/molecules/site-header/site-header.tsx
+++ src/components/molecules/site-header/site-header.tsx
@@ -23,28 +23,45 @@ export const SiteHeader = (): JSX.Element => {
               <div className={`${styles['wrapper']} ${styles['main-menu']} ${styles['main-menu--js']} ${styles['main-menu--shown']}`} id="mainMenu" data-behaviour="MainMenu" tabIndex={0} aria-expanded="true" data-behaviour-initialised="true">
                 <nav className={styles['main-menu__container']} role="navigation">
                   <button className={styles['main-menu__close_control']} id="mainMenuCloseControl" onClick={() => setShowMenu(false)}>Close</button>
-                  <h3 className={styles['list-heading']}>Menu</h3>
+                  <img
+                      src="/elife-logo.svg"
+                      alt="eLife logo"
+                      width="88"
+                      height="35"
+                  />
                   <ul className={styles['main-menu__list']}>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
+                      <a href="https://elifesciences.org/" className={styles['main-menu__list_link']}>Home</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
+                      <a href="https://elifesciences.org/magazine" className={styles['main-menu__list_link']}>Magazine</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
+                      <a href="https://elifesciences.org/community" className={styles['main-menu__list_link']}>Community</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
+                      <a href="https://elifesciences.org/about" className={styles['main-menu__list_link']}>About</a>
+                    </li>
                     <li className={styles['main-menu__list_item']}>
-                      <a href="/subjects" className={styles['main-menu__list_link']}>Research categories</a>
+                      <a href="https://elifesciences.org/subjects" className={styles['main-menu__list_link']}>Research categories</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']} ${styles['end-of-group']}`}>
+                      <a href="https://elifesciences.org/inside-elife" className={styles['main-menu__list_link']}>Inside eLife</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
+                      <a href="https://elifesciences.org/about" className={styles['main-menu__list_link']}>Search</a>
+                    </li>
+                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']} ${styles['end-of-group']}`}>
+                      <a href="https://elifesciences.org/content-alerts" className={styles['main-menu__list_link']}>Subscribe to alerts</a>
+                    </li>
+                    <li className={styles['main-menu__list_item']}>
+                      <a href="https://reviewer.elifesciences.org/login" className={styles['main-menu__list_link']}>Submit your research</a>
                     </li>
                     <li className={styles['main-menu__list_item']}>
                       <a href="https://reviewer.elifesciences.org/author-guide/editorial-process" className={styles['main-menu__list_link']}>Author guide</a>
                     </li>
                     <li className={styles['main-menu__list_item']}>
                       <a href="https://reviewer.elifesciences.org/reviewer-guide/review-process" className={styles['main-menu__list_link']}>Reviewer guide</a>
-                    </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="/about" className={styles['main-menu__list_link']}>About</a>
-                    </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="/inside-elife" className={styles['main-menu__list_link']}>Inside eLife</a>
-                    </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="/community" className={styles['main-menu__list_link']}>Community</a>
-                    </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="/labs" className={styles['main-menu__list_link']}>Innovation</a>
                     </li>
                   </ul>
                   <a href="#siteHeader" className={styles['to-top-link']}>Back to top</a>
