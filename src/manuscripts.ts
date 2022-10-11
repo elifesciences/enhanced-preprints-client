9448cd41d76e61d8c2e317078f2729b1513d2461
FAtherden-eLife <f.atherden@elifesciences.org>
2022-10-11 14:11:09 +0100

    Adds timeline events for more launch examples
    - Adds timeline information for next set of reviewed preprints
    - Revised status description inline with Chris' latest designs
    - Timeline event name changed from `Peer review` to `Sent for peer review`

--- src/manuscripts.ts
+++ src/manuscripts.ts
@@ -10,9 +10,9 @@ const preprintConfigs: Record<string, ReviewedPreprintConfig> = {
     preprintDoi: '10.1101/2022.03.04.482974',
     status: {
       articleType: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      status: 'This preprint was published after peer review by eLife.',
       timeline: [
-        { name: 'Peer review', date: '2022-04-07' },
+        { name: 'Sent for peer review', date: '2022-03-04' },
         { name: 'Preprint posted', date: '2022-03-05' },
       ],
     },
@@ -21,9 +21,9 @@ const preprintConfigs: Record<string, ReviewedPreprintConfig> = {
     preprintDoi: '10.1101/2022.06.24.497502',
     status: {
       articleType: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      status: 'This preprint was published after peer review by eLife.',
       timeline: [
-        { name: 'Peer review', date: '2022-09-06' },
+        { name: 'Sent for peer review', date: '2022-06-24' },
         { name: 'Preprint posted', date: '2022-06-26' },
       ],
     },
@@ -32,10 +32,76 @@ const preprintConfigs: Record<string, ReviewedPreprintConfig> = {
     preprintDoi: '10.1101/2022.07.26.501569',
     status: {
       articleType: 'Reviewed Preprint',
-      status: 'This preprint has been reviewed by eLife. Authors have responded but not yet submitted a revised edition',
+      status: 'This preprint was published after peer review by eLife.',
       timeline: [
-        { name: 'Peer review', date: '2022-09-05' },
+        { name: 'Sent for peer review', date: '2022-07-26' },
         { name: 'Preprint posted', date: '2022-07-28' },
+      ],
+    },
+  },
+  '10.1101/2022.05.28.493855': {
+    preprintDoi: '10.1101/2022.05.28.493855',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-12-07' },
+        { name: 'Preprint posted', date: '2022-05-29' },
+      ],
+    },
+  },
+  '10.1101/2022.06.30.498369': {
+    preprintDoi: '10.1101/2022.06.30.498369',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-07-01' },
+        { name: 'Preprint posted', date: '2022-07-02' },
+      ],
+    },
+  },
+  '10.1101/2022.05.30.22275761': {
+    preprintDoi: '10.1101/2022.05.30.22275761',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-06-29' },
+        { name: 'Preprint posted', date: '2022-05-31' },
+      ],
+    },
+  },
+  '10.1101/2022.07.21.500925': {
+    preprintDoi: '10.1101/2022.07.21.500925',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-07-21' },
+        { name: 'Preprint posted', date: '2022-07-22' },
+      ],
+    },
+  },
+  '10.1101/2020.07.27.223354': {
+    preprintDoi: '10.1101/2020.07.27.223354',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-07-20' },
+        { name: 'Preprint posted', date: '2022-06-21' },
+      ],
+    },
+  },
+  '10.1101/2021.11.12.468444': {
+    preprintDoi: '10.1101/2021.11.12.468444',
+    status: {
+      articleType: 'Reviewed Preprint',
+      status: 'This preprint was published after peer review by eLife.',
+      timeline: [
+        { name: 'Sent for peer review', date: '2022-07-12' },
+        { name: 'Preprint posted', date: '2022-06-29' },
       ],
     },
   },
