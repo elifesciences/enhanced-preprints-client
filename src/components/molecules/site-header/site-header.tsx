96adc25d430a99184d4ee882356e8b162471b73c
Scott Aubrey <scott@aubrey.org.uk>
2022-10-11 11:16:47 +0100

    make sure the elife-logo goes through image optimisation (and thus also production asset build process)

--- src/components/molecules/site-header/site-header.tsx
+++ src/components/molecules/site-header/site-header.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import styles from './site-header.module.scss';
+import logo from '../../../../public/elife-logo.svg';
 
 export const SiteHeader = (): JSX.Element => (
   <div className="site-header">
     <Image
       className={styles['site-header__logo']}
-      src="/reviewed-preprints/elife-logo.svg"
+        src={logo}
         alt="eLife logo"
         width="80"
         height="30"
