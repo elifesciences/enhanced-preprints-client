6cf88f5ef8b5a36ee1ea491fcc3f011af5d3dd2b
Scott Aubrey <scott@aubrey.org.uk>
2022-10-13 07:58:54 +0100

    Add a ridiculously simple index page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,16 @@
+import { manuscripts } from '../manuscripts';
+
+console.log(manuscripts);
+
 export const App = (): JSX.Element => (
   <div className="App">
-    Hello
+    <ul>
+      {Object.values(manuscripts)
+        .map((manuscript) => manuscript.msid)
+        .filter((v, i, a) => a.indexOf(v) === i)
+        .map((msid, index) => <li key={index}><a href={`/reviewed-preprints/${msid}`}>{msid}</a></li>)
+      }
+    </ul>
   </div>
 );
 
