8cdfe759b400f30f624dace80d3b1614c5d293c3
Will Byrne <will.alex.byrne@gmail.com>
2022-10-17 16:35:41 +0100

    fix smooth scrolling flickering issue

--- src/components/atoms/jump-to-menu/jump-to-menu.tsx
+++ src/components/atoms/jump-to-menu/jump-to-menu.tsx
@@ -11,24 +11,36 @@ export type Heading = {
 
 export const JumpToMenu = ({ headings }: { headings: Heading[] }): JSX.Element => {
   const [active, setActive] = useState(0);
+  const [jumping, setJumping] = useState(false);
+
+  const linkClickHandler = (index: number) => {
+    setJumping(true);
+    setActive(index);
+    setTimeout(() => {
+      setJumping(false);
+    }, 1000);
+  };
+
   useEffect(() => {
     const headingElements = headings
       .map(({ id }) => document.getElementById(id))
       .filter((element): element is HTMLElement => element !== null);
 
     const handleScroll = () => {
-      const { scrollY } = window;
+      if (!jumping) {
+        const { scrollY } = window;
 
-      for (let i = 0; i < headingElements.length; i += 1) {
-        const current = headingElements[i];
-        const next = headingElements[i + 1];
-        const sectionTop = current.offsetTop - 120;
+        for (let i = 0; i < headingElements.length; i += 1) {
+          const current = headingElements[i];
+          const next = headingElements[i + 1];
+          const sectionTop = current.offsetTop - 120;
 
-        if (
-          (scrollY > sectionTop || i === 0)
-          && (next === undefined || scrollY <= next.offsetTop - 50)
-        ) {
-          setActive(i);
+          if (
+            (scrollY > sectionTop || i === 0)
+            && (next === undefined || scrollY <= next.offsetTop - 50)
+          ) {
+            setActive(i);
+          }
         }
       }
     };
@@ -37,7 +49,7 @@ export const JumpToMenu = ({ headings }: { headings: Heading[] }): JSX.Element =
     document.addEventListener('scroll', throttledScrollHandler);
 
     return () => document.removeEventListener('scroll', throttledScrollHandler);
-  }, []);
+  }, [jumping]);
 
   return (
     <div className={styles['jump-menu']}>
@@ -46,7 +58,7 @@ export const JumpToMenu = ({ headings }: { headings: Heading[] }): JSX.Element =
           {
             headings.map((heading, index) => (
               <li className={`${styles['jump-menu-list__item']} ${active === index ? ` ${styles['jump-menu-list__item--active']}` : ''}`} key={index}>
-                <a onClick={() => setActive(index)} className={styles['jump-menu-list__link']} href={`#${heading.id}`}>{contentToJsx(heading.text)}</a>
+                <a onClick={() => linkClickHandler(index)} className={styles['jump-menu-list__link']} href={`#${heading.id}`}>{contentToJsx(heading.text)}</a>
               </li>
             ))
           }
