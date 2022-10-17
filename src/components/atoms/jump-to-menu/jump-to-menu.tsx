a79caf34b75dab4baea3c5133f427fb8ccee1aef
Will Byrne <will.alex.byrne@gmail.com>
2022-10-17 11:51:20 +0100

    move active state to jump to menu and add click handler for setting state

--- src/components/atoms/jump-to-menu/jump-to-menu.tsx
+++ src/components/atoms/jump-to-menu/jump-to-menu.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { Content } from '../../../types/content';
 import { contentToJsx } from '../../../utils/content-to-jsx';
 import styles from './jump-to-menu.module.scss';
@@ -7,13 +8,14 @@ export type Heading = {
   text: Content,
 };
 
-export const JumpToMenu = ({ headings, active }: { headings: Heading[], active: number }): JSX.Element => (
-  <div className={styles['jump-menu']}>
+export const JumpToMenu = ({ headings }: { headings: Heading[] }): JSX.Element => {
+  const [active, setActive] = useState(0);
+  return ( <div className={styles['jump-menu']}>
     <nav className={styles['jump-menu-navigation']}>
       <ul className={styles['jump-menu-list']}>
         {
           headings.map((heading, index) => (
-            <li className={`${styles['jump-menu-list__item']} ${active === index ? ` ${styles['jump-menu-list__item--active']}` : ''}`} key={index}>
+            <li onClick={() => setActive(index)} className={`${styles['jump-menu-list__item']} ${active === index ? ` ${styles['jump-menu-list__item--active']}` : ''}`} key={index}>
               <a className={styles['jump-menu-list__link']} href={`#${heading.id}`}>{contentToJsx(heading.text)}</a>
             </li>
           ))
@@ -21,4 +23,6 @@ export const JumpToMenu = ({ headings, active }: { headings: Heading[], active: 
       </ul>
     </nav>
   </div>
-);
+ );
+};
+
