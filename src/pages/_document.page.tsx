20730b579a29a8c4b45758cb1355e6e6b5abc7cd
Scott Aubrey <scott@aubrey.org.uk>
2022-10-18 11:14:40 +0100

    fix the html encoding of the script contents because of JSX

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -22,11 +22,11 @@ export default function Document() {
           `}
         </style>
         { config.gtmId &&
-          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${config.gtmId}');`}</script>
+          })(window,document,'script','dataLayer','${config.gtmId}');`}}></script>
         }
 
       </Head>
