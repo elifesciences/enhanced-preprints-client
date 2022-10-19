659c125c6524800aeb9005150f3aa6cbfbbd60c5
Will Byrne <will.alex.byrne@gmail.com>
2022-10-19 13:48:55 +0100

    move the selected tab functionality to the article level so it can be passed to the assesment box link

--- src/components/molecules/tabbed-navigation/tabbed-navigation.stories.tsx
+++ src/components/molecules/tabbed-navigation/tabbed-navigation.stories.tsx
@@ -1,4 +1,5 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useState } from 'react';
 import { Tab, TabbedNavigation } from '.';
 
 export default {
@@ -6,17 +7,21 @@ export default {
   component: TabbedNavigation,
 } as ComponentMeta<typeof TabbedNavigation>;
 
-const Template: ComponentStory<typeof TabbedNavigation> = (args) => (
-  <TabbedNavigation {...args}>
-    <Tab label="Tab 1">
-      <h2>Tab 1</h2>
-      <p>Test</p>
-    </Tab>
-    <Tab label="Tab 2">
-      <h2>Tab 2</h2>
-      <img src="https://placekitten.com/300/400" alt="cat picture"/>
-    </Tab>
-  </TabbedNavigation>
-);
+const Template: ComponentStory<typeof TabbedNavigation> = (args) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <TabbedNavigation {...args} activeTab={activeTab} setActiveTab={setActiveTab}>
+      <Tab label="Tab 1">
+        <h2>Tab 1</h2>
+        <p>Test</p>
+      </Tab>
+      <Tab label="Tab 2">
+        <h2>Tab 2</h2>
+        <img src="https://placekitten.com/300/400" alt="cat picture"/>
+      </Tab>
+    </TabbedNavigation>
+  );
+};
 
 export const MultipleTabbedNavigation = Template.bind({});
