b236accf079721f35c1c7bcf1e6e6f1f3b2bbb69
Dave Price <d.price@elifesciences.org>
2022-10-31 10:12:39 +0000

    remove accidental characters

--- src/components/atoms/institutions/institutions.tsx
+++ src/components/atoms/institutions/institutions.tsx
@@ -23,7 +23,7 @@ export const Institutions = ({ institutions }: { institutions: Institution[] }):
         ))}
       </ol>
       { institutions.length > institutionLimit
-        ? <span className={styles['institutions-list__expan```sion']} onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${institutions.length - institutionLimit} more`}</span> : ''}
+        ? <span className={styles['institutions-list__expansion']} onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${institutions.length - institutionLimit} more`}</span> : ''}
     </div>
   );
 };
