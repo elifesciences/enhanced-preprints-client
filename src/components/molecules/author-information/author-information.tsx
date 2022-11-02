e8fcade0b9ae1f122f44b2461080d2d6a6e2e2bc
Will Byrne <will.alex.byrne@gmail.com>
2022-11-02 15:46:35 +0000

    extract author-description into a componenet

--- src/components/molecules/author-information/author-information.tsx
+++ src/components/molecules/author-information/author-information.tsx
@@ -1,22 +1,24 @@
 import styles from './author-list.module.scss';
 import { Author } from '../../../types';
 
+const AuthorDescription = ({ author }: { author: Author }): JSX.Element => (
+  <li className={styles['author-list__author']}>
+    <h4 className={styles['author-list__author_name']}>{author.givenNames.join(' ')} {author.familyNames.join(' ')}</h4>
+    {
+      author.affiliations && (
+        <span className={styles['author-list__affiliations']}>
+          {author.affiliations.map(({ name, address }) => `${name}${address ? `, ${address.addressCountry}` : ''}`).join(', ')}
+        </span>
+      )
+    }
+  </li>
+);
+
 export const AuthorInformation = ({ authors }: { authors: Author[] }): JSX.Element => (
   <section id="author-list" className={styles['author-list']}>
     <h2 id="author-information" className={styles['author-list__title']}>Author information</h2>
     <ol className={styles['author-list__authors']}>
-      { authors.map(({ givenNames, familyNames, affiliations }, index) => (
-        <li key={index} className={styles['author-list__author']}>
-          <h4 className={styles['author-list__author_name']}>{givenNames.join(' ')} {familyNames.join(' ')}</h4>
-          {
-            affiliations && (
-              <span className={styles['author-list__affiliations']}>
-                {affiliations.map(({ name, address }) => `${name}${address ? `, ${address.addressCountry}` : ''}`).join(', ')}
-              </span>
-            )
-          }
-        </li>
-      ))}
+      {authors.map((author, index) => <AuthorDescription author={author} key={index}/>)}
     </ol>
   </section>
 );
