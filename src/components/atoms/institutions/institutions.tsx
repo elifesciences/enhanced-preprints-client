84ff040c9c208f2f289873e97c947c1793e09f4b
Will Byrne <will.alex.byrne@gmail.com>
2022-11-02 16:08:26 +0000

    extract logic from institutions render

--- src/components/atoms/institutions/institutions.tsx
+++ src/components/atoms/institutions/institutions.tsx
@@ -6,18 +6,20 @@ const institutionLimit = 3;
 
 export const Institutions = ({ institutions }: { institutions: Institution[] }): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
+  const displayInstitutions = institutions.slice(0, expanded ? institutions.length : institutionLimit);
+  const expansionText = expanded ? 'show less' : `...show ${institutions.length - institutionLimit} more`;
 
   return (
     <div className={styles.institutions}>
       <ol className={styles['institutions-list']} aria-label="Author institutions">
-        { institutions.slice(0, expanded ? institutions.length : institutionLimit).map(({ name, address }, index) => (
+        { displayInstitutions.map(({ name, address }, index) => (
           <li className={styles['institutions-list__item']} key={index}>
             {name}{ address ? <address className={styles.institution__address}>{address.addressCountry ?? ''}</address> : ''}
           </li>
         ))}
       </ol>
       { institutions.length > institutionLimit
-        ? <span className={styles['institutions-list__expansion']} onClick={() => setExpanded(!expanded)}>{expanded ? 'show less' : `...show ${institutions.length - institutionLimit} more`}</span> : ''}
+        ? <span className={styles['institutions-list__expansion']} onClick={() => setExpanded(!expanded)}>{expansionText}</span> : ''}
     </div>
   );
 };
