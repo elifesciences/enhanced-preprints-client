c1623e3b0f7a6e2edb74e98af8eeadaa59365fad
Dave Price <d.price@elifesciences.org>
2022-11-08 08:59:11 +0000

    Add 404 elements and styling

---
+++ src/pages/404.page.tsx
@@ -0,0 +1,9 @@
+import { PageNotFound } from '../components/pages/error-pages/page-not-found';
+
+export const App = (): JSX.Element => (
+  <div className="App">
+    <PageNotFound></PageNotFound>
+  </div>
+);
+
+export default App;
