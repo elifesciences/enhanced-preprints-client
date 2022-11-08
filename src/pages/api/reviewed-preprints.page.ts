0815ba688add372b1a8415477bff8085f4bad416
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-11-08 11:40:47 +0000

    Gather the metadata for all reviewed preprints

--- src/pages/api/reviewed-preprints.page.ts
+++ src/pages/api/reviewed-preprints.page.ts
@@ -1,19 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { config } from '../../config';
 import { manuscripts } from '../../manuscripts';
+import { jsonFetch } from '../../utils/json-fetch';
+import { MetaData } from '../../types';
 import { SubjectList } from '../../components/molecules/article-flag-list/article-flag-list';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const ids = Object.keys(manuscripts).filter(id => id.match(/^[0-9]+$/));
 
+  const meta = await Promise.all(ids.map(async id => jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${manuscripts[id].preprintDoi}/metadata`).then(js => js.title)));
+
+  // res.status(200).json({
+  //   total: ids.length,
+  //   items: ids.map(id => ({
+  //     id,
+  //     doi: manuscripts[id].preprintDoi,
+  //     pdf: manuscripts[id].pdfUrl,
+  //     status: "reviewed",
+  //     stage: "published",
+  //     subjects: SubjectList({ msas: manuscripts[id].msas }),
+  //   })),
+  // });
+
   res.status(200).json({
     total: ids.length,
-    items: ids.map(id => ({
-      id,
-      doi: manuscripts[id].preprintDoi,
-      pdf: manuscripts[id].pdfUrl,
-      status: "reviewed",
-      stage: "published",
-      subjects: SubjectList({ msas: manuscripts[id].msas }),
-    })),
+    items: meta,
   });
 };
