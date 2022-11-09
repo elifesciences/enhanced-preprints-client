eab2834e741303d08de55f5836b930add697a129
Dave Price <d.price@elifesciences.org>
2022-11-09 15:46:24 +0000

    Adding siteheader to storybook

--- src/components/pages/error-pages/page-not-found.stories.tsx
+++ src/components/pages/error-pages/page-not-found.stories.tsx
@@ -1,10 +1,11 @@
 import { ComponentStory } from '@storybook/react';
 import { PageNotFound } from './page-not-found';
+import { DefaultLayout } from '../../layouts/default';
 
 export default {
   title: 'Pages/Page not found',
 };
 
-const Template: ComponentStory<typeof PageNotFound> = () => <PageNotFound />;
+const Template: ComponentStory<typeof PageNotFound> = () => <DefaultLayout><PageNotFound /></DefaultLayout>;
 
 export const PageNotFoundPage = Template.bind({});
