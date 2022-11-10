f18abb1a76d5d3b510cfd2c9fd0c674869899805
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-11-10 00:55:06 +0000

    Handle gathering of MetaData

--- src/pages/api/reviewed-preprints.page.ts
+++ src/pages/api/reviewed-preprints.page.ts
@@ -2,28 +2,87 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { config } from '../../config';
 import { manuscripts } from '../../manuscripts';
 import { jsonFetch } from '../../utils/json-fetch';
-import { MetaData } from '../../types';
+import { Author, Content, MetaData } from '../../types';
 import { SubjectList } from '../../components/molecules/article-flag-list/article-flag-list';
 
+const wrapContent = (content: Content, type: string) : string => {
+  let tag = null;
+
+  if (type === 'Emphasis') {
+    tag = 'i';
+  } else if (type === 'Subscript') {
+    tag = 'sub';
+  }
+
+  return (tag ? `<${tag}>` : '')+renderContent(content)+(tag ? `</${tag}>` : '');
+};
+
+const renderContent = (content: Content) : string => {
+  if (Array.isArray(content)) {
+    return content.map(i => {
+      if (typeof i === 'string') {
+        return i;
+      }
+
+      if (!Array.isArray(i) && i === Object(i) && i.hasOwnProperty('content')) {
+        return wrapContent(i.content, i.type);
+      }
+    }).join(' ');
+  }
+
+  return content as string;
+};
+
+const prepareAuthor = (author: Author) : string => `${author.givenNames.join(' ')} ${author.familyNames.join(' ')}`;
+
+const prepareAuthors = (authors: Author[]) : string => {
+  let authorLine = '';
+
+  console.log(authors);
+
+  if (authors.length > 0) {
+    authorLine += prepareAuthor(authors[0]);
+  }
+
+  if (authors.length > 1) {
+    authorLine += `, ${prepareAuthor(authors[1])}`;
+  }
+
+  if (authors.length > 2) {
+    authorLine += `${(authors.length > 3) ? ' ... ' : ', '}${prepareAuthor(authors[authors.length - 1])}`;
+  }
+
+  return authorLine;
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const ids = Object.keys(manuscripts).filter(id => id.match(/^[0-9]+$/));
+  const ids = Object.keys(manuscripts).filter(id => id.match(/^[0-9]+$/)).sort();
 
-  const meta = await Promise.all(ids.map(async id => jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${manuscripts[id].preprintDoi}/metadata`).then(js => js.title)));
+  const meta = await Promise.all(ids.map(async id => jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${manuscripts[id].preprintDoi}/metadata`).then(js => {
+    return {
+      id,
+      title: renderContent(js.title),
+      authorLine: prepareAuthors(js.authors),
+    };
+  })));
 
-  // res.status(200).json({
-  //   total: ids.length,
-  //   items: ids.map(id => ({
-  //     id,
-  //     doi: manuscripts[id].preprintDoi,
-  //     pdf: manuscripts[id].pdfUrl,
-  //     status: "reviewed",
-  //     stage: "published",
-  //     subjects: SubjectList({ msas: manuscripts[id].msas }),
-  //   })),
-  // });
+  const items = ids.map(id => {
+    const iMeta = meta.find(obj => obj.id === id)
+
+    return {
+      id,
+      doi: manuscripts[id].preprintDoi,
+      pdf: manuscripts[id].pdfUrl,
+      status: "reviewed",
+      authorLine: iMeta?.authorLine,
+      title: iMeta?.title,
+      stage: "published",
+      subjects: SubjectList({ msas: manuscripts[id].msas }),
+    };
+  });
 
   res.status(200).json({
     total: ids.length,
-    items: meta,
+    items: items,
   });
 };
