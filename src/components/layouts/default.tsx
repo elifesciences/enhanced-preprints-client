e92aff38da598df654a166faf7d831843f5e19f1
Dave Price <d.price@elifesciences.org>
2022-11-16 13:50:05 +0000

    adding to default layout

--- src/components/layouts/default.tsx
+++ src/components/layouts/default.tsx
@@ -1,11 +1,16 @@
 import { SiteHeader } from '../molecules/site-header/site-header';
+import { Footer } from '../molecules/footer/footer';
 import styles from './default.module.scss';
 
 export const DefaultLayout = ({ children }: any): JSX.Element => (
-  <div className={`${styles['grid-container']} ${styles['article-page']}`}>
-    <div className={styles['grid-header']}>
-      <SiteHeader />
+  <>
+    <div className={`${styles['grid-container']} ${styles['article-page']}`}>
+      <div className={styles['grid-header']}>
+        <SiteHeader />
+      </div>
+        {children}
     </div>
-    {children}
-  </div>
+    <Footer/>
+  </>
+
 );
