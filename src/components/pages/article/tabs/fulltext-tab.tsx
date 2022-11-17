3cc1cbc445b8dfe292a937036928ca82c04a91a9
Will Byrne <will.alex.byrne@gmail.com>
2022-11-17 15:49:51 +0000

    minor ts tweaks

--- src/components/pages/article/tabs/fulltext-tab.tsx
+++ src/components/pages/article/tabs/fulltext-tab.tsx
@@ -1,30 +1,30 @@
 import styles from '../article-page.module.scss';
 import { ArticleContent } from '../../../atoms/article-content/article-content';
-import { Content } from '../../../../types/content';
 import { JumpToMenu } from '../../../atoms/jump-to-menu/jump-to-menu';
 import { Abstract } from '../../../atoms/abstract/abstract';
 import { ReviewContent } from '../../../atoms/review-content/review-content';
 import { ReferenceList } from '../../../atoms/reference-list/reference-list';
 import { AuthorInformationList } from '../../../molecules/author-information-list/author-information-list';
-import { MetaData, PeerReview } from '../../../../types';
+import { Content, MetaData, PeerReview } from '../../../../types';
 
 export const ArticleFullTextTab = (props: { metaData: MetaData, content: Content, peerReview?: PeerReview }): JSX.Element => {
-  const assessment = [];
+  const headings = [
+    { id: 'abstract', text: 'Abstract' },
+    ...props.metaData.headings,
+    { id: 'references', text: 'References' },
+    { id: 'author-list', text: 'Author Information' },
+  ];
+
   if (props.peerReview !== undefined) {
-    assessment.push({ id: 'assessment', text: 'eLife assessment' });
+    headings.splice(1, 0, { id: 'assessment', text: 'eLife assessment' });
   }
+
   return (
     <div className={styles['tabbed-navigation__content']}>
-      <JumpToMenu headings={[
-        { id: 'abstract', text: 'Abstract' },
-        ...assessment,
-        ...props.metaData.headings,
-        { id: 'references', text: 'References' },
-        { id: 'author-list', text: 'Author Information' },
-      ]} />
+      <JumpToMenu headings={headings} />
       <div className={styles['article-body-container']}>
         <Abstract content={props.metaData.abstract} />
-        { props.peerReview !== undefined ? <ReviewContent content={props.peerReview.evaluationSummary.text} isAssessment={true} peerReviewUrl={`/reviewed-preprints/${props.metaData.msid}/reviews`}/> : '' }
+        { props.peerReview && <ReviewContent content={props.peerReview.evaluationSummary.text} isAssessment={true} peerReviewUrl={`/reviewed-preprints/${props.metaData.msid}/reviews`}/> }
         <ArticleContent content={props.content} />
         <ReferenceList references={props.metaData.references} />
         <AuthorInformationList authors={props.metaData.authors}/>
