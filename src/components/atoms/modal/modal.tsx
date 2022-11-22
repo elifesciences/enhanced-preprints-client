5d8f7daf41101d0e3bc04c9d32f72c73fc765f2a
Will Byrne <will.alex.byrne@gmail.com>
2022-11-22 12:28:58 +0000

    remove hook and simplify click handling

--- src/components/atoms/modal/modal.tsx
+++ src/components/atoms/modal/modal.tsx
@@ -1,18 +1,22 @@
-import { useRef, useState } from 'react';
-import { useOnClickOutside } from '../../../utils/use-onclick-outside';
+import { useRef, useState, MouseEvent } from 'react';
 import styles from './modal.module.scss';
 
 export const Modal = (): JSX.Element => {
   const [showModal, setShowModal] = useState(false);
-  const outsideRef = useRef<HTMLDivElement>(null!);
+  const contentRef = useRef<HTMLDivElement | null>(null);
   const closeModal = () => setShowModal(false);
-  // useOnClickOutside(outsideRef, closeModal);
+
+  const clickHandler = (event: MouseEvent<HTMLDivElement>) => {
+    if (contentRef.current && !contentRef.current.contains(event.target as Element)) {
+      closeModal();
+    }
+  };
 
   return (
   <>
     <button onClick={() => setShowModal(!showModal)}>Popup Link</button>
-    <div className={`${styles['modal-container']} ${showModal ? styles['modal-content__show'] : ''} `}>
-      <div ref={outsideRef} className={styles['modal-content']}>
+    <div onClick={(event) => clickHandler(event)} className={`${styles['modal-container']} ${showModal ? styles['modal-content__show'] : ''} `}>
+      <div ref={contentRef} className={styles['modal-content']}>
         <button onClick={closeModal}>Close</button>
         <div className="modal-content__show">
           <h6>Modal Window Title</h6>
@@ -21,4 +25,5 @@ export const Modal = (): JSX.Element => {
       </div>
     </div>
   </>
-)};
+  );
+};
