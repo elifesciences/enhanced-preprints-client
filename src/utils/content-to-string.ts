03a28e73317c124e3a3bd36875d4b373f0bf5f99
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-11-22 17:40:46 +0000

    Allow optional tags to wrap content in contentToString

--- src/utils/content-to-string.ts
+++ src/utils/content-to-string.ts
@@ -1,6 +1,19 @@
 import { Content } from '../types/content';
 
-export const contentToString = (content: Content): string => {
+export enum ContentType {
+  paragraph = 'Paragraph',
+  emphasis = 'Emphasis',
+  strong = 'Strong',
+  subscript = 'Subscript',
+  superscript = 'Superscript',
+};
+
+type ContentTypeTag = {
+  id: ContentType,
+  tag: string,
+}
+
+export const contentToString = (content: Content, contentTypeTags?: ContentTypeTag[]): string => {
   if (typeof content === 'undefined') {
     return '';
   }
@@ -9,15 +22,16 @@ export const contentToString = (content: Content): string => {
   }
 
   if (Array.isArray(content)) {
-    return content.map((part) => contentToString(part)).join('');
+    return content.map((part) => contentToString(part, contentTypeTags)).join('');
   }
   switch (content.type) {
-    case 'Paragraph':
-    case 'Emphasis':
-    case 'Strong':
-    case 'Superscript':
-    case 'Subscript':
-      return contentToString(content.content);
+    case ContentType.paragraph:
+    case ContentType.emphasis:
+    case ContentType.strong:
+    case ContentType.subscript:
+    case ContentType.superscript:
+      const tag = contentTypeTags?.find((typeTag) => typeTag.id === content.type)?.tag;
+      return (tag ? `<${tag}>` : '') + contentToString(content.content, contentTypeTags) + (tag ? `</${tag}>` : '');
     default:
       return '';
   }
