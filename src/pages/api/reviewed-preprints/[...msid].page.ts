cd94936f82583d8126d2b67a1df9a6bfb2a5c75a
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-11-22 12:17:15 +0000

    Slight refactor

--- src/pages/api/reviewed-preprints/[...msid].page.ts
+++ src/pages/api/reviewed-preprints/[...msid].page.ts
@@ -16,22 +16,19 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (typeof msid !== 'string' || !manuscripts[msid]) {
     console.log('Cannot find msid configured'); // eslint-disable-line no-console
     errorNotFoundRequest(res);
-    return;
+  } else {
+    const manuscript = manuscripts[msid];
+  
+    const [metaData, content] = await Promise.all([
+      jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${manuscript.preprintDoi}/metadata`),
+      jsonFetch<Content>(`${config.apiServer}/api/reviewed-preprints/${manuscript.preprintDoi}/content`),
+    ]);
+  
+    writeResponse(
+      res,
+      'application/vnd.elife.reviewed-preprint-item+json; version=1',
+      200,
+      { ...reviewedPreprintSnippet(manuscript, metaData), indexContent: contentToString(content) },
+    );
   }
-
-  const manuscript = manuscripts[msid];
-
-  const [metaData, content] = await Promise.all([
-    jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${manuscript.preprintDoi}/metadata`),
-    jsonFetch<Content>(`${config.apiServer}/api/reviewed-preprints/${manuscript.preprintDoi}/content`),
-  ]);
-
-  const item = reviewedPreprintSnippet(manuscript, metaData);
-
-  writeResponse(
-    res,
-    'application/vnd.elife.reviewed-preprint-item+json; version=1',
-    200,
-    { ...item, indexContent: contentToString(content) },
-  );
 };
