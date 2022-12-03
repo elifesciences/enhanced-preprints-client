d3c5fc5322fb42b3918c49abfd40b2e7486de910
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-03 09:59:24 +0000

    Check response headers set correctly

--- browser-tests/api-reviewed-preprints.spec.ts
+++ browser-tests/api-reviewed-preprints.spec.ts
@@ -3,6 +3,15 @@ import { test, expect } from '@playwright/test';
 test('reviewed preprints api listing', async ({ request }) => {
   const response = await request.get('http://localhost:3001/api/reviewed-preprints');
   expect(response.ok()).toBeTruthy();
+  
+  const headers = response.headers();
+  expect(headers['content-type']).toBe('application/vnd.elife.reviewed-preprint-list+json; version=1');
+  expect(headers['cache-control']).toBe('max-age=300, public, stale-if-error=86400, stale-while-revalidate=300');
+
+  const headerVary = headers['vary'].split(', ');
+  expect(headerVary).toContain('Accept');
+  expect(headerVary).toContain('Authorization');
+
   const responseJson = await response.json();
   expect(responseJson.total).toBe(1);
   expect(responseJson.items[0].id).toBe('123');
@@ -13,6 +22,15 @@ test('reviewed preprints api listing', async ({ request }) => {
 test('reviewed preprints api item found', async ({ request }) => {
   const response = await request.get('http://localhost:3001/api/reviewed-preprints/123');
   expect(response.ok()).toBeTruthy();
+  
+  const headers = response.headers();
+  expect(headers['content-type']).toBe('application/vnd.elife.reviewed-preprint-item+json; version=1');
+  expect(headers['cache-control']).toBe('max-age=300, public, stale-if-error=86400, stale-while-revalidate=300');
+
+  const headerVary = headers['vary'].split(', ');
+  expect(headerVary).toContain('Accept');
+  expect(headerVary).toContain('Authorization');
+
   const responseJson = await response.json();
   expect(responseJson.id).toBe('123');
   expect(responseJson.doi).toBe('10.1101/123456');
@@ -21,6 +39,14 @@ test('reviewed preprints api item found', async ({ request }) => {
 });
 
 test('reviewed preprints api item unknown', async ({ request }) => {
-  const response = await request.get('http://localhost:3001/api/retoBeTruthyviewed-preprints/unknown');
+  const response = await request.get('http://localhost:3001/api/reviewed-preprints/unknown');
   expect(response.status()).toEqual(404);
+  
+  const headers = response.headers();
+  expect(headers['content-type']).toBe('application/json');
+  expect(headers['cache-control']).toBe('must-revalidate, no-cache, private');
+
+  const headerVary = headers['vary'].split(', ');
+  expect(headerVary).toContain('Accept');
+  expect(headerVary).toContain('Authorization');
 });
