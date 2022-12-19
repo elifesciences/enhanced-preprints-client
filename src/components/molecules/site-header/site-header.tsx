4ab56544e79b2a9b6a52ee06d26bdc834ecef5bc
Scott Aubrey <scott@aubrey.org.uk>
2022-12-19 11:45:38 +0000

    switch all molecules to use non-modular SCSS

--- src/components/molecules/site-header/site-header.tsx
+++ src/components/molecules/site-header/site-header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
-import styles from './site-header.module.scss';
+import './site-header.scss';
 import logo from '../../../../public/elife-logo.svg';
 
 const Overlay = ({ closeOverlay }: { closeOverlay: () => void }): JSX.Element => createPortal(<div onClick={() => closeOverlay()} className={styles.overlay} id="overlayMainMenu"></div>, document.getElementsByTagName('BODY')[0]);
@@ -9,101 +9,101 @@ const Overlay = ({ closeOverlay }: { closeOverlay: () => void }): JSX.Element =>
 export const SiteHeader = (): JSX.Element => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <div className={styles['site-header']}>
-      <div className={styles['site-header-container']}>
+    <div className="site-header">
+      <div className="site-header-container">
         <Image
-          className={styles['site-header__logo']}
+          className="site-header__logo"
           src={logo}
           alt="eLife logo"
           width="80"
           height="30"
         />
-        <span className={styles['site-header__title']}>Enhanced Preprints</span>
+        <span className="site-header__title">Enhanced Preprints</span>
 
         {showMenu &&
             <>
           <Overlay closeOverlay={() => setShowMenu(false)}/>
-              <div className={`${styles.wrapper} ${styles['main-menu--js']} ${styles['main-menu--shown']}`} id="mainMenu" data-behaviour="MainMenu" tabIndex={0} aria-expanded="true" data-behaviour-initialised="true">
-                <div className={styles['main-menu__title']}>
+              <div className="wrapper main-menu--js main-menu--shown" id="mainMenu" data-behaviour="MainMenu" tabIndex={0} aria-expanded="true" data-behaviour-initialised="true">
+                <div className="main-menu__title">
                   <Image
                       src={logo}
                       alt="eLife logo"
                       width="80"
                       height="30"
                   />
-                  <button className={styles['main-menu__close_control']} id="mainMenuCloseControl" onClick={() => setShowMenu(false)}>Close</button>
+                  <button className="main-menu__close_control" id="mainMenuCloseControl" onClick={() => setShowMenu(false)}>Close</button>
                 </div>
-                <nav className={styles['main-menu__container']} role="navigation">
-                  <ul className={styles['main-menu__list']}>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://elifesciences.org/" className={styles['main-menu__list_link']}>Home</a>
+                <nav className="main-menu__container" role="navigation">
+                  <ul className="main-menu__list">
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://elifesciences.org/" className="main-menu__list_link">Home</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://elifesciences.org/magazine" className={styles['main-menu__list_link']}>Magazine</a>
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://elifesciences.org/magazine" className="main-menu__list_link">Magazine</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://elifesciences.org/community" className={styles['main-menu__list_link']}>Community</a>
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://elifesciences.org/community" className="main-menu__list_link">Community</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://elifesciences.org/about" className={styles['main-menu__list_link']}>About</a>
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://elifesciences.org/about" className="main-menu__list_link">About</a>
                     </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="https://elifesciences.org/subjects" className={styles['main-menu__list_link']}>Research categories</a>
+                    <li className="main-menu__list_item">
+                      <a href="https://elifesciences.org/subjects" className="main-menu__list_link">Research categories</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['end-of-group']}`}>
-                      <a href="https://elifesciences.org/inside-elife" className={styles['main-menu__list_link']}>Inside eLife</a>
+                    <li className="main-menu__list_item end-of-group">
+                      <a href="https://elifesciences.org/inside-elife" className="main-menu__list_link">Inside eLife</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://elifesciences.org/search" className={styles['main-menu__list_link']}>Search</a>
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://elifesciences.org/search" className="main-menu__list_link">Search</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']} ${styles['end-of-group']}`}>
-                      <a href="https://elifesciences.org/content-alerts" className={styles['main-menu__list_link']}>Subscribe to alerts</a>
+                    <li className="main-menu__list_item hidden-wide end-of-group">
+                      <a href="https://elifesciences.org/content-alerts" className="main-menu__list_link">Subscribe to alerts</a>
                     </li>
-                    <li className={`${styles['main-menu__list_item']} ${styles['hidden-wide']}`}>
-                      <a href="https://reviewer.elifesciences.org/login" className={styles['main-menu__list_link']}>Submit your research</a>
+                    <li className="main-menu__list_item hidden-wide">
+                      <a href="https://reviewer.elifesciences.org/login" className="main-menu__list_link">Submit your research</a>
                     </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="https://reviewer.elifesciences.org/author-guide/editorial-process" className={styles['main-menu__list_link']}>Author guide</a>
+                    <li className="main-menu__list_item">
+                      <a href="https://reviewer.elifesciences.org/author-guide/editorial-process" className="main-menu__list_link">Author guide</a>
                     </li>
-                    <li className={styles['main-menu__list_item']}>
-                      <a href="https://reviewer.elifesciences.org/reviewer-guide/review-process" className={styles['main-menu__list_link']}>Reviewer guide</a>
+                    <li className="main-menu__list_item">
+                      <a href="https://reviewer.elifesciences.org/reviewer-guide/review-process" className="main-menu__list_link">Reviewer guide</a>
                     </li>
                   </ul>
-                  <a href="#siteHeader" className={styles['to-top-link']}>Back to top</a>
+                  <a href="#siteHeader" className="to-top-link">Back to top</a>
                 </nav>
               </div>
             </>
         }
-        <nav className={styles['nav-primary']}>
-          <ul className={styles['nav-primary__list']}>
-            <li className={`${styles['nav-primary__item']} ${styles['nav-primary__item--first']}`}>
+        <nav className="nav-primary">
+          <ul className="nav-primary__list">
+            <li className="nav-primary__item nav-primary__item--first">
               <a href="#mainMenu" onClick={() => setShowMenu(true)}>Menu</a>
             </li>
-            <li className={styles['nav-primary__item']}>
+            <li className="nav-primary__item">
               <a href="https://elifesciences.org/">Home</a>
             </li>
-            <li className={styles['nav-primary__item']}>
+            <li className="nav-primary__item">
               <a href="https://elifesciences.org/magazine">Magazine</a>
             </li>
-            <li className={styles['nav-primary__item']}>
+            <li className="nav-primary__item">
               <a href="https://elifesciences.org/community">Community</a>
             </li>
-            <li className={styles['nav-primary__item']}>
+            <li className="nav-primary__item">
               <a href="https://elifesciences.org/about">About</a>
             </li>
           </ul>
         </nav>
 
-        <nav className={styles['nav-secondary']}>
-          <ul className={styles['nav-secondary__list']}>
-            <li className={`${styles['nav-secondary__item']} ${styles['nav-secondary__item--search']}`}>
+        <nav className="nav-secondary">
+          <ul className="nav-secondary__list">
+            <li className="nav-secondary__item nav-secondary__item--search">
               <a href="https://elifesciences.org/search" rel="search">Search</a>
             </li>
-            <li className={`${styles['nav-secondary__item']} ${styles['nav-secondary__item--alert']}`}>
+            <li className="nav-secondary__item nav-secondary__item--alert">
               <a href="https://elifesciences.org/content-alerts">Alerts</a>
             </li>
-            <li className={styles['nav-secondary__item']}>
-              <a href="https://reviewer.elifesciences.org/login" className={`${styles['block-button']} ${styles['block-button__variant-one']}`}>Submit your research</a>
+            <li className="nav-secondary__item">
+              <a href="https://reviewer.elifesciences.org/login" className="block-button block-button__variant-one">Submit your research</a>
             </li>
           </ul>
         </nav>
