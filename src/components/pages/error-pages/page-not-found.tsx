890e944d0aa761d533d482d44f1a995703dc9e4c
Scott Aubrey <scott@aubrey.org.uk>
2022-12-19 11:49:02 +0000

    switch all pages to use non-modular SCSS

--- src/components/pages/error-pages/page-not-found.tsx
+++ src/components/pages/error-pages/page-not-found.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image';
-import styles from './page-not-found.module.scss';
+import './page-not-found.scss';
 import MissingImage from '../../../images/errors/404.svg';
 import { Button } from '../../atoms/button/button';
 
 export const PageNotFound = (): JSX.Element => (
-  <main className={styles['full-width-section']}>
-    <div className={styles['error-wrapper']}>
-      <div className={styles.error}>
-        <Image src={MissingImage} width="64" height="157" alt="Oops" className={styles.error__icon} />
-        <h1 className={styles.error__title}>Oops!</h1>
+  <main className="full-width-section">
+    <div className="error-wrapper">
+      <div className="error">
+        <Image src={MissingImage} width="64" height="157" alt="Oops" className="error__icon" />
+        <h1 className="error__title">Oops!</h1>
         <p>The page you were looking for is not found.</p>
         <Button iconName={'default'} text={'Back to homepage'} url={'/'} />
       </div>
