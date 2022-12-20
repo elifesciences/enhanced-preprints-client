3504af584fa436f8a1656177ab52ba6542bc644f
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-20 14:43:32 +0000

    Add clipboard click behaviour if supported by browser (first attempt)

--- src/components/atoms/clipboard/clipboard.tsx
+++ src/components/atoms/clipboard/clipboard.tsx
@@ -1,10 +1,16 @@
 import './clipboard.scss';
 
-export const Clipboard = (): JSX.Element => (
-  <div className="clipboard-container">
-    <div className="form-item">
-        <input type="url" className="text-field text-field--url" value="https://doi.org/10.7554/eLife.09560" />
-        <button className="button button--default">copy to clipboard</button>
+const supportsClipboardAPI = () => (navigator.clipboard);
+
+export const Clipboard = (): JSX.Element => {
+
+  return (
+    <div className="clipboard-container">
+      <div className="form-item">
+          <input type="url" className="text-field text-field--url" value="https://doi.org/10.7554/eLife.09560" />
+
+          {(supportsClipboardAPI() ? <button className="button button--default" onClick={() => {navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560')}}>copy to clipboard</button> : <></>)}
+      </div>
     </div>
-  </div>
-);
+  );
+};
