1fa3846d919b78029eb25eab987258fa9316f159
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-20 15:12:03 +0000

    Add client side check for clipboard support and add class if button clicked

--- src/components/atoms/clipboard/clipboard.tsx
+++ src/components/atoms/clipboard/clipboard.tsx
@@ -1,15 +1,20 @@
+import { useEffect, useState } from 'react';
 import './clipboard.scss';
 
-const supportsClipboardAPI = () => (navigator.clipboard);
+const supportsClipboardAPI = () => (navigator.clipboard ? true : false);
 
 export const Clipboard = (): JSX.Element => {
+  const [showButton, setShowButton] = useState<boolean>(false);
+  const [copied, setCopied] = useState<boolean>(false);
+
+  useEffect(() => setShowButton(supportsClipboardAPI()), []);
 
   return (
     <div className="clipboard-container">
       <div className="form-item">
           <input type="url" className="text-field text-field--url" value="https://doi.org/10.7554/eLife.09560" />
 
-          {(supportsClipboardAPI() ? <button className="button button--default" onClick={() => {navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560')}}>copy to clipboard</button> : <></>)}
+          {(showButton ? <button className={`button button--default${copied ? ' copied' : ''}`} onClick={() => {setCopied(true); navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560');}}>copy to clipboard</button> : <></>)}
       </div>
     </div>
   );
