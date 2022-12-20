879db2516e11c679d3f9a3dc4dbca16e2ad93806
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-20 15:24:06 +0000

    Allow clipboard text to be set as parameter

--- src/components/atoms/clipboard/clipboard.stories.tsx
+++ src/components/atoms/clipboard/clipboard.stories.tsx
@@ -2,10 +2,10 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Clipboard } from './clipboard';
 
 export default {
-  title: 'Atoms/ Clipboard',
+  title: 'Atoms/Clipboard',
   component: Clipboard,
 } as ComponentMeta<typeof Clipboard>;
 
-const Template: ComponentStory<typeof Clipboard> = () => <Clipboard />;
+const Template: ComponentStory<typeof Clipboard> = () => <Clipboard text={'https://doi.org/10.7554/eLife.09560'} />;
 
 export const ClipboardContainer = Template.bind({});
--- src/components/atoms/clipboard/clipboard.tsx
+++ src/components/atoms/clipboard/clipboard.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import './clipboard.scss';
 
+type ClipboardProps = {
+  text: string,
+};
+
 const supportsClipboardAPI = () => (navigator.clipboard ? true : false);
 
-export const Clipboard = (): JSX.Element => {
+export const Clipboard = ({ text }: ClipboardProps): JSX.Element => {
   const [showButton, setShowButton] = useState<boolean>(false);
   const [copied, setCopied] = useState<boolean>(false);
 
@@ -12,9 +16,9 @@ export const Clipboard = (): JSX.Element => {
   return (
     <div className="clipboard-container">
       <div className="form-item">
-          <input type="url" className="text-field text-field--url" value="https://doi.org/10.7554/eLife.09560" />
+          <input type="input" className="text-field" value={text} />
 
-          {(showButton ? <button className={`button button--default${copied ? ' copied' : ''}`} onClick={() => {setCopied(true); navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560');}}>{copied ? 'copied' : 'copy to clipboard'}</button> : <></>)}
+          {(showButton ? <button className={`button button--default${copied ? ' copied' : ''}`} onClick={() => {setCopied(true); navigator.clipboard.writeText(text);}}>{copied ? 'copied' : 'copy to clipboard'}</button> : <></>)}
       </div>
     </div>
   );
