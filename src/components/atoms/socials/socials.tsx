d6584c6eaeb6e01a385a715df065e40844ff3e4b
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-20 16:36:18 +0000

    Refactor to bring inline with upstream changes

--- src/components/atoms/socials/socials.tsx
+++ src/components/atoms/socials/socials.tsx
@@ -1,4 +1,4 @@
-import styles from './socials.module.scss';
+import './socials.scss';
 
 type Props = {
   emailUrl: string,
@@ -11,30 +11,30 @@ type Props = {
 export const Socials = ({
   emailUrl, twitterUrl, facebookUrl, linkedinUrl, redditUrl,
 }: Props): JSX.Element => (
-  <div className={styles['socials-container']}>
-    <ul className={styles['socials-sharers']}>
+  <div className="socials-container">
+    <ul className="socials-sharers">
       <li>
-        <a className={`${styles['socials-sharer']} ${styles.email}`} href={ emailUrl } target="_blank" rel="noopener noreferrer" aria-label="Share by Email">
+        <a className="socials-sharer email" href={ emailUrl } target="_blank" rel="noopener noreferrer" aria-label="Share by Email">
           Email
         </a>
       </li>
       <li>
-        <a className={`${styles['socials-sharer']} ${styles.twitter}`} href={twitterUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Twitter">
+        <a className="socials-sharer twitter" href={twitterUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Twitter">
           Twitter
         </a>
       </li>
       <li>
-        <a className={`${styles['socials-sharer']} ${styles.facebook}`} href={facebookUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Facebook">
+        <a className="socials-sharer facebook" href={facebookUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Facebook">
           Facebook
         </a>
       </li>
       <li>
-        <a className={`${styles['socials-sharer']} ${styles.linkedin}`} href={linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by LinkedIn">
+        <a className="socials-sharer linkedin" href={linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by LinkedIn">
           LinkedIn
         </a>
       </li>
       <li>
-        <a className={`${styles['socials-sharer']} ${styles.reddit}`} href={redditUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Reddit">
+        <a className="socials-sharer reddit" href={redditUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Reddit">
           Reddit
         </a>
       </li>
