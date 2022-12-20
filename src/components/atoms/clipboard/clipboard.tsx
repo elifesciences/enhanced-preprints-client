31f9aef5a8db7efa7c0fb8aaa7c06b729b025139
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-12-20 15:15:07 +0000

    Change text of button on click

--- src/components/atoms/clipboard/clipboard.tsx
+++ src/components/atoms/clipboard/clipboard.tsx
@@ -14,7 +14,7 @@ export const Clipboard = (): JSX.Element => {
       <div className="form-item">
           <input type="url" className="text-field text-field--url" value="https://doi.org/10.7554/eLife.09560" />
 
-          {(showButton ? <button className={`button button--default${copied ? ' copied' : ''}`} onClick={() => {setCopied(true); navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560');}}>copy to clipboard</button> : <></>)}
+          {(showButton ? <button className={`button button--default${copied ? ' copied' : ''}`} onClick={() => {setCopied(true); navigator.clipboard.writeText('https://doi.org/10.7554/eLife.09560');}}>{copied ? 'copied' : 'copy to clipboard'}</button> : <></>)}
       </div>
     </div>
   );
