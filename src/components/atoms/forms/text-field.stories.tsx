52d4ffc8a667058aa6bdc6fde7a6ea34d6ca9de1
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-01-05 13:08:06 +0000

    Introduce form-item text-field into component library

---
+++ src/components/atoms/forms/text-field.stories.tsx
@@ -0,0 +1,70 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { TextField } from './text-field';
+
+export default {
+  title: 'Atoms/Forms/TextField',
+  component: TextField,
+} as ComponentMeta<typeof TextField>;
+
+const Template: ComponentStory<typeof TextField> = (args) => <TextField {...args} />;
+
+export const TextFieldDefault = Template.bind({});
+TextFieldDefault.args = {
+  id: 'emailAddress1',
+  inputType: 'email',
+  label: {
+    isVisuallyHidden: false,
+    labelText: 'Email Address',
+  },
+  name: 'emailAddress',
+  autofocus: true,
+};
+
+export const TextFieldError = Template.bind({});
+TextFieldError.args = {
+  id: 'name4',
+  inputType: 'text',
+  label: {
+    labelText: 'Error text field',
+  },
+  name: 'name',
+  state: 'invalid',
+  messageGroup: {
+    id: 'idOfTheMessageGroup',
+    errorText: 'Please provide a valid value',
+  }
+};
+
+export const TextFieldInfo = Template.bind({});
+TextFieldInfo.args = {
+  id: 'comment1',
+  inputType: 'text',
+  label: {
+    labelText: 'Text field with additional info',
+  },
+  name: 'comment',
+  state: 'invalid',
+  autofocus: true,
+  disabled: false,
+  messageGroup: {
+    id: 'idOfTheMessageGroup',
+    infoText: 'You may wish to consider this',
+  }
+};
+
+export const TextFieldInfoError = Template.bind({});
+TextFieldInfoError.args = {
+  id: 'name4',
+  inputType: 'text',
+  label: {
+    labelText: 'Error text field with additional info',
+  },
+  name: 'name',
+  state: 'invalid',
+  messageGroup: {
+    id: 'idOfTheMessageGroup',
+    errorText: 'Please provide a valid value',
+    infoText: 'You may wish to consider this',
+  }
+};
---
+++ src/components/atoms/forms/text-field.tsx
@@ -0,0 +1,83 @@
+import './form-item.scss';
+import './text-field.scss';
+
+type TextFieldProps = {
+  id?: string,
+  state?: 'valid' | 'invalid',
+  label?: {
+    labelText: string,
+    isVisuallyHidden?: boolean,
+  },
+  inputType: 'email' | 'text',
+  name?: string,
+  autofocus?: boolean,
+  value?: string,
+  required?: boolean,
+  disabled?: boolean,
+  placeholder?: string,
+  messageGroup?: {
+    id: string,
+  } & (
+    {
+      errorText: string,
+    } |
+    {
+      infoText: string,
+    }
+  ),
+};
+
+export const TextField = ({
+  id,
+  state,
+  label,
+  inputType,
+  name,
+  autofocus,
+  value,
+  required,
+  disabled,
+  placeholder,
+  messageGroup,
+}: TextFieldProps) => (
+  <div className={`form-item${state ? ` form-item--${state}` : ''}`}>
+    {label ? (
+      <div className="form-item__label_container">
+        <label
+          className={`form-item__label${label.isVisuallyHidden ? ' visuallyhidden' : ''}`}
+          {...{for: id ? id : undefined,}}
+        >
+        {label.labelText}
+      </label>
+      </div>
+    ) : ''}
+
+    <input
+      type={inputType}
+      className={`text-field text-field--${inputType}${state ? ` text-field--${state}` : ''}`}
+      {...{
+        id,
+        name,
+        autofocus: autofocus ? '' : undefined,
+        required,
+        disabled,
+        placeholder,
+      }}
+      {...value ? {value} : {}}
+    />
+
+    {messageGroup ? (
+      <div id={messageGroup.id}>
+
+        {'infoText' in messageGroup ? (
+          <div className="form-item__message form-item__message--info">{messageGroup.infoText}</div>
+        ) : ''}
+
+        {'errorText' in messageGroup ? (
+          <div className="form-item__message form-item__message--invalid">{messageGroup.errorText}</div>
+        ) : ''}
+  
+      </div>
+    ) : ''}
+  </div>
+);
