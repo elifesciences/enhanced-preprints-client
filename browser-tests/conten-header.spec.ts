53356cfacb7164fe463b3b26bfa06b31cd91a614
Will Byrne <will.alex.byrne@gmail.com>
2023-01-09 11:44:47 +0000

    update test names

--- browser-tests/conten-header.spec.ts
+++ browser-tests/conten-header.spec.ts
@@ -8,26 +8,26 @@ test.describe('content header', () => {
     contentHeader = new ContentHeader(page);
   });
 
-  test('homepage has title and links to intro page', async () => {
+  test('content header has title and links to intro page', async () => {
     await contentHeader.assertTitle('Tonight we take over the world!');
   });
 
-  test('homepage has correct msas', async () => {
+  test('content header has correct msas', async () => {
     await contentHeader.assertMSAExists('World Domination');
     await contentHeader.assertMSAExists('Pondering');
     await contentHeader.assertMSAExists('Narf!');
   });
 
-  test('homepage has correct authors', async () => {
+  test('content header has correct authors', async () => {
     await contentHeader.assertAuthorExists('Brain');
     await contentHeader.assertAuthorExists('Pinky Mouse');
   });
 
-  test('homepage has correct institutions', async () => {
+  test('content header has correct institutions', async () => {
     await contentHeader.assertInstitutionExists('Acme LabsNew York'); // formatted this way due to css before adding space and comma
   });
 
-  test('homepage has correct doi', async () => {
+  test('content header has correct doi', async () => {
     await contentHeader.assertDOI('10.7554/eLife.123.1');
   });
 });
