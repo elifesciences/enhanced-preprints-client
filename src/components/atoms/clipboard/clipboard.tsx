9768b0337756a51e38c12942ee252ac9aae3fe37
Dave Price <d.price@elifesciences.org>
2023-01-11 16:39:23 +0000

    refactor text-field and button for share modal

--- src/components/atoms/clipboard/clipboard.tsx
+++ src/components/atoms/clipboard/clipboard.tsx
@@ -17,7 +17,7 @@ export const Clipboard = ({ text }: ClipboardProps): JSX.Element => {
   return (
     <div className="clipboard-container">
       <div className="form-item">
-          <input type="input" className="text-field" value={text} />
+          <input type="input" className="text-field text-field--clipboard" value={text} />
 
           {(showButton ? <button className={`button button--clipboard${copied ? ' copied' : ''}`}
             onClick={() => { setCopied(true); navigator.clipboard.writeText(text); }}>{copied ? 'copied' : 'copy to clipboard'}</button> : <></>)}
