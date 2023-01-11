52bd870305de93875011e933b07a9cca953bed3c
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-01-11 11:12:31 +0000

    Refactor test

--- src/components/atoms/button/button.test.tsx
+++ src/components/atoms/button/button.test.tsx
@@ -3,7 +3,7 @@ import { Button } from './button';
 
 describe('Button', () => {
   it('renders with the provided text', () => {
-    render(<Button text="button text" url='bbc.co.uk' />);
+    render(<Button text='button text' url='bbc.co.uk' />);
 
     expect(screen.getByText('button text')).toBeInTheDocument();
     expect(screen.getByText('button text')).toHaveAttribute('href');
@@ -11,22 +11,20 @@ describe('Button', () => {
   });
 
   it('renders with the provided text and iconName', () => {
-    render(<Button text="button text" iconName="dark_mode" url='bbc.co.uk' />);
+    render(<Button text='button text' iconName='download' url='bbc.co.uk' />);
 
     expect(screen.getByText('button text')).toBeInTheDocument();
-    expect(screen.getByText('button text')).toHaveClass('button--icon');
-    expect(screen.getByText('button text')).toHaveClass('button--icon-dark_mode');
+    expect(screen.getByText('button text')).toHaveClass('button--icon-download');
     expect(screen.getByText('button text')).toHaveAttribute('href');
     expect(screen.getByText('button text').getAttribute('href')).toStrictEqual('bbc.co.uk');
   });
 
   it('renders with the provided text, iconName and variant', () => {
-    render(<Button text="button text" iconName="dark_mode" variant="dark_mode" url='bbc.co.uk' />);
+    render(<Button text='button text' iconName='follow' variant='action' url='bbc.co.uk' />);
 
     expect(screen.getByText('button text')).toBeInTheDocument();
-    expect(screen.getByText('button text')).toHaveClass('button--icon');
-    expect(screen.getByText('button text')).toHaveClass('button--icon-dark_mode');
-    expect(screen.getByText('button text')).toHaveClass('button--dark_mode');
+    expect(screen.getByText('button text')).toHaveClass('button--icon-follow');
+    expect(screen.getByText('button text')).toHaveClass('button--action');
     expect(screen.getByText('button text')).toHaveAttribute('href');
     expect(screen.getByText('button text').getAttribute('href')).toStrictEqual('bbc.co.uk');
   });
