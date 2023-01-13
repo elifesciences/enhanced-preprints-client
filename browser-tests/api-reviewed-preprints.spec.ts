c6d073cb039d3746c636a33218d102b02d7d718b
Will Byrne <will.alex.byrne@gmail.com>
2023-01-13 10:45:21 +0000

    refactor tests and add linting

--- browser-tests/api-reviewed-preprints.spec.ts
+++ browser-tests/api-reviewed-preprints.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test';
 test('reviewed preprints api listing', async ({ request }) => {
   const response = await request.get('http://localhost:3001/api/reviewed-preprints');
   expect(response.ok()).toBeTruthy();
-  
+
   const headers = response.headers();
   expect(headers['content-type']).toBe('application/vnd.elife.reviewed-preprint-list+json; version=1');
   expect(headers['cache-control']).toBe('max-age=300, public, stale-if-error=86400, stale-while-revalidate=300');
 
-  const headerVary = headers['vary'].split(', ');
+  const headerVary = headers.vary.split(', ');
   expect(headerVary).toContain('Accept');
   expect(headerVary).toContain('Authorization');
 
@@ -22,12 +22,12 @@ test('reviewed preprints api listing', async ({ request }) => {
 test('reviewed preprints api item found', async ({ request }) => {
   const response = await request.get('http://localhost:3001/api/reviewed-preprints/123');
   expect(response.ok()).toBeTruthy();
-  
+
   const headers = response.headers();
   expect(headers['content-type']).toBe('application/vnd.elife.reviewed-preprint-item+json; version=1');
   expect(headers['cache-control']).toBe('max-age=300, public, stale-if-error=86400, stale-while-revalidate=300');
 
-  const headerVary = headers['vary'].split(', ');
+  const headerVary = headers.vary.split(', ');
   expect(headerVary).toContain('Accept');
   expect(headerVary).toContain('Authorization');
 
@@ -41,12 +41,12 @@ test('reviewed preprints api item found', async ({ request }) => {
 test('reviewed preprints api item unknown', async ({ request }) => {
   const response = await request.get('http://localhost:3001/api/reviewed-preprints/unknown');
   expect(response.status()).toEqual(404);
-  
+
   const headers = response.headers();
   expect(headers['content-type']).toBe('application/json');
   expect(headers['cache-control']).toBe('must-revalidate, no-cache, private');
 
-  const headerVary = headers['vary'].split(', ');
+  const headerVary = headers.vary.split(', ');
   expect(headerVary).toContain('Accept');
   expect(headerVary).toContain('Authorization');
 });
--- browser-tests/page-objects/content-header.ts
+++ browser-tests/page-objects/content-header.ts
@@ -9,6 +9,8 @@ export class ContentHeader {
 
   readonly authors: Locator;
 
+  readonly authorsExpansion: Locator;
+
   readonly institutions: Locator;
 
   readonly doi: Locator;
@@ -18,8 +20,13 @@ export class ContentHeader {
     this.msas = this.page.locator('.article-flag-list');
     this.title = this.page.locator('.title');
     this.authors = this.page.locator('.authors');
+    this.authorsExpansion = this.page.locator('.authors-list__expansion');
     this.institutions = this.page.locator('.institutions');
     this.doi = this.page.locator('.descriptors__identifier a'); // may need to change when we have more identifiers
+  }
+
+  async toggleAuthorExpansion(): Promise<void> {
+    await this.authorsExpansion.click();
   }
 
   async assertMSAExists(msa: string): Promise<void> {
@@ -42,25 +49,16 @@ export class ContentHeader {
     await expect(authors).toHaveCount(count);
   }
 
-  async assertVisibleAuthorCountAfterToggle(count: number): Promise<void> {
-    const expansion = this.authors.locator('.authors-list__expansion');
-
-    await expansion.click();
-    await this.assertVisibleAuthorCount(count);
-  }
-
   async assertAuthorShowMore(more: number, smallViewport: boolean = false): Promise<void> {
-    const expansion = this.authors.locator('.authors-list__expansion');
-    await expect(expansion).toBeVisible();
-    await expect(expansion).toContainText('show');
-    await expect(expansion).toContainText('more');
-    await expect(expansion.locator(`.authors-list__expansion-count-${smallViewport ? '3' : '10'}`)).toHaveText(more.toString());
+    await expect(this.authorsExpansion).toBeVisible();
+    await expect(this.authorsExpansion).toContainText('show');
+    await expect(this.authorsExpansion).toContainText('more');
+    await expect(this.authorsExpansion.locator(`.authors-list__expansion-count-${smallViewport ? '3' : '10'}`)).toHaveText(more.toString());
   }
 
   async assertAuthorShowLess(): Promise<void> {
-    const expansion = this.authors.locator('.authors-list__expansion');
-    await expect(expansion).toBeVisible();
-    await expect(expansion).toHaveText('show less');
+    await expect(this.authorsExpansion).toBeVisible();
+    await expect(this.authorsExpansion).toHaveText('show less');
   }
 
   async assertInstitutionExists(institution: string): Promise<void> {
