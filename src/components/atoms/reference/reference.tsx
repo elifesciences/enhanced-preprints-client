add4d5bb941b319580e4ba44cb00a89cc33e7ec0
Dave Price <d.price@elifesciences.org>
2023-01-16 15:12:25 +0000

    Reference tests

--- src/components/atoms/reference/reference.tsx
+++ src/components/atoms/reference/reference.tsx
@@ -3,35 +3,41 @@ import './reference.scss';
 
 const formatName = (author: Author) => `${author.familyNames?.join(' ')} ${author.givenNames?.join(' ')}`;
 
-export const Reference = ({ reference, doiIsLink = false }: { reference: ReferenceData, doiIsLink: boolean }): JSX.Element => {
+export const ReferenceBody = ({ reference, isReferenceList = false }: { reference: ReferenceData, isReferenceList: boolean }): JSX.Element => {
   const referenceJournal = reference.isPartOf?.isPartOf?.name ?? reference.isPartOf?.name;
   const referenceVolume = reference.isPartOf?.isPartOf?.volumeNumber ?? reference.isPartOf?.volumeNumber;
-
   const doiIdentifier = reference.identifiers?.find((identifier) => identifier.name === 'doi');
 
   return (
-      <div className="reference" id={reference.id}>
-        <ol className="reference__authors_list">
-          {reference.authors.map((author, index) => (
-            <li key={index} className="reference__author">
-              {formatName(author)}
-            </li>
-          ))}
-        </ol>
-        <span className="reference__authors_list_suffix">{new Date(reference.datePublished).getFullYear()}</span>
-        <span className="reference__title">{reference.title}</span>
-        <span className="reference__origin">
-          {referenceJournal ? <i>{referenceJournal} </i> : ''}
-          {referenceVolume ? <b>{referenceVolume}:</b> : ''}
-          {reference.pageStart}{reference.pageEnd ? `–${reference.pageEnd}` : ''}
+    <>
+      <ol className="reference__authors_list">
+        {reference.authors.map((author, index) => (
+          <li key={index} className="reference__author">
+            {formatName(author)}
+          </li>
+        ))}
+      </ol>
+
+      <span className="reference__authors_list_suffix">{new Date(reference.datePublished).getFullYear()}</span>
+      <span className="reference__title">{reference.title}</span>
+      <span className="reference__origin">
+        {referenceJournal ? <i>{referenceJournal} </i> : ''}
+        {referenceVolume ? <b>{referenceVolume}:</b> : ''}
+        {reference.pageStart}{reference.pageEnd ? `–${reference.pageEnd}` : ''}
+      </span>
+      {doiIdentifier && <span className="reference__doi">
+        {isReferenceList ?
+          <a href={`https://doi.org/${doiIdentifier.value}`} className="reference__doi_link">
+            https://doi.org/{doiIdentifier.value}
+          </a> : `https://doi.org/${doiIdentifier.value}`}
         </span>
-        {doiIdentifier && <span className="reference__doi">
-          {doiIsLink ?
-            <a href={`https://doi.org/${doiIdentifier.value}`} className="reference__doi_link">
-              https://doi.org/{doiIdentifier.value}
-            </a> : `https://doi.org/${doiIdentifier.value}`}
-          </span>
-        }
-      </div>
+      }
+    </>
   );
 };
+
+export const Reference = ({ reference, isReferenceList = false }: { reference: ReferenceData, isReferenceList: boolean }): JSX.Element => (isReferenceList ?
+  <li className="reference" id={reference.id}>{ReferenceBody({ reference, isReferenceList })}</li> :
+  <div className="reference" id={reference.id}>{ReferenceBody({ reference, isReferenceList })}</div>
+);
+
