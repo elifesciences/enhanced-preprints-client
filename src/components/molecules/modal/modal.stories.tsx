a24a34989c6d883006f39b1968a9d8ec984c87b1
Dave Price <d.price@elifesciences.org>
2023-01-16 10:16:18 +0000

    style cite modal

--- src/components/molecules/modal/modal.stories.tsx
+++ src/components/molecules/modal/modal.stories.tsx
@@ -5,6 +5,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Modal } from './modal';
 import { Socials } from '../../atoms/socials/socials';
 import { Clipboard } from '../../atoms/clipboard/clipboard';
+import { ReferenceList } from '../../atoms/reference-list/reference-list';
+import { references } from '../../../utils/mocks';
 
 export default {
   title: 'Molecules/Modal',
@@ -34,5 +36,21 @@ ModalShare.args = {
   children: (<>
     <Clipboard text={'https://doi.org/10.7554/eLife.09560'} />
     <Socials emailUrl={''} facebookUrl={''} twitterUrl={''} linkedinUrl={''} redditUrl={''} />
+  </>),
+};
+
+export const ModalCite = Template.bind({});
+ModalCite.args = {
+  modalTitle: 'Share this article',
+  children: (<>
+    <ReferenceList references={references} />
+    <div className="downloads-container">
+      <div className="form-item">
+        <button className='button'>Copy to clipboard</button>
+        <button className='button button--action'>Download BIBTEX</button>
+        <button className='button button--action'>Download .RIS</button>
+      </div>
+    </div>
+
   </>),
 };
