ce6d4e9e6b6adc83fd3f573e7fae1ea0c561999d
Will Byrne <will.alex.byrne@gmail.com>
2023-01-17 15:24:29 +0000

    add more tests with mocks for clipboard.tsx

--- src/components/atoms/clipboard/clipboard.test.tsx
+++ src/components/atoms/clipboard/clipboard.test.tsx
@@ -1,27 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Clipboard } from './clipboard';
 
 describe('Clipboard', () => {
-  it('renders with the default button text', () => {
-    Object.defineProperty(global.navigator, 'clipboard', {
-      value: {
-        writeText: jest.fn().mockResolvedValue(true),
-      },
+  describe('navigation supported', () => {
+    const writeTextMock: any = jest.fn().mockResolvedValue(true);
+
+    beforeAll(() => {
+      Object.defineProperty(global.navigator, 'clipboard', {
+        value: {
+          writeText: writeTextMock,
+        },
+        configurable: true,
+      });
     });
 
-    render(<Clipboard text='will be copied to clipboard' />);
-    expect(screen.getByText('Copy to clipboard')).toBeInTheDocument();
+    it('renders with the default button text', () => {
+      render(<Clipboard text='will be copied to clipboard'/>);
+      expect(screen.getByText('Copy to clipboard')).toBeInTheDocument();
+    });
+
+    it('calls the navigation clipboard write text api when the button is clicked', () => {
+      const clipboardText = 'will be copied to clipboard';
+      render(<Clipboard text={clipboardText}/>);
+
+      expect(writeTextMock).toBeCalledTimes(0);
+
+      fireEvent.click(screen.getByText('Copy to clipboard'));
+
+      expect(writeTextMock).toBeCalledTimes(1);
+      expect(writeTextMock).toBeCalledWith(clipboardText);
+    });
+
+    it('renders the button text when passed in', () => {
+      const buttonText = 'I am a button';
+      render(<Clipboard text='will be copied to clipboard' buttonText={buttonText}/>);
+
+      expect(screen.queryByText('Copy to clipboard')).not.toBeInTheDocument();
+      expect(screen.getByText(buttonText)).toBeInTheDocument();
+    });
   });
 
-  it.only('renders with the default button text', () => {
-    const writeTextMock = jest.fn().mockResolvedValue(true);
-    Object.defineProperty(global.navigator, 'clipboard', {
-      value: {
-        writeText: writeTextMock,
-      },
-    });
+  describe('navigation not supported', () => {
+    it('does not render the button if clipboard is not supported', () => {
+      Object.defineProperty(global.navigator, 'clipboard', {
+        value: false,
+        configurable: true,
+      });
+      render(<Clipboard text='will be copied to clipboard'/>);
 
-    render(<Clipboard text='will be copied to clipboard' />);
-    expect(writeTextMock).toBeCalled();
+      expect(screen.queryByText('Copy to clipboard')).not.toBeInTheDocument();
+    });
   });
 });
