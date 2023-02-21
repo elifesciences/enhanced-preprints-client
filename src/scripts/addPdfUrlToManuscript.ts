d642bdc4652082fbdcfe79e34b9ab1dbc7807b08
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-02-21 12:34:12 +0000

    Add pdf url to manuscript (#401)

---
+++ src/scripts/addPdfUrlToManuscript.ts
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+import * as yargs from 'yargs';
+
+interface Args {
+  doi: string;
+  urlPdf: string;
+}
+
+const args = yargs
+  .option('doi', {
+    type: 'string',
+    describe: 'Preprint DOI',
+    demandOption: true,
+  })
+  .option('urlPdf', {
+    type: 'string',
+    describe: 'PDF location (url)',
+    demandOption: true,
+  })
+  .argv as Args;
+
+type Preprint = {
+  preprintDoi: string;
+  status: {
+    articleType: string;
+    status: string;
+    timeline: {
+      name: string;
+      date: string;
+      link?: {
+        url: string;
+        text: string;
+      }
+    }[]
+  };
+  pdfUrl?: string;
+  msas?: string[];
+};
+
+type Preprints = {
+  [preprintDoi: string]: Preprint;
+};
+
+type PreprintManuscripts = {
+  preprints: Preprints,
+};
+
+const addPdfUrlToManuscript = (preprintManuscripts: PreprintManuscripts, preprintDoi: string, pdfUrl: string) : void => {
+  if (!(preprintDoi in preprintManuscripts.preprints)) {
+    throw new Error(`doi not found in manuscripts.json (${preprintDoi})`);
+  }
+
+  const updatedData: PreprintManuscripts = preprintManuscripts;
+
+  updatedData.preprints[preprintDoi] = {
+    ...preprintManuscripts.preprints[preprintDoi],
+    ...{ pdfUrl },
+  };
+
+  process.stdout.write(`${JSON.stringify(updatedData, null, 2)}\n`);
+};
+
+let input = '';
+
+if (!process.stdin.isTTY) {
+  process.stdin.on('data', (chunk) => {
+    input += chunk;
+  });
+
+  process.stdin.on('end', () => {
+    addPdfUrlToManuscript(JSON.parse(input), args.doi, args.urlPdf);
+  });
+}
+
+export {};
