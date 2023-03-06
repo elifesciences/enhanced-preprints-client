30093527337e602631ceddc7cbd754075f45d49e
Dave Price <d.price@elifesciences.org>
2023-03-06 11:55:15 +0000

    Add initial tests for email corresponding authors

--- src/components/atoms/authors/authors.test.tsx
+++ src/components/atoms/authors/authors.test.tsx
@@ -1,45 +1,31 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { authors } from '../../../utils/mocks';
 import { Authors } from './authors';
-
-const authorList = [
-  { givenNames: ['Steve'], familyNames: ['Rogers'] },
-  { givenNames: ['Antony'], familyNames: ['Stark'] },
-  { givenNames: ['Natasha'], familyNames: ['Romanov'] },
-  { givenNames: ['Bruce'], familyNames: ['Banner'] },
-  { givenNames: ['Wanda'], familyNames: ['Maximof'] },
-  { givenNames: ['Bucky'], familyNames: ['Barnes'] },
-  { givenNames: ['Barry'], familyNames: ['Allen'] },
-  { givenNames: ['Jesse'], familyNames: ['Quick'] },
-  { givenNames: ['Kara'], familyNames: ['Zor-el'] },
-  { givenNames: ['Arthur'], familyNames: ['Curry'] },
-  { givenNames: ['Kal'], familyNames: ['El'] },
-  { givenNames: ['Oliver'], familyNames: ['Queen'] },
-];
 
 describe('authors', () => {
   it('should render correctly a list of authors', () => {
-    render(<Authors authors={[authorList[0]]}/>);
+    render(<Authors authors={[authors[0]]}/>);
     const captainAmerica = screen.getByText('Steve Rogers');
 
     expect(captainAmerica).toBeInTheDocument();
   });
 
   it('should hide authors after the author limit', () => {
-    const { container } = render(<Authors authors={authorList}/>);
+    const { container } = render(<Authors authors={authors}/>);
 
     expect(container.getElementsByClassName('authors--collapsible')).toHaveLength(1);
     expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
   });
 
   it('shows a clickable expansion element when over the author limit', () => {
-    render(<Authors authors={authorList}/>);
+    render(<Authors authors={authors}/>);
     const expansionElement = screen.getByText('show', { exact: false });
 
     expect(expansionElement).toBeInTheDocument();
   });
 
   it('does not show a clickable expansion element when under the author limit', () => {
-    render(<Authors authors={[authorList[0]]}/>);
+    render(<Authors authors={[authors[0]]}/>);
     const expansionElement = screen.queryByText('show', { exact: false });
 
     expect(expansionElement).not.toBeInTheDocument();
@@ -47,7 +33,7 @@ describe('authors', () => {
 
   describe('expansion behaviour', () => {
     it('shows on click', () => {
-      const { container } = render(<Authors authors={authorList}/>);
+      const { container } = render(<Authors authors={authors}/>);
 
       expect(container.getElementsByClassName('authors--collapsible')).toHaveLength(1);
       expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
@@ -59,7 +45,52 @@ describe('authors', () => {
     });
 
     it('hides on click', () => {
-      const { container } = render(<Authors authors={authorList}/>);
+      const { container } = render(<Authors authors={authors}/>);
+
+      expect(container.getElementsByClassName('authors--collapsible')).toHaveLength(1);
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
+
+      const expansionElement = screen.getByText('show', { exact: false });
+      fireEvent.click(expansionElement);
+
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(1);
+
+      const contractionElement = screen.getByText('show less');
+      fireEvent.click(contractionElement);
+
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
+    });
+  });
+
+  describe('corresponding author', () => {
+    it('show email icon for corresponding authors', () => {
+      render(<Authors authors={authors}/>);
+
+      screen.debug();
+
+      expect(screen.getByText('Kara Zor-el')).toHaveClass('authors-email');
+      expect(screen.getByText('Kal El')).toHaveClass('authors-email');
+    });
+
+    it('does not show email icon for non corresponding authors', () => {
+      const { container } = render(<Authors authors={authors}/>);
+
+      expect(container.getElementsByClassName('authors--collapsible')).toHaveLength(1);
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
+
+      const expansionElement = screen.getByText('show', { exact: false });
+      fireEvent.click(expansionElement);
+
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(1);
+
+      const contractionElement = screen.getByText('show less');
+      fireEvent.click(contractionElement);
+
+      expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
+    });
+
+    it('shows accessibility span', () => {
+      const { container } = render(<Authors authors={authors}/>);
 
       expect(container.getElementsByClassName('authors--collapsible')).toHaveLength(1);
       expect(container.getElementsByClassName('authors-list--expanded')).toHaveLength(0);
