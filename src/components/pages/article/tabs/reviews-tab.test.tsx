590a488cc54e4f19a713b6b27647f594d34eac24
nlisgo <n.lisgo@elifesciences.org>
2023-03-08 10:31:21 +0000

    Add test for jump menu

--- src/components/pages/article/tabs/reviews-tab.test.tsx
+++ src/components/pages/article/tabs/reviews-tab.test.tsx
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { ArticleReviewsTab } from './reviews-tab';
 import { peerReview } from '../../../../utils/mocks';
+import { PeerReview } from '../../../../types';
 
 describe('ReviewsTab', () => {
+  // This could be useful for other tabs.
+  type ExpectedJumpLinksType = {
+    description: string,
+    peerReviewExample: PeerReview,
+    expectedJumpToLinks: {
+      href: string,
+      text: string,
+    }[],
+  };
+
   it('renders with reviews tab', () => {
     expect(() => render(<ArticleReviewsTab peerReview={peerReview} />)).not.toThrow();
   });
@@ -20,7 +31,81 @@ describe('ReviewsTab', () => {
     expect(screen.getByText(peerReview.authorResponse!.text)).toBeInTheDocument();
   });
 
-  it('jump to menu links match content', () => {
-    const {container} = render(<ArticleReviewsTab peerReview={peerReview} />);
+  it.each([
+    {
+      description: 'complete',
+      peerReviewExample: peerReview,
+      expectedJumpToLinks: [
+        {
+          href: '#editors-and-reviewers',
+          text: 'Editors',
+        },
+        {
+          href: '#peer-review-0',
+          text: 'Reviewer #1',
+        },
+        {
+          href: '#peer-review-1',
+          text: 'Reviewer #2',
+        },
+        {
+          href: '#author-response',
+          text: 'Author Response',
+        },
+      ],
+    },
+    {
+      description: 'without authorResponse',
+      peerReviewExample: {
+        evaluationSummary: peerReview.evaluationSummary,
+        reviews: peerReview.reviews,
+      },
+      expectedJumpToLinks: [
+        {
+          href: '#editors-and-reviewers',
+          text: 'Editors',
+        },
+        {
+          href: '#peer-review-0',
+          text: 'Reviewer #1',
+        },
+        {
+          href: '#peer-review-1',
+          text: 'Reviewer #2',
+        },
+      ],
+    },
+    {
+      description: 'without reviews',
+      peerReviewExample: {
+        evaluationSummary: peerReview.evaluationSummary,
+        reviews: [],
+      },
+      expectedJumpToLinks: [
+        {
+          href: '#editors-and-reviewers',
+          text: 'Editors',
+        },
+      ],
+    },
+  ])('jump to menu links match content ($description)', ({
+    peerReviewExample,
+    expectedJumpToLinks,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    description,
+  }: ExpectedJumpLinksType) => {
+    const { container } = render(<ArticleReviewsTab peerReview={peerReviewExample} />);
+    const jumpLinks = container.querySelectorAll('.jump-menu-list__link');
+
+    expect(jumpLinks).toHaveLength(expectedJumpToLinks.length);
+
+    const jumpLinkValues = Array.from(jumpLinks).map((link: Element) => (
+      {
+        href: link.getAttribute('href')!,
+        text: link.textContent!,
+      }
+    ));
+
+    expect(jumpLinkValues).toStrictEqual(expectedJumpToLinks);
   });
 });
