f21bb3980cc65bd5e7c3feccd3e3868001f2769b
Dave Price <d.price@elifesciences.org>
2023-03-09 08:52:28 +0000

    add anchor link and classes for author link

--- src/components/atoms/authors/authors.tsx
+++ src/components/atoms/authors/authors.tsx
@@ -20,7 +20,9 @@ export const Authors = ({ authors }: { authors: Author[] }): JSX.Element => {
       <ol className={`authors-list${expanded ? ' authors-list--expanded' : ''}`} aria-label="Authors of this article">
         { authors.map(({ givenNames, familyNames, emails }, index) => (
           <li className="authors-list__item" key={index}>
-            <span className={`${emails ? 'authors-email__link' : ''}`}>{givenNames?.join(' ')} {familyNames?.join(' ')} {emails ? <span className="visuallyhidden">author has email address</span> : ''}</span>
+            <a href="#anchor-id-to-go-here" className={`authors-link ${emails ? 'authors-email__link' : ''}`}>
+              {givenNames?.join(' ')} {familyNames?.join(' ')} {emails ? <span className="visuallyhidden">author has email address</span> : ''}
+            </a>
           </li>
         ))}
       </ol>
