ffea22c75411978a8ef6499a13aa9fb475b4eeab
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-03-31 11:06:07 +0100

    Replaced input with external service link

--- src/components/atoms/socials/socials.tsx
+++ src/components/atoms/socials/socials.tsx
@@ -1,17 +1,13 @@
 import './socials.scss';
-import { useState } from 'react';
 
-type Props = {
+type SocialsProps = {
   doi: string,
   title: string
 };
 
 export const Socials = ({
   doi, title,
-}: Props): JSX.Element => {
-  const [showMastodonInputs, setShowMastodonInputs] = useState(false);
-  const [mastodonAddress, setMastodonAddress] = useState<string>();
-
+}: SocialsProps): JSX.Element => {
   const doiUrl = `https://doi.org/${doi}`;
   const encodedTitle = encodeURIComponent(title);
   const encodedUrl = encodeURIComponent(doiUrl);
@@ -20,55 +16,40 @@ export const Socials = ({
   const facebookUrl = `https://facebook.com/sharer/sharer.php?u=${encodedUrl}`;
   const linkedinUrl = `https://www.linkedin.com/shareArticle?title=${encodedTitle}&url=${encodedUrl}`;
   const redditUrl = `https://reddit.com/submit/?title=${encodedTitle}&url=${encodedUrl}`;
-  const mastodonUrl = `https://${mastodonAddress}/share?text=${encodedTitle}%20${encodedUrl}}`;
+  const mastodonUrl = `https://toot.kytta.dev/?text=${encodedTitle}%20${encodedUrl}`;
 
   return (
-    <div className="socials-container">
-      <ul className="socials-sharers">
-        <li>
-          <a className="socials-sharer email" href={emailUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Email">
-            Email
-          </a>
-        </li>
-        <li>
-          <a className="socials-sharer twitter" href={twitterUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Twitter">
-            Twitter
-          </a>
-        </li>
-        <li>
-          <a className="socials-sharer facebook" href={facebookUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Facebook">
-            Facebook
-          </a>
-        </li>
-        <li>
-          <a className="socials-sharer linkedin" href={linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by LinkedIn">
-            LinkedIn
-          </a>
-        </li>
-        <li>
-          <a className="socials-sharer reddit" href={redditUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Reddit">
-            Reddit
-          </a>
-        </li>
-        <li>
-          <a className="socials-sharer mastadon" target="_blank" rel="noopener noreferrer" aria-label="Share by Mastadon with URL input" onClick={() => setShowMastodonInputs(!showMastodonInputs)}>
-            Mastadon
-          </a>
-        </li>
-      </ul>
-      {showMastodonInputs &&
-        <div className="socials-mastadon">
-          <input className="socials-mastadon__input" placeholder="Mastadon URL" defaultValue={mastodonAddress ?? ''} onInput={(e) => setMastodonAddress(e.currentTarget.value)} />
-          <a
-            className="button button--action socials-mastadon__action"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={mastodonUrl}
-            aria-label="Share by Mastodon">
-            Share
-          </a>
-        </div>
-      }
-    </div>
+    <ul className="socials-sharers">
+      <li>
+        <a className="socials-sharer email" href={emailUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Email">
+          Email
+        </a>
+      </li>
+      <li>
+        <a className="socials-sharer twitter" href={twitterUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Twitter">
+          Twitter
+        </a>
+      </li>
+      <li>
+        <a className="socials-sharer facebook" href={facebookUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Facebook">
+          Facebook
+        </a>
+      </li>
+      <li>
+        <a className="socials-sharer linkedin" href={linkedinUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by LinkedIn">
+          LinkedIn
+        </a>
+      </li>
+      <li>
+        <a className="socials-sharer reddit" href={redditUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Reddit">
+          Reddit
+        </a>
+      </li>
+      <li>
+        <a className="socials-sharer mastadon" href={mastodonUrl} target="_blank" rel="noopener noreferrer" aria-label="Share by Mastadon via Toot">
+          Mastadon
+        </a>
+      </li>
+    </ul>
   );
 };
