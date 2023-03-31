0190122a6f787710305a5bd7c5bd3c2de1b1fc1d
FAtherden-eLife <f.atherden@elifesciences.org>
2023-03-31 09:32:31 +0100

    linting

--- src/components/atoms/abstract/abstract.tsx
+++ src/components/atoms/abstract/abstract.tsx
@@ -3,12 +3,12 @@ import { contentToJsx } from '../../../utils/content-to-jsx';
 import { Content } from '../../../types';
 
 const abstractHasNoAltHeading = (content: Content): boolean => {
-  const firstItem = Array.isArray(content) ? content[0]: content;
+  const firstItem = Array.isArray(content) ? content[0] : content;
   if (typeof firstItem === 'object' && firstItem !== null && 'type' in firstItem) {
     return firstItem.type !== 'Heading';
   }
   return true;
-}
+};
 
 export const Abstract = ({ content }: { content: Content }): JSX.Element => (
   <section id="abstract" className="abstract">
