43bdcb9f30139c7b3a67e2c9b479f5ed4d9bae14
FAtherden-eLife <f.atherden@elifesciences.org>
2023-03-31 08:32:23 +0100

    update abstract test

--- src/components/atoms/abstract/abstract.test.tsx
+++ src/components/atoms/abstract/abstract.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { Abstract } from './abstract';
+import { Content } from '../../../types';
 
 describe('ArticleContent', () => {
   it('renders with a simple string content', async () => {
@@ -20,5 +21,21 @@ describe('ArticleContent', () => {
     render(<Abstract content={['I ', 'am ', 'an ', 'array ']}/>);
     expect(screen.getByText('I am an array', { exact: false })).toBeInTheDocument();
     expect(screen.getByText('I am an array').tagName).toStrictEqual('SECTION');
+  });
+
+  it('renders with a different title than `Abstract`', async () => {
+    const complexContent: Content = [
+      { type: 'Heading', depth: 1, id: 'abstract1', content: ['Summary']},
+      { type: 'Paragraph', content: ['This is the text for the summary.']}
+    ];
+
+    render(<Abstract content={complexContent}/>);
+
+    const abstractHeading = screen.queryByText('Abstract');
+    expect(abstractHeading).toBeNull();
+    expect(screen.getByText('Summary')).toBeInTheDocument();
+    expect(screen.getByText('Summary').tagName).toStrictEqual('H1');
+    expect(screen.getByText('This is the text for the summary.',{ exact: false })).toBeInTheDocument();
+    expect(screen.getByText('This is the text for the summary.').tagName).toStrictEqual('P');
   });
 });
