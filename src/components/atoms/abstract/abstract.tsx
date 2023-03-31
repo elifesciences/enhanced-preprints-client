0ca86e70a8c9abf9fcd9a82a9167aab3de8e4075
FAtherden-eLife <f.atherden@elifesciences.org>
2023-03-31 09:29:18 +0100

    Handle alternative headings in abstracts

--- src/components/atoms/abstract/abstract.tsx
+++ src/components/atoms/abstract/abstract.tsx
@@ -2,9 +2,17 @@ import './abstract.scss';
 import { contentToJsx } from '../../../utils/content-to-jsx';
 import { Content } from '../../../types';
 
+const abstractHasNoAltHeading = (content: Content): boolean => {
+  const firstItem = Array.isArray(content) ? content[0]: content;
+  if (typeof firstItem === 'object' && firstItem !== null && 'type' in firstItem) {
+    return firstItem.type !== 'Heading';
+  }
+  return true;
+}
+
 export const Abstract = ({ content }: { content: Content }): JSX.Element => (
-  <section className="abstract">
-    <h1 id="abstract">Abstract</h1>
+  <section id="abstract" className="abstract">
+    {abstractHasNoAltHeading(content) && <h1>Abstract</h1>}
     {contentToJsx(content)}
   </section>
 );
