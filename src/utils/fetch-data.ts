7a288b2db0ce5938e2eecf222744f56469c3e5af
Scott Aubrey <scott@aubrey.org.uk>
2023-04-14 09:19:01 +0100

    Allow reviews JSON to be overridden with a local json file, and implement for ms 85111

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -1,8 +1,20 @@
+import { readFileSync } from 'fs';
 import { config } from '../config';
 import { jsonFetch } from './json-fetch';
 import { Content } from '../types/content';
 import { MetaData, PeerReview } from '../types';
 
+type ReviewsJson = {
+  [index: string]: PeerReview;
+};
+
+const reviewsJson = JSON.parse(readFileSync('reviews.json').toString()) as ReviewsJson;
+
 export const fetchMetadata = (id: string) => jsonFetch<MetaData>(`${config.apiServer}/api/reviewed-preprints/${id}/metadata`);
 export const fetchContent = (id: string) => jsonFetch<Content>(`${config.apiServer}/api/reviewed-preprints/${id}/content`);
-export const fetchReviews = (id: string) => jsonFetch<PeerReview>(`${config.apiServer}/api/reviewed-preprints/${id}/reviews`);
+export const fetchReviews = (id: string) => {
+  if (reviewsJson[id]) {
+    return reviewsJson[id];
+  }
+  return jsonFetch<PeerReview>(`${config.apiServer}/api/reviewed-preprints/${id}/reviews`);
+};
