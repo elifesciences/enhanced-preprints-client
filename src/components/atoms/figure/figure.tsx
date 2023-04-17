efa698282bab0297c855597a49b6937aba809844
Dave Price <d.price@elifesciences.org>
2023-04-17 16:30:51 +0100

    Add line-clamp to fig caption

--- src/components/atoms/figure/figure.tsx
+++ src/components/atoms/figure/figure.tsx
@@ -6,6 +6,6 @@ export const Figure = ({ content }: { content: FigureContent }) => (
   <figure className="figure" id={content.id}>
     <label className="figure__label">{content.label}</label>
     {contentToJsx(content.content)}
-    <figcaption className="figure__caption">{contentToJsx(content.caption)}</figcaption>
+    <figcaption className="figure__caption prototype">{contentToJsx(content.caption)}</figcaption>
   </figure>
 );
