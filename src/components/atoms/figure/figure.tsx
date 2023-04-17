b040ec7038160c5df7d2d5b65ef598fac193561c
nlisgo <n.lisgo@elifesciences.org>
2023-04-17 17:01:19 +0100

    Progress with the client javascript implementation

--- src/components/atoms/figure/figure.tsx
+++ src/components/atoms/figure/figure.tsx
@@ -1,11 +1,17 @@
+import {useMemo, useRef, useState} from 'react';
 import { contentToJsx } from '../../../utils/content-to-jsx';
 import './figure.scss';
 import { FigureContent } from '../../../types';
 
-export const Figure = ({ content }: { content: FigureContent }) => (
-  <figure className="figure" id={content.id}>
-    <label className="figure__label">{content.label}</label>
-    {contentToJsx(content.content)}
-    <figcaption className="figure__caption prototype">{contentToJsx(content.caption)}</figcaption>
-  </figure>
-);
+export const Figure = ({ content }: { content: FigureContent }) => {
+  const captionRef = useRef<HTMLElement>(null);
+  const expansionMemo = useMemo(() => captionRef.current && captionRef.current.offsetHeight < captionRef.current.scrollHeight, [captionRef.current?.scrollHeight]);
+  const [expanded, setExpanded] = useState(expansionMemo ?? false);
+  return (
+    <figure className="figure" id={content.id}>, setExpanded
+      <label className="figure__label">{content.label}</label>
+      {contentToJsx(content.content)}
+      <figcaption ref={captionRef} className={`figure__caption${expanded && ' prototype'}`}>{contentToJsx(content.caption)}</figcaption>
+    </figure>
+  );
+};
