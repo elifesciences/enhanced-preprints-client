0e9b1ba892cca95bb2a80c829e58b1f6eabafdd1
Scott Aubrey <scott@aubrey.org.uk>
2023-04-19 13:16:13 +0100

    reimplement darkmode addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "storybook-dark-mode"],
   "framework": {
     name: "@storybook/nextjs",
     // Add this
