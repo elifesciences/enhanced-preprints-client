8ccbe1f3def4e42eda4ca8382f9d8eb0b917fa52
Scott Aubrey <scott@aubrey.org.uk>
2023-04-19 14:54:07 +0100

    tidy up diff

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,29 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "storybook-dark-mode"],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "storybook-dark-mode",
+  ],
   "framework": {
     name: "@storybook/nextjs",
     // Add this
     options: {}
   },
-  "webpackFinal": async config => {
+  "webpackFinal": async (config) => {
     config.resolve = {
       ...config.resolve,
       fallback: {
         ...(config.resolve || {}).fallback,
         fs: false,
         stream: false,
-        os: false
-      }
+        os: false,
+      },
     };
 
     // Return the altered config
