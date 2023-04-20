eb2a48c19f8b29d2c2769e5d83e5826b5a3b95d6
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-04-20 14:17:37 +0100

    Another go at institutions using useMemo

--- src/components/atoms/institutions/institutions.tsx
+++ src/components/atoms/institutions/institutions.tsx
@@ -1,37 +1,41 @@
-import { useLayoutEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import './institutions.scss';
 import { Institution } from '../../../types';
 
 const institutionLimit = 3;
 
 export const Institutions = ({ institutions }: { institutions: Institution[] }): JSX.Element => {
-  const [expanded, setExpanded] = useState<boolean>(false);
+  const [expanded, setExpanded] = useState<boolean | null>(null);
 
-  useLayoutEffect(() => setExpanded(false), []);
+  useMemo(() => setExpanded(false), []);
+
   const displayInstitutions = useMemo(() => institutions.slice(0, expanded !== false ? institutions.length : institutionLimit), [expanded]);
   const expansionText = useMemo(() => (expanded ? 'show less' : `show ${institutions.length - institutionLimit} more`), [expanded]);
 
   return (
     <div className="institutions">
-      <script type="text/javascript">
-        <ol className="institutions-list" aria-label="Author institutions">
-          { displayInstitutions.map(({ name, address }, index) => (
-            <li className="institutions-list__item" key={index}>
-              {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
-            </li>
-          ))}
-        </ol>
-        {(institutions.length > institutionLimit) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
-      </script>
+      {expanded !== null && (
+        <>
+          <ol className="institutions-list" aria-label="Author institutions">
+            { displayInstitutions.map(({ name, address }, index) => (
+              <li className="institutions-list__item" key={index}>
+                {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
+              </li>
+            ))}
+          </ol>
+          {(institutions.length > institutionLimit) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
+        </>
+      )}
       <noscript>
         <ol className="institutions-list" aria-label="Author institutions">
-          { institutions.map(({ name, address }, index) => (
+          {institutions.map(({ name, address }, index) => (
             <li className="institutions-list__item" key={index}>
               {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
             </li>
           ))}
         </ol>
       </noscript>
+
     </div>
   );
 };
