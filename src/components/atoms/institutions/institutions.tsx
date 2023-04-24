e3e6b4f9b6f8a93c51d4e828637a508a83b9e516
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-04-24 10:28:47 +0100

    Added new method to check whether JS is active

--- src/components/atoms/institutions/institutions.tsx
+++ src/components/atoms/institutions/institutions.tsx
@@ -1,26 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useMemo, useState } from 'react';
 import './institutions.scss';
 import { Institution } from '../../../types';
 
 const institutionLimit = 3;
 
 export const Institutions = ({ institutions }: { institutions: Institution[] }): JSX.Element => {
-  const [expanded, setExpanded] = useState<boolean | null>(null);
+  const [expanded, setExpanded] = useState<boolean>(false);
 
-  useEffect(() => setExpanded(false), []);
-  const displayInstitutions = institutions.slice(0, expanded !== false ? institutions.length : institutionLimit);
-  const expansionText = expanded ? 'show less' : `show ${institutions.length - institutionLimit} more`;
+  const displayInstitutions = useMemo(() => institutions.slice(0, expanded !== false ? institutions.length : institutionLimit), [expanded]);
+  const expansionText = useMemo(() => (expanded ? 'show less' : `show ${institutions.length - institutionLimit} more`), [expanded]);
 
   return (
     <div className="institutions">
-      <ol className="institutions-list" aria-label="Author institutions">
-        { displayInstitutions.map(({ name, address }, index) => (
-          <li className="institutions-list__item" key={index}>
-            {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
-          </li>
-        ))}
-      </ol>
-      {(institutions.length > institutionLimit && expanded !== null) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
+      {typeof window !== 'undefined' ?
+        <>
+          <ol className="institutions-list" aria-label="Author institutions">
+            { displayInstitutions.map(({ name, address }, index) => (
+              <li className="institutions-list__item" key={index}>
+                {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
+              </li>
+            ))}
+          </ol>
+          {(institutions.length > institutionLimit && expanded !== null) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
+        </>
+        :
+        <ol className="institutions-list" aria-label="Author institutions">
+          {institutions.map(({ name, address }, index) => (
+            <li className="institutions-list__item" key={index}>
+              {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
+            </li>
+          ))}
+        </ol>
+      }
     </div>
   );
 };
