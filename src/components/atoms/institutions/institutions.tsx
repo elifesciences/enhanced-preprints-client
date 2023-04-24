4069b2996145f4fb28ca7f6d46817baf51f47921
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-04-24 10:30:36 +0100

    Removed institutions change, branch should only be static generation

--- src/components/atoms/institutions/institutions.tsx
+++ src/components/atoms/institutions/institutions.tsx
@@ -1,37 +1,26 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './institutions.scss';
 import { Institution } from '../../../types';
 
 const institutionLimit = 3;
 
 export const Institutions = ({ institutions }: { institutions: Institution[] }): JSX.Element => {
-  const [expanded, setExpanded] = useState<boolean>(false);
+  const [expanded, setExpanded] = useState<boolean | null>(null);
 
-  const displayInstitutions = useMemo(() => institutions.slice(0, expanded !== false ? institutions.length : institutionLimit), [expanded]);
-  const expansionText = useMemo(() => (expanded ? 'show less' : `show ${institutions.length - institutionLimit} more`), [expanded]);
+  useEffect(() => setExpanded(false), []);
+  const displayInstitutions = institutions.slice(0, expanded !== false ? institutions.length : institutionLimit);
+  const expansionText = expanded ? 'show less' : `show ${institutions.length - institutionLimit} more`;
 
   return (
     <div className="institutions">
-      {typeof window !== 'undefined' ?
-        <>
-          <ol className="institutions-list" aria-label="Author institutions">
-            { displayInstitutions.map(({ name, address }, index) => (
-              <li className="institutions-list__item" key={index}>
-                {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
-              </li>
-            ))}
-          </ol>
-          {(institutions.length > institutionLimit && expanded !== null) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
-        </>
-        :
-        <ol className="institutions-list" aria-label="Author institutions">
-          {institutions.map(({ name, address }, index) => (
-            <li className="institutions-list__item" key={index}>
-              {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
-            </li>
-          ))}
-        </ol>
-      }
+      <ol className="institutions-list" aria-label="Author institutions">
+        { displayInstitutions.map(({ name, address }, index) => (
+          <li className="institutions-list__item" key={index}>
+            {name}{ address ? <address className="institution__address">{address.addressCountry ?? ''}</address> : ''}
+          </li>
+        ))}
+      </ol>
+      {(institutions.length > institutionLimit && expanded !== null) && <span className="institutions-list__expansion" onClick={() => setExpanded(!expanded)}>{expansionText}</span>}
     </div>
   );
 };
