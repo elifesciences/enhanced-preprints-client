6254b4781074b518b0221b03c1de2f7490a0b8b7
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-04-26 14:40:19 +0100

    Removed useEffect from Authors

--- src/components/atoms/authors/authors.tsx
+++ src/components/atoms/authors/authors.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './authors.scss';
 import { Author } from '../../../types';
 import { createAuthorId } from '../../../utils/create-author-id';
@@ -7,15 +7,13 @@ const authorLimit = 3;
 const authorLimits = [authorLimit, 10];
 
 export const Authors = ({ authors }: { authors: Author[] }): JSX.Element => {
-  const [expanded, setExpanded] = useState<boolean | null>(null);
+  const [expanded, setExpanded] = useState(false);
 
-  useEffect(() => setExpanded(false), []);
-
-  return (
+  return typeof window !== undefined ? (
     <div className={
       `
-      authors${authors.length > authorLimit && expanded !== null ? ' authors--collapsible' : ''}
-      ${expanded !== null ? authorLimits.map((limit) => (limit < authors.length ? ` authors--limit-${limit}` : '')).join(' ') : ''}
+      authors${authors.length > authorLimit ? ' authors--collapsible' : ''}
+      ${authorLimits.map((limit) => (limit < authors.length ? ` authors--limit-${limit}` : '')).join(' ')}
       `
     }>
       <ol className={`authors-list${expanded ? ' authors-list--expanded' : ''}`} aria-label="Authors of this article">
@@ -32,6 +30,18 @@ export const Authors = ({ authors }: { authors: Author[] }): JSX.Element => {
           show{!expanded ? authorLimits.map(
           (limit, index) => <span key={index} aria-hidden="true" className={`authors-list__expansion-count authors-list__expansion-count-${limit}`}> {authors.length - limit}</span>,
         ) : ''} {expanded ? 'less' : 'more'}</span>}
+    </div>
+  ) : (
+    <div className="authors">
+      <ol className="authors-list" aria-label="Authors of this article">
+        { authors.map((author, index) => (
+          <li className="authors-list__item" key={index}>
+            <a href={`#${createAuthorId(author)}`} className={`authors-link${author.emails ? ' authors-email__link' : ''}`}>
+              {author.givenNames?.join(' ')} {author.familyNames?.join(' ')}{author.emails ? <span className="visuallyhidden"> author has email address</span> : ''}
+            </a>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
