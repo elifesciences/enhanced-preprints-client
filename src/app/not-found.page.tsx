75dd5930aadcb3cedfe4992de705758cc9041fd9
Scott Aubrey <scott@aubrey.org.uk>
2023-05-16 09:21:19 +0100

    remove test code

--- src/app/not-found.page.tsx
+++ src/app/not-found.page.tsx
@@ -1,5 +1,5 @@
 import { PageNotFound } from '../components/pages/error-pages/page-not-found';
 
-export const MissingPage = (): JSX.Element => <><PageNotFound />boo</>;
+export const MissingPage = (): JSX.Element => <PageNotFound />;
 
 export default MissingPage;
