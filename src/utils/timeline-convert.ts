1db7c89c913eb706606fc9debffaacb0485a1b5e
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-05-26 10:15:09 +0100

    Added timeline-convert file

---
+++ src/utils/timeline-convert.ts
@@ -0,0 +1,12 @@
+import { TimelineEvent } from '../components/molecules/timeline/timeline';
+import { Timeline } from '../types/enhanced-article';
+
+export const convertTimeline = (events: Timeline[]) => events.sort((a, b) => b.date.getTime() - a.date.getTime())
+  .map<TimelineEvent>((event: Timeline) => ({
+  name: event.name,
+  date: event.date.toDateString(),
+  link: event.url ? {
+    text: 'Link',
+    url: event.url,
+  } : undefined,
+}));
