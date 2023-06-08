7763b1075e77fd1b8f8957c008576ceb662864a2
Elliot Jordan Kemp <elliotjordankemp@Elliots-MacBook-Pro.local>
2023-06-08 16:34:30 +0100

    Type import hotfix

--- src/types/meta-data.ts
+++ src/types/meta-data.ts
@@ -3,6 +3,7 @@ import { Author } from './author';
 import { Reference } from './reference';
 import { PeerReview } from './peer-review';
 import { TimelineEvent } from '../components/molecules/timeline/timeline';
+import { Heading } from './enhanced-article';
 
 export type MetaData = {
   abstract: Content;
