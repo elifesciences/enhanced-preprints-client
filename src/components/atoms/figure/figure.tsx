9d8ebb021d46517783c8855357ba39415475faf4
Dave Price <d.price@elifesciences.org>
2023-06-21 16:21:14 +0100

    refactor fig-captions

--- src/components/atoms/figure/figure.tsx
+++ src/components/atoms/figure/figure.tsx
@@ -8,13 +8,13 @@ export const Figure = ({ content }: { content: FigureContent }) => {
   const expansionMemo = useMemo(() => captionRef.current && captionRef.current.offsetHeight < captionRef.current.scrollHeight, [captionRef.current?.scrollHeight]);
   const [expanded, setExpanded] = useState(expansionMemo ?? false);
   return (
-    <div>
+    <>
       <figure className="figure" id={content.id}>
         <label className="figure__label">{content.label}</label>
         {contentToJsx(content.content)}
         <figcaption ref={captionRef} className={`figure__caption${expanded ? '--expanded' : ''}`}>{contentToJsx(content.caption)}</figcaption>
       </figure>
       <button className={`figure__caption__button ${expanded ? 'expanded' : ''}`} onClick={() => { setExpanded(!expanded); }}>{expanded ? 'Show less' : 'Show more'}</button>
-    </div>
+    </>
   );
 };
