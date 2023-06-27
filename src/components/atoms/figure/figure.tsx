d2e4c89138904cbeda0065892d1c9b234a98b0da
nlisgo <n.lisgo@elifesciences.org>
2023-06-27 23:02:48 +0100

    Figure id should be optional

--- src/components/atoms/figure/figure.tsx
+++ src/components/atoms/figure/figure.tsx
@@ -3,7 +3,7 @@ import './figure.scss';
 import { FigureContent } from '../../../types';
 
 export const Figure = ({ content }: { content: FigureContent }) => (
-  <figure className="figure" id={content.id}>
+  <figure className="figure" {...(content.id && { id: content.id })}>
     {content.label && <label className="figure__label">{content.label}</label>}
     {contentToJsx(content.content)}
     {content.caption && <figcaption className="figure__caption">{contentToJsx(content.caption)}</figcaption>}
