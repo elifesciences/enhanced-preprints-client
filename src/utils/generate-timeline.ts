508262c04e9b4116330bca80b108a26aa57b00f0
Scott Aubrey <scott@aubrey.org.uk>
2023-06-30 09:01:35 +0100

    fix generated timeline to not have duplicates

--- src/utils/generate-timeline.ts
+++ src/utils/generate-timeline.ts
@@ -3,12 +3,8 @@ import { TimelineEvent } from '../components/molecules/timeline/timeline';
 
 export const generateTimeline = (version: EnhancedArticleWithVersions): TimelineEvent[] => {
   // Extra sort here the top to ascertain the first version for getting dates
-  const versions = [...Object.values(version.versions), version.article].sort((a, b) => new Date(b.published).getTime() - new Date(a.published).getTime());
-  const timeline: TimelineEvent[] = versions.length === 1 ? [{
-    date: new Date(version.article.published).toDateString(),
-    name: 'Reviewed preprint posted',
-    eventDescription: '(this version)',
-  }] : versions.map<TimelineEvent>((current) => (current.id === version.article.id ?
+  const versions = Object.values(version.versions).sort((a, b) => new Date(b.published).getTime() - new Date(a.published).getTime());
+  const timeline: TimelineEvent[] = versions.map<TimelineEvent>((current) => (current.id === version.article.id ?
     {
       date: new Date(current.published).toDateString(),
       name: `Reviewed preprint version ${current.versionIdentifier}`,
