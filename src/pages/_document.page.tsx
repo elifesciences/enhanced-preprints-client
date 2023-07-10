19553eb2b4d263d1adaf979c22c56388cd51319a
Scott Aubrey <scott@aubrey.org.uk>
2023-07-10 12:52:38 +0100

    move next/script tags to _app because it is not supported in next/head, and breaks if outside next/next in MyApp

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -4,7 +4,6 @@ import {
   Main,
   NextScript,
 } from 'next/document';
-import Script from 'next/script';
 import { config } from '../config';
 
 export default function Document() {
@@ -26,20 +25,6 @@ export default function Document() {
           }
           `}
         </style>
-        { config.cookiebotId &&
-          <Script id="Cookiebot"
-            src="https://consent.cookiebot.com/uc.js"
-            data-cbid={config.cookiebotId}></Script>
-        }
-        { config.gtmId &&
-          <Script id="GTM" dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${config.gtmId}');`,
-          }}></Script>
-        }
       </Head>
       <body>
         { config.gtmId &&
