a173d824219af5d1f1d6be24b842d6fa1e57c320
Scott Aubrey <scott@aubrey.org.uk>
2023-07-24 10:30:43 +0100

    Remove react-scripts from our repo (#920)
    * remove react-scripts from our repo
    
    * fix issue with type in build
    
    * tidy up commented example jest config
    
    * ignore new jest config file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'; // eslint-disable-line import/no-extraneous-dependencies
+import '@testing-library/jest-dom/extend-expect'; // eslint-disable-line import/no-extraneous-dependencies
+import { TextEncoder } from 'util';
+
+global.TextEncoder = TextEncoder;
