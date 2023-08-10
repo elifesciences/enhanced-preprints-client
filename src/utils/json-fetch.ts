ed08b00a1b2f531b6709c847b12de6adf7c5e5d8
Nathan Lisgo <nathan@lisgo.co.uk>
2023-08-10 08:47:49 +0100

    Revert "fix: add error handling to jsonFetch utility"
    This reverts commit f64a26e3b080e8f6e5613cc248e8c3d581b2c125.

--- src/utils/json-fetch.ts
+++ src/utils/json-fetch.ts
@@ -1,12 +1,7 @@
-export const jsonFetch = <T>(url: string): Promise<T | null> => fetch(url)
+export const jsonFetch = <T>(url: string): Promise<T> => fetch(url)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`error fetching (${url}): ${response.statusText}`);
     }
     return response.json() as Promise<T>;
-  })
-  .catch((error) => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    return null;
   });
