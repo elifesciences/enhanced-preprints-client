f64a26e3b080e8f6e5613cc248e8c3d581b2c125
Nathan Lisgo <nathan@lisgo.co.uk>
2023-08-10 08:38:29 +0100

    fix: add error handling to jsonFetch utility
    This commit introduces error handling to the `jsonFetch` utility function. Now, when a fetch request fails, it will log the error and return null. Corresponding tests to validate this behavior have also been added.

--- src/utils/json-fetch.ts
+++ src/utils/json-fetch.ts
@@ -1,7 +1,12 @@
-export const jsonFetch = <T>(url: string): Promise<T> => fetch(url)
+export const jsonFetch = <T>(url: string): Promise<T | null> => fetch(url)
   .then((response) => {
     if (!response.ok) {
       throw new Error(`error fetching (${url}): ${response.statusText}`);
     }
     return response.json() as Promise<T>;
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return null;
   });
