c9e03d676c14fe79dcde651f7df1a3d871ba5b57
Dave Price <d.price@elifesciences.org>
2023-08-14 12:06:48 +0100

    refactor html elements (h2,p) to classnames

--- src/components/atoms/error-messages/error-messages.tsx
+++ src/components/atoms/error-messages/error-messages.tsx
@@ -6,8 +6,8 @@ export const ErrorMessages = (): JSX.Element => (
  <div className="error_container">
   <div className="error">
     <Image src={MissingImage} width="64" height="157" alt="Oops" className="error__icon" />
-    <h2>We're on it</h2>
-    <p>The info you're looking for isn't available right now, and that's on us. Please try again later and thank you for your patience while we fix this.</p>
+    <h2 className="error-title">We're on it</h2>
+    <p className="error-message">The info you're looking for isn't available right now, and that's on us. Please try again later and thank you for your patience while we fix this.</p>
   </div>
 </div>
 );
