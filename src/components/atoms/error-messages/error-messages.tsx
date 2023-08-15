0f091f55bc77eb468fca92d5e7718b88d1c8f9f5
Dave Price <d.price@elifesciences.org>
2023-08-15 12:35:14 +0100

    refactor code

--- src/components/atoms/error-messages/error-messages.tsx
+++ src/components/atoms/error-messages/error-messages.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import MissingImage from '../../../images/errors/404.svg';
 
 export const ErrorMessages = (): JSX.Element => (
- <div className="error-container">
+<div className="error-container">
   <Image src={MissingImage} width="64" height="157" alt="We're on it" className="error__icon" />
   <h2 className="error__title">We&apos;re looking into it</h2>
   <p className="error__message">The information you&apos;re looking for isn&apos;t available right now but we&apos;re working to fix it. Please try again later and thank you for your patience.</p>
