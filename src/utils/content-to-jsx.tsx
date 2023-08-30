13753ffe5dcfbec214b21dbad4c1a5220f2d897d
Dave Price <d.price@elifesciences.org>
2023-08-30 10:37:00 +0100

    Refactor image element

--- src/utils/content-to-jsx.tsx
+++ src/utils/content-to-jsx.tsx
@@ -48,7 +48,7 @@ export const contentToJsx = (content: Content, index?: number, maxHeadingLevel?:
         return '';
       }
       // eslint-disable-next-line @next/next/no-img-element
-      return <picture><source media="(max-width: 767px)" srcSet={`${generateImageUrl(content.contentUrl)} 1x`} /><img loading="lazy" {...(content.meta.inline ? { className: 'inline-image' } : {})} key={index} src={generateImageUrl(content.contentUrl)} alt={content.alt} /></picture>;
+      return <picture key={index}><source media="(max-width: 767px)" srcSet={`${generateImageUrl(content.contentUrl)} 1x`} /><img loading="lazy" {...(content.meta.inline ? { className: 'inline-image' } : {})} src={generateImageUrl(content.contentUrl)} alt="" /></picture>;
     case 'ListItem':
       return <li key={index}>{contentToJsx(content.content)}</li>;
     case 'List':
