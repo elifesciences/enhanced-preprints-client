837ef07f32772f2549a45c585fc1455d4b91c531
Scott Aubrey <scott@aubrey.org.uk>
2023-09-05 15:39:05 +0100

    fix merge conflict issue

--- src/utils/content-to-jsx.tsx
+++ src/utils/content-to-jsx.tsx
@@ -51,8 +51,8 @@ export const contentToJsx = (content: Content, index?: number, maxHeadingLevel?:
       return <picture key={index}>
         <source media="(max-width: 479px)" srcSet={generateImageUrlSized(content.contentUrl, 479)} />
         <source media="(max-width: 767px)" srcSet={generateImageUrlSized(content.contentUrl, 767)} />
-        <source media="(min-width: 768px)" srcSet={generateImageUrl(content.contentUrl)} />
-        <img {...(content.meta.inline ? { className: 'inline-image' } : {})} src={generateImageUrl(content.contentUrl)} alt="" />
+        <source media="(min-width: 768px)" srcSet={generateImageUrlSized(content.contentUrl, 1000)} />
+        <img loading="lazy" {...(content.meta.inline ? { className: 'inline-image' } : {})} src={generateImageUrl(content.contentUrl)} alt="" />
       </picture>;
     case 'ListItem':
       return <li key={index}>{contentToJsx(content.content)}</li>;
