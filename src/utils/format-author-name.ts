c8f371885250e6fc8eaa9afcf320a065ad094b5f
Scott Aubrey <scott@aubrey.org.uk>
2023-09-21 16:56:12 +0100

    move the correct name formatting to a utility function and use as part of metadata (#1182)

---
+++ src/utils/format-author-name.ts
@@ -0,0 +1,3 @@
+import { Author } from '../types';
+
+export const formatAuthorName = ({ givenNames, familyNames, honorificSuffix }: Author) => `${givenNames && givenNames.join()} ${familyNames && familyNames.join()}${honorificSuffix ? ` ${honorificSuffix}` : ''}`.trim();
