3084b9cada1f0feec415379c31d973a7f566fec6
viktoria.mahmud <viktoria.nedyalkova@endava.com>
2023-09-27 12:14:52 +0300

    Hide Copyright component when there is different license link and change tests

--- src/components/atoms/copyright/copyright.test.tsx
+++ src/components/atoms/copyright/copyright.test.tsx
@@ -1,34 +1,52 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Copyright } from './copyright';
 import { authors } from '../../../utils/mocks';
 
 describe('Copyright Component', () => {
-  it('renders correct text for Creative Commons Attribution License', () => {
+  it('renders correct text for Creative Commons License', () => {
     const license = 'https://creativecommons.org/licenses/by/4.0/';
     const publishedYear = 2023;
 
-    const { getByText } = render(<Copyright license={license} publishedYear={publishedYear} author={authors[0]} />);
+    render(<Copyright license={license} publishedYear={publishedYear} author={authors[0]} />);
 
-    const attributionText = getByText(/This article is distributed under the terms of the/);
-    expect(attributionText).toBeInTheDocument();
+    const licenseText = screen.getByText(/This article is distributed under the terms of the/);
+    expect(licenseText).toBeInTheDocument();
 
-    const yearText = getByText(/© 2023/);
+    const yearText = screen.getByText(/© 2023/);
     expect(yearText).toBeInTheDocument();
 
     const authorName = `${authors[0]?.givenNames?.join(' ')} ${authors[0]?.familyNames?.join(' ')}`;
-    const authorText = getByText(new RegExp(authorName));
+    const authorText = screen.getByText(new RegExp(authorName));
     expect(authorText).toBeInTheDocument();
 
-    const authorEtAlText = getByText(/et al./);
+    const authorEtAlText = screen.getByText(/et al./);
     expect(authorEtAlText).toBeInTheDocument();
   });
 
   it('renders correct text for Creative Commons CC0 public domain dedication', () => {
     const license = 'https://creativecommons.org/publicdomain/zero/1.0/';
 
-    const { getByText } = render(<Copyright license={license} />);
+    render(<Copyright license={license} />);
 
-    const cc0Text = getByText(/This is an open-access article, free of all copyright/);
+    const yearText = screen.queryByText(/© 2023/);
+    expect(yearText).not.toBeInTheDocument();
+
+    const cc0Text = screen.getByText(/This is an open-access article, free of all copyright/);
     expect(cc0Text).toBeInTheDocument();
+  });
+
+  it('renders no copyright notice', () => {
+    const license = 'license';
+
+    render(<Copyright license={license} />);
+
+    const licenseText = screen.queryByText(/This article is distributed under the terms of the/);
+    expect(licenseText).not.toBeInTheDocument();
+
+    const yearText = screen.queryByText(/© 2023/);
+    expect(yearText).not.toBeInTheDocument();
+
+    const cc0Text = screen.queryByText(/This is an open-access article, free of all copyright/);
+    expect(cc0Text).not.toBeInTheDocument();
   });
 });
--- src/components/atoms/copyright/copyright.tsx
+++ src/components/atoms/copyright/copyright.tsx
@@ -8,25 +8,26 @@ type CopyrightProps = {
 };
 
 export const Copyright = ({ license, publishedYear, author }: CopyrightProps) => {
-  let copyrightText = (
-    <>
-      This article is distributed under the terms of the <a href={license}>Creative Commons Attribution License</a>, which
-      permits unrestricted use and redistribution provided that the original author and source are credited.
-    </>
-  );
-
-  let hasCopyright = true;
+  let copyrightText;
+  let hasCopyright = false;
   let authorName: string | undefined;
 
-  if (license?.length && license.includes('/zero/')) {
+  if (license?.includes('/by/')) {
+    copyrightText = (
+      <>
+        This article is distributed under the terms of the <a href={license}>Creative Commons Attribution License</a>, which
+        permits unrestricted use and redistribution provided that the original author and source are credited.
+      </>
+    );
+
+    hasCopyright = true;
+  } else if (license?.includes('/zero/')) {
     copyrightText = (
       <>
         This is an open-access article, free of all copyright, and may be freely reproduced, distributed, transmitted, modified, built upon, or
         otherwise used by anyone for any lawful purpose. The work is made available under the <a href={license}>Creative Commons CC0 public domain dedication</a>.
       </>
     );
-
-    hasCopyright = false;
   }
 
   if (author) {
@@ -38,10 +39,13 @@ export const Copyright = ({ license, publishedYear, author }: CopyrightProps) =>
   }
 
   return (
-    <div className="copyright">
-      <h3>Copyright</h3>
-      {hasCopyright && <p>{publishedYear && `© ${publishedYear},`}{authorName && ` ${authorName} et al.`}</p>}
-      <p>{copyrightText}</p>
-    </div>
+    <>
+      {copyrightText &&
+        <div className="copyright">
+          <h3>Copyright</h3>
+          {hasCopyright && <p>{publishedYear && `© ${publishedYear},`}{authorName && ` ${authorName} et al.`}</p>}
+          <p>{copyrightText}</p>
+        </div>}
+    </>
   );
 };
