38ea328af6e593b4e684f6fc3d4a8d8b057e97d0
Dave <discodavey@gmail.com>
2023-11-21 15:04:11 +0000

    Add function generate image info

--- src/utils/generate-image-url.ts
+++ src/utils/generate-image-url.ts
@@ -1,9 +1,19 @@
 import { config } from '../config';
 
-export const generateImageUrl = (contentUrl: string): string => (config.imageServer ? `${config.imageServer}/2/${encodeURIComponent(contentUrl)}/full/max/0/default.jpg` : contentUrl);
+export const generateImageUrl = (contentImageId: string): string => (config.imageServer ? `${config.imageServer}/2/${encodeURIComponent(contentImageId)}/full/max/0/default.jpg` : contentImageId);
 
-export const generateImageUrlSized = (contentUrl: string, size: number): string => {
-  if (!config.imageServer) return contentUrl;
+export const generateImageUrlSized = (contentImageId: string, size: number): string => {
+  if (!config.imageServer) return contentImageId;
 
-  return `${config.imageServer}/2/${encodeURIComponent(contentUrl)}/full/${size},/0/default.jpg`;
+  return `${config.imageServer}/2/${encodeURIComponent(contentImageId)}/full/${size},/0/default.jpg`;
+};
+
+export const generateImageInfo = async (contentImageId: string): Promise<{ width: number, height: number }> => {
+  const infoUrl = `${config.imageServer}/2/${encodeURIComponent(contentImageId)}/info.json`;
+  const imageInfo = await fetch(infoUrl);
+  const imageJson = await imageInfo.json();
+  const height = imageJson['height'];
+  const width = imageJson['width'];
+
+  return { height, width };
 };
