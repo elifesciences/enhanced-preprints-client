83e294253de8cc0759029941b00833d72c530bf2
viktoria-mahmud <111341278+viktoria-mahmud@users.noreply.github.com>
2023-11-29 14:47:16 +0200

    Add another type to support different identifier type (#1434)

--- src/types/enhanced-article.ts
+++ src/types/enhanced-article.ts
@@ -15,12 +15,19 @@ export type Address = {
   addressCountry?: string,
 };
 
-export type OrcidIdentifier = {
+// Support for this can be dropped after automation is deployed
+export type LegacyOrcidIdentifier = {
   type: 'orcid',
   value: string,
 };
 
-export type Identifier = OrcidIdentifier;
+export type OrcidIdentifier = {
+  type: 'PropertyValue',
+  propertyID: 'https://registry.identifiers.org/registry/orcid',
+  value: string,
+};
+
+export type Identifier = LegacyOrcidIdentifier | OrcidIdentifier;
 
 export type Author = {
   familyNames: string[],
