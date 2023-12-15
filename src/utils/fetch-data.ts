bdd5a20cc57edc5b317bc5802a9d23fdc43c6261
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-12-15 14:06:17 +0000

    feat: update fetchVersionsNoContent and reviewed-preprints.page (#1502)
    * feat: update fetchVersionsNoContent and reviewed-preprints.page
    
    Update fetchVersionsNoContent to take parameters for pagination and ordering.
    Remove redundant logic from reviewed-preprints.page.ts and replace with
    fetchVersionsNoContent function.
    
    * feat: refactor fetching and processing of reviewed preprints data
    
    Change the data processing logic in 'reviewed-preprints.page.ts' to use the
    new data structure returned from the fetch function. Update the fetch function
    in 'fetch-data.ts' to include total count in the returned object. Previously,
    the total count was fetched separately from the request headers.

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -28,4 +28,24 @@ export const fetchReviews = (id: string) => {
 
 export const fetchVersion = (id: string, preview: boolean = false) => jsonFetchOrNull<EnhancedArticleWithVersions>(`${config.apiServer}/api/preprints/${id}${preview ? '?previews=true' : ''}`);
 export const fetchVersions = () => jsonFetch<{ items: ArticleSummary[], total: number }>(`${config.apiServer}/api/preprints`);
-export const fetchVersionsNoContent = () => jsonFetch<EnhancedArticleNoContent[]>(`${config.apiServer}/api/preprints-no-content`);
+// export const fetchVersionsNoContent = (page: number, perPage: number, order: string) => jsonFetch<EnhancedArticleNoContent[]>(`${config.apiServer}/api/preprints-no-content?page=${page}&per-page=${perPage}&order=${order}`);
+export const fetchVersionsNoContent = async (page: number, perPage: number, order: string) => {
+  const url = `${config.apiServer}/api/preprints-no-content?page=${page}&per-page=${perPage}&order=${order}`;
+  return fetch(url)
+    .then(async (response) => {
+      if (!response.ok) {
+        throw new Error(`error fetching (${url}): ${response.statusText}`);
+      }
+
+      const items = await response.json() as EnhancedArticleNoContent[];
+
+      const total = response.headers.get('x-total-count')
+        ? parseInt(response.headers.get('x-total-count') as string, 10)
+        : Object.keys(items).length;
+
+      return {
+        total,
+        items,
+      };
+    });
+};
