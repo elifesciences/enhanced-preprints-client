a65941cd841019071d1877acc3ca88ec4c0474d9
Nathan Lisgo <nathan@lisgo.co.uk>
2024-01-15 21:08:35 +0000

    feat: add citations download feature
    This commit includes changes for downloading bibtex and ris citations.
    Updates have been made to the nginx-browser-test.conf file to add new
    location blocks for bibtex and ris citations. New test cases have been
    added to the 'citations.spec.ts' file for testing the download of these
    citations. The 'docker-compose.browsertest.yaml' file has been updated
    to expose the API and client via proxy. New .bib and .ris files have
    been added to the 'test-utils/data/preprints' directory for testing
    purposes.

---
+++ browser-tests/citations.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('citations', () => {
+  test('can download a bibtex citation', async ({ request }) => {
+    const response = await request.get('http://localhost:3001/reviewed-preprints/123.bib');
+    expect(response.ok()).toBeTruthy();
+    const headers = response.headers();
+    expect(headers['content-type']).toBe('application/x-bibtex');
+    const responseBody = await response.text();
+    expect(responseBody).toContain('A study of world domination by genetically enhanced mice');
+  });
+
+  test('can download a ris citation', async ({ request }) => {
+    const response = await request.get('http://localhost:3001/reviewed-preprints/123.ris');
+    expect(response.ok()).toBeTruthy();
+    const headers = response.headers();
+    expect(headers['content-type']).toBe('application/x-research-info-systems');
+    const responseBody = await response.text();
+    expect(responseBody).toContain('A study of world domination by genetically enhanced mice');
+  });
+});
