4f770e734ba7cb9f0a549329ebf137469910cca9
Nathan Lisgo <nathan@lisgo.co.uk>
2024-01-23 21:16:12 +0000

    feat(site-footer): remove SignUp and Investors components from SiteFooter

--- src/components/molecules/site-footer/site-footer.tsx
+++ src/components/molecules/site-footer/site-footer.tsx
@@ -3,5 +3,5 @@ import { Investors } from '../../atoms/investors/investors';
 import { SignUp } from '../../atoms/sign-up/sign-up';
 
 export const SiteFooter = () => (
-  <><SignUp /><Investors /><FooterMain /></>
+  <><FooterMain /></>
 );
