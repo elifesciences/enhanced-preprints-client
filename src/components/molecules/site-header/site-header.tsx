87120bd87793ac3dfa8dbc64b280acef9127bf6d
Nathan Lisgo <nathan@lisgo.co.uk>
2024-01-23 15:09:04 +0000

    feat: update navigation links in site-header component
    This commit updates the navigation links in the site-header component. The
    links for 'Home', 'About', and 'Search' have been changed. The component
    structure has also been simplified.

--- src/components/molecules/site-header/site-header.tsx
+++ src/components/molecules/site-header/site-header.tsx
@@ -2,40 +2,38 @@ import Image from 'next/image';
 import './site-header.scss';
 import logo from '../../../../public/elife-logo.svg';
 
-export const SiteHeader = () => {
-  return (
-    <div className="site-header">
-      <div className="site-header-container">
-        <a href="/" className="site-header__logo_link">
-          <Image
-            className="site-header__logo"
-            src={logo}
-            alt="eLife logo"
-            width="80"
-            height="30"
-          />
-        </a>
-        <span className="site-header__title">Enhanced Preprints</span>
+export const SiteHeader = () => (
+  <div className="site-header">
+    <div className="site-header-container">
+      <a href="/" className="site-header__logo_link">
+        <Image
+          className="site-header__logo"
+          src={logo}
+          alt="eLife logo"
+          width="80"
+          height="30"
+        />
+      </a>
+      <span className="site-header__title">Enhanced Preprints</span>
 
-        <nav className="nav-primary">
-          <ul className="nav-primary__list">
-            <li className="nav-primary__item">
-              <a href="https://elifesciences.org/">Home</a>
-            </li>
-            <li className="nav-primary__item">
-              <a href="https://elifesciences.org/about">About</a>
-            </li>
-          </ul>
-        </nav>
+      <nav className="nav-primary">
+        <ul className="nav-primary__list">
+          <li className="nav-primary__item">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-primary__item">
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
 
-        <nav className="nav-secondary">
-          <ul className="nav-secondary__list">
-            <li className="nav-secondary__item nav-secondary__item--search">
-              <a href="https://elifesciences.org/search" rel="search">Search</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="nav-secondary">
+        <ul className="nav-secondary__list">
+          <li className="nav-secondary__item nav-secondary__item--search">
+            <a href="#" rel="search">Search</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  );
-};
+  </div>
+);
