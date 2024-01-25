885f1cecdd606ed6ce929e2ff2514eb29f7c942a
Nathan Lisgo <nathan@lisgo.co.uk>
2024-01-25 14:20:27 +0000

    feat: add SiteHeaderBiophysicsColab component
    This commit introduces a new component, `SiteHeaderBiophysicsColab`,
    to the `biophysics-colab` layout. The new component includes a logo image,
    primary and secondary navigation elements. The commit also includes
    associated test and storybook files for the new component.

--- src/components/layouts/biophysics-colab.tsx
+++ src/components/layouts/biophysics-colab.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import './biophysics-colab.scss';
+import { SiteHeaderBiophysicsColab } from '../molecules/site-header/site-header-biophysics-colab';
 
 type Props = {
   children: ReactNode,
@@ -8,6 +9,9 @@ type Props = {
 export const BiophysicsColabLayout = ({ children }: Props) => (
   <>
     <div className="grid-container article-page">
+      <div className="grid-header">
+        <SiteHeaderBiophysicsColab/>
+      </div>
       {children}
     </div>
   </>
