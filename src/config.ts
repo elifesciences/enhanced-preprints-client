378dd8fbd5859c8e836a8e4e22fbe7d539929d7e
Nathan Lisgo <nathan@lisgo.co.uk>
2024-01-25 10:05:26 +0000

    feat: Update environment variable SITE_NAME to NEXT_PUBLIC_SITE_NAME
    This commit updates the environment variable SITE_NAME to
    NEXT_PUBLIC_SITE_NAME across the README.md, docker-compose.yaml, and
    src/config.ts files for site specific overrides. This modification
    allows for better consistency and compatibility with Next.js
    environment variables.

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ type Config = {
 };
 
 export const config: Config = {
-  siteName: process.env.SITE_NAME,
+  siteName: process.env.NEXT_PUBLIC_SITE_NAME,
   showPreviews: (!!process.env.SHOW_PREVIEWS) || false,
   apiServer: process.env.API_SERVER,
   imageServer: process.env.NEXT_PUBLIC_IMAGE_SERVER,
