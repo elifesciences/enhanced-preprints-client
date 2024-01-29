f9b62cf91f9bc26d2f29d42f99a74040b213cf3c
aishakapan <a.kapan@elifesciences.org>
2024-01-29 15:15:06 +0000

    Replace reviewed preprint string with endorsed article

--- src/utils/generate-timeline.ts
+++ src/utils/generate-timeline.ts
@@ -1,3 +1,4 @@
+import i18n from '../i18n';
 import {
   EnhancedArticleWithVersions, VersionSummary, TimelineEvent,
 } from '../types';
@@ -11,11 +12,11 @@ export const generateTimeline = (version: EnhancedArticleWithVersions): Timeline
       events.push(current.id === version.article.id ?
         {
           date: new Date(current.published).toDateString(),
-          name: `Reviewed preprint version ${current.versionIdentifier}`,
+          name: i18n.t('Reviewed preprint version {{versionIdentifier}}', { versionIdentifier: current.versionIdentifier }),
           eventDescription: '(this version)',
         } : {
           date: new Date(current.published).toDateString(),
-          name: `Reviewed preprint version ${current.versionIdentifier}`,
+          name: i18n.t('Reviewed preprint version {{versionIdentifier}}', { versionIdentifier: current.versionIdentifier }),
           link: {
             url: `/reviewed-preprints/${current.id}`,
             text: 'Go to version',
