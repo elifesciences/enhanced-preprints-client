d4dbdab70822b12f94bd999186255d33aa66b6b9
Scott Aubrey <scott@aubrey.org.uk>
2024-02-01 17:46:24 +0000

    add a default brand logo

--- src/brand.ts
+++ src/brand.ts
@@ -1,4 +1,7 @@
 import React from 'react';
+import { StaticImageData } from 'next/image';
+import logo from '../public/epp_logo.png';
+
 
 export type Brand = {
   colors: {
@@ -9,13 +12,18 @@ export type Brand = {
   processUrl?: string,
   showElifeMenus: boolean,
   logo?: {
-    url: string,
+    url: string | StaticImageData,
     height: number,
     width: number,
   }
 };
 
 export const defaultBrand = {
+  logo: {
+    url: logo,
+    width: 100,
+    height: 40,
+  },
   colors: {
     primary: 'grey',
   },
