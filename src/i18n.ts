4d4aa6a18628792078336ef459a74e762555e5c8
Nathan Lisgo <nathan@lisgo.co.uk>
2024-02-02 14:05:01 +0000

    [#996] feat: use i18n for URLs in review-content, i18n and reviewed-preprints
    Use i18n for generating URLs in review-content, i18n and reviewed-preprints
    modules. This improves maintainability by centralizing URL management.

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,8 +10,10 @@ const resources = {
       publisher_short: 'Publisher short name',
       publisher_long: 'Publisher long name',
       process_url: '#', // link to review process
+      about_assessments_url: '#', // link to about assessments
 
       // Defaults:
+      reviewed_preprints_url: '/reviewed-preprints/{{msid}}', // link to reviewed preprints
       heading_assessment: '$t(publisher_short) assessment',
       about_assessments: 'About $t(publisher_short) assessments',
       status_description_reviewed: 'Published from the original preprint after peer review and assessment by $t(publisher_short).',
@@ -25,7 +27,8 @@ const resources = {
       publisher_short: 'eLife',
       publisher_long: 'eLife Sciences Publications Limited',
       process_url: 'https://elifesciences.org/peer-review-process',
-
+      about_assessments_url: 'https://elifesciences.org/inside-elife/db24dd46',
+      reviewed_preprints_url: 'https://elifesciences.org/reviewed-preprints/{{msid}}',
     },
     biophysics_colab: {
       twitter_handle: '@BiophysicsColab',
@@ -37,6 +40,7 @@ const resources = {
       status_description_reviewed: '$t(publisher_short) have endorsed this preprint that was revised by authors after peer review.',
       status_description_revised: '$t(publisher_short) have endorsed this preprint that was revised by authors after peer review.',
       process_url: 'https://www.sciencecolab.org/biophysics-colab',
+      about_assessments_url: 'https://www.sciencecolab.org/biophysics-colab',
     },
   },
 };
