ce51a61c7953d9b7646c1ff9635a6d8f15b8d4ed
Scott Aubrey <scott@aubrey.org.uk>
2024-02-02 17:19:02 +0000

    fix timeline types in stories

--- src/components/molecules/timeline/timeline.stories.tsx
+++ src/components/molecules/timeline/timeline.stories.tsx
@@ -12,9 +12,9 @@ const Template: StoryFn<typeof Timeline> = (args) => <Timeline {...args} />;
 export const EventTimeline = Template.bind({});
 EventTimeline.args = {
   events: [
-    { name: 'event 1', date: '1999-01-01' },
-    { name: 'event 2', date: '2000-02-02' },
-    { name: 'event 3', date: '2001-03-03' },
+    { name: 'event 1', versionIdentifier: '1', date: '1999-01-01' },
+    { name: 'event 2', versionIdentifier: '1', date: '2000-02-02' },
+    { name: 'event 3', versionIdentifier: '1', date: '2001-03-03' },
   ],
 };
 export const EventTimelineWithLinks = Template.bind({});
@@ -23,6 +23,7 @@ EventTimelineWithLinks.args = {
     {
       name: 'event 1',
       date: '1999-01-01',
+      versionIdentifier: '1',
       link: {
         text: 'Go to bioRxiv',
         url: 'https://preprint.url',
@@ -30,10 +31,12 @@ EventTimelineWithLinks.args = {
     },
     {
       name: 'event 2',
+      versionIdentifier: '1',
       date: '2000-02-02',
     },
     {
       name: 'event 3',
+      versionIdentifier: '1',
       date: '2001-03-03',
     },
   ],
@@ -43,15 +46,18 @@ EventTimelineWithDescriptions.args = {
   events: [
     {
       name: 'event 1',
+      versionIdentifier: '1',
       date: '1999-01-01',
       eventDescription: '(this version)',
     },
     {
       name: 'event 2',
+      versionIdentifier: '1',
       date: '2000-02-02',
     },
     {
       name: 'event 3',
+      versionIdentifier: '1',
       date: '2001-03-03',
     },
   ],
@@ -59,15 +65,15 @@ EventTimelineWithDescriptions.args = {
 export const EventTimelineUnordered = Template.bind({});
 EventTimelineUnordered.args = {
   events: [
-    { name: 'event 1', date: '1999-01-01' },
-    { name: 'event 3', date: '2001-03-03' },
-    { name: 'event 2', date: '2000-02-02' },
+    { name: 'event 1', versionIdentifier: '1', date: '1999-01-01' },
+    { name: 'event 3', versionIdentifier: '1', date: '2001-03-03' },
+    { name: 'event 2', versionIdentifier: '1', date: '2000-02-02' },
   ],
 };
 export const EventTimelineISO8601 = Template.bind({});
 EventTimelineISO8601.args = {
   events: [
-    { name: 'event 1', date: '2022-12-31T23:59:59Z' },
-    { name: 'event 2', date: '2023-01-01T00:00:00Z' },
+    { name: 'event 1', versionIdentifier: '1', date: '2022-12-31T23:59:59Z' },
+    { name: 'event 2', versionIdentifier: '1', date: '2023-01-01T00:00:00Z' },
   ],
 };
