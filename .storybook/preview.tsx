d95eff957e1b15373a0df1ab5f62874cd79d2ec2
Scott Aubrey <scott@aubrey.org.uk>
2024-02-02 23:07:31 +0000

    get storybook working with a brandswitcher

---
+++ .storybook/preview.tsx
@@ -0,0 +1,88 @@
+import { Preview } from '@storybook/react';
+import { BrandContext, Brand, elifeBrand, biophysicsColabBrand, defaultBrand } from '../src/brand';
+import { i18n } from '../src/i18n';
+import { I18nextProvider } from 'react-i18next';
+
+const preview: Preview = {
+  globalTypes: {
+    brand: {
+      description: 'Branding for components',
+      defaultValue: 'elife',
+      toolbar: {
+        title: 'Brading',
+        icon: 'circlehollow',
+        items: ['elife', 'biophysics-colab'],
+        dynamicTitle: true,
+      },
+    },
+  },
+  decorators: [
+    (Story, context) => {
+      let brand: Brand;
+      switch (context.globals.brand) {
+        case 'elife':
+          brand = elifeBrand;
+          break;
+        case 'biophysics-colab':
+          brand = biophysicsColabBrand;
+          break;
+        default:
+          brand = defaultBrand;
+      }
+      if (brand.translationNamespace) {
+        i18n.setDefaultNamespace(brand.translationNamespace);
+      }
+
+      const brandStyles = {
+        '--color-primary': brand.colors.primary,
+      } as React.CSSProperties;
+
+      return (
+        <I18nextProvider i18n={i18n}>
+          <BrandContext.Provider value={brand}>
+            <div style={brandStyles}>
+              <Story />
+            </div>
+          </BrandContext.Provider>
+        </I18nextProvider>
+      );
+    },
+  ],
+};
+
+export default preview;
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   layout: 'fullscreen',
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+//   backgrounds: {
+//     default: 'white',
+//     values: [
+//       {
+//         name: 'white',
+//         value: '#fff',
+//       },
+//     ]
+//   },
+//   globalTypes: {
+//     theme: {
+//       description: 'Global theme for components',
+//       defaultValue: 'light',
+//       toolbar: {
+//         // The label to show for this toolbar item
+//         title: 'Theme',
+//         icon: 'circlehollow',
+//         // Array of plain string values or MenuItem shape (see below)
+//         items: ['light', 'dark'],
+//         // Change title based on selected value
+//         dynamicTitle: true,
+//       },
+//     },
+//   },
+// }
