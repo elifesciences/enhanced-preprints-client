f5dfe9fbc049ea19c152ec7b3c097ab3e217f3db
Scott Aubrey <scott@aubrey.org.uk>
2024-02-05 07:54:31 +0000

    update mock data

--- src/utils/mocks/status.ts
+++ src/utils/mocks/status.ts
@@ -1,6 +1,6 @@
 export const status = {
   articleType: 'reviewed_preprint',
-  status: 'This Reviewed Preprint was published after peer review and assessment by eLife.',
+  status: 'status_description_reviewed',
   timeline: [
     { name: 'Peer review done', versionIdentifier: '1', date: '2022-01-02' },
     { name: 'Preprint posted', versionIdentifier: '1', date: '2022-01-01' },
