6f45190baa178bc01fa8be5999a1edd7ced33e83
Nathan Lisgo <nathan@lisgo.co.uk>
2024-02-07 16:17:45 +0000

    [#1005] feat: adjust end-date in fetch-data utility
    This commit modifies the end-date parameter in the fetch-data utility.
    It now adds a day to the end-date before converting it to ISO string format.

--- src/utils/fetch-data.ts
+++ src/utils/fetch-data.ts
@@ -14,7 +14,7 @@ export const fetchVersionsNoContent = async (page: number, perPage: number, orde
       `order=${order}`,
       useDate === 'published' ? 'use-date=firstPublished' : '',
       startDate ? `start-date=${startDate}` : '',
-      endDate ? `end-date=${endDate}` : '',
+      endDate ? `end-date=${new Date(new Date().setDate(new Date(endDate).getDate() + 1)).toISOString().split('T')[0]}` : '',
     ].filter((q) => q).join('&'),
   ].join('');
   return fetch(url)
