1c8ffcd3690cb6f2d1b5a1bafca7c55613303654
Will Byrne <will.alex.byrne@gmail.com>
2024-02-15 11:23:11 +0000

    [#640]: Add listType type and sass entries for the different list styles

--- src/components/atoms/list/list.stories.tsx
+++ src/components/atoms/list/list.stories.tsx
@@ -7,55 +7,61 @@ export default {
 } as Meta<typeof List>;
 
 const Template: StoryFn<typeof List> = (args) => <List {...args} />;
+const listContent = {
+  type: 'List' as const,
+  items: [
+    {
+      type: 'ListItem' as const,
+      content: 'list item 1',
+    },
+    {
+      type: 'ListItem' as const,
+      content: 'list item 2',
+    },
+    {
+      type: 'ListItem' as const,
+      content: 'list item 3',
+    },
+  ],
+  order: 'Unordered' as const,
+};
 
-export const BasicList = Template.bind({});
-BasicList.args = {
-  content: {
-    type: 'List',
-    items: [
-      {
-        type: 'ListItem',
-        content: [
-          { type: 'Paragraph', content: ['1)'] },
-          {
-            type: 'Paragraph',
-            content: [
-              // eslint-disable-next-line max-len
-              'We showed that the IL-6/JAK/STAT signaling cascade is enriched in human tendinopathic tendons alongside gene signatures typical for fibroblasts and progenitors as well as downstream gene sets suggesting excessive cell proliferation (hypercellularity) and imbalanced extracellular matrix turnover.',
-            ],
-          },
-        ],
-      },
-      {
-        type: 'ListItem',
-        content: [
-          { type: 'Paragraph', content: ['2)'] },
-          {
-            type: 'Paragraph',
-            content: [
-              'We exploited an explant-based assembloid model system to first confirm the causal effect of IL-6 signaling on extrinsic fibroblast progenitor activation, recruitment, and proliferation. We then found additional ',
-              { type: 'Emphasis', content: ['in vivo'] },
-              ' support for IL-6 dependent recruitment of Scx',
-              { type: 'Superscript', content: ['+'] },
-              ' fibroblast populations using an Achilles tendon lesion model in IL-6 KO mice.',
-            ],
-          },
-        ],
-      },
-      {
-        type: 'ListItem',
-        content: [
-          { type: 'Paragraph', content: ['3)'] },
-          {
-            type: 'Paragraph',
-            content: [
-              // eslint-disable-next-line max-len
-              'We followed the downstream effects of enhanced extrinsic fibroblast progenitor activation and accumulation on the tendon core embedded in our assembloids. Here, we document the emergence of central tendinopathic hallmarks including aberrant (catabolic) matrix turnover, hypercellularity, and hypoxic responses with a stronger fibroblast progenitor presence, which is reduced when IL-6 core signaling is inhibited.',
-            ],
-          },
-        ],
-      },
-    ],
-    order: 'Unordered',
-  },
+export const DefaultList = Template.bind({});
+DefaultList.args = {
+  content: listContent,
+};
+
+export const OrderList = Template.bind({});
+OrderList.args = {
+  content: { ...listContent, meta: { listType: 'order' } },
+};
+
+export const AlphaLowerList = Template.bind({});
+AlphaLowerList.args = {
+  content: { ...listContent, meta: { listType: 'alpha-lower' } },
+};
+
+export const AlphaUpperList = Template.bind({});
+AlphaUpperList.args = {
+  content: { ...listContent, meta: { listType: 'alpha-upper' } },
+};
+
+export const RomanLowerList = Template.bind({});
+RomanLowerList.args = {
+  content: { ...listContent, meta: { listType: 'roman-lower' } },
+};
+
+export const RomanUpperList = Template.bind({});
+RomanUpperList.args = {
+  content: { ...listContent, meta: { listType: 'roman-upper' } },
+};
+
+export const SimpleList = Template.bind({});
+SimpleList.args = {
+  content: { ...listContent, meta: { listType: 'simple' } },
+};
+
+export const CustomList = Template.bind({});
+CustomList.args = {
+  content: { ...listContent, meta: { listType: 'custom' } },
 };
