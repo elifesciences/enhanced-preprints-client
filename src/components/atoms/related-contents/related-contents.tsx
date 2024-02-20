f271d70f97a4e0ee65223f0b3e2db0d27267b769
Scott Aubrey <scott@aubrey.org.uk>
2024-02-20 15:21:29 +0000

    [#1001]: Add eLife lookups for related content types

--- src/components/atoms/related-contents/related-contents.tsx
+++ src/components/atoms/related-contents/related-contents.tsx
@@ -9,6 +9,13 @@ type RelatedContentsProps = {
   articles: RelatedContent[]
 };
 
+const types: Record<string, string> = {
+  collections: 'Is part of',
+  podcast: 'Discussed in',
+  'focus-issue': 'Is part of',
+  insight: 'Related Insight',
+};
+
 export const RelatedContents = ({ articles }: RelatedContentsProps) => (
   <ul className="related-contents">
     {articles.map(({
@@ -16,7 +23,7 @@ export const RelatedContents = ({ articles }: RelatedContentsProps) => (
     }, index) => (
       <li key={`related-contents-${index}`} className="related-contents__item">
         <header>
-          <div>Related {type}</div>
+          <div>{types[type]}</div>
           <h4><a href={url}>{title}</a></h4>
         </header>
         {content && <div>{content}</div>}
