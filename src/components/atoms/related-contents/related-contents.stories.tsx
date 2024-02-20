40bb9c1429ad0f68e55f236be74d25ee42c0ce8e
Scott Aubrey <scott@aubrey.org.uk>
2024-02-20 15:42:05 +0000

    [#1001]: fix storybook stories and add imageUrl

--- src/components/atoms/related-contents/related-contents.stories.tsx
+++ src/components/atoms/related-contents/related-contents.stories.tsx
@@ -13,15 +13,16 @@ export const RelatedContentsStory = Template.bind({});
 RelatedContentsStory.args = {
   articles: [
     {
-      type: 'Insight',
+      type: 'insight',
       title: 'Summary of this article',
       content: 'Some impact statement content on the value of this article',
       url: 'https://elifesciences.org/articles/123456',
     },
     {
-      type: 'Podcast',
+      type: 'podcast',
       title: 'Podcast related to this article',
       url: 'https://elifesciences.org/podcast/episode123456',
+      imageUrl: 'http://placekitten.com/200/150',
     },
   ],
 };
