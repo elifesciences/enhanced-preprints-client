e49d91c150dd5a966dff0286761125da223090c3
Will Byrne <will.alex.byrne@gmail.com>
2024-02-22 11:35:22 +0000

    [#1001]: Move image to match designs

--- src/components/atoms/related-contents/related-contents.tsx
+++ src/components/atoms/related-contents/related-contents.tsx
@@ -27,11 +27,11 @@ export const RelatedContents = ({ articles }: RelatedContentsProps) => (
       <li key={`related-contents-${index}`} className="related-contents__item">
         <header>
           <div className="related-contents__item-type">{types[type]}</div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          {imageUrl && <img src={imageUrl} alt="related content thumbnail"/> }
           <h4 className="related-contents__item-title"><a className="related-contents__item-link" href={url}>{title}</a></h4>
         </header>
         {content && <div className="related-contents__item-content">{content}</div>}
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        {imageUrl && <img src={imageUrl} alt="related content thumbnail"/> }
       </li>
     ))}
   </ul>
