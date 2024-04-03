70c3dfdd751fd0473f37566743941dcab7a8844e
Nathan Lisgo <nathan@lisgo.co.uk>
2024-04-03 23:02:43 +0100

    [#1038] feat: add onReadyScript to backstop.json and ignore contextual data in visual regression tests
    This commit adds an onReadyScript to the backstop.json configuration file
    and also modifies the ContextualData component to ignore it during visual
    regression testing by adding the 'visual-regression-ignore' class.

--- src/components/atoms/contextual-data/contextual-data.tsx
+++ src/components/atoms/contextual-data/contextual-data.tsx
@@ -7,7 +7,7 @@ export type ContextualDataProps = {
 };
 
 export const ContextualData = ({ views, citations, downloads }: ContextualDataProps) => (
-  <ul className="contextual-data">
+  <ul className="contextual-data visual-regression-ignore">
     <li className="contextual-data__item"><span className="contextual-data__item--highlight">{views}</span> {views === 1 ? 'view' : 'views'}</li>
     <li className="contextual-data__item"><span className="contextual-data__item--highlight">{citations}</span> {citations === 1 ? 'citation' : 'citations'}</li>
     <li className="contextual-data__item"><span className="contextual-data__item--highlight">{downloads}</span> {downloads === 1 ? 'download' : 'downloads'}</li>
