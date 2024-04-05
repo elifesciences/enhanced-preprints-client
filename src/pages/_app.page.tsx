01b9c4fea6e148bd712c23bd6037a0d2c20a66ef
Scott Aubrey <scott@aubrey.org.uk>
2024-04-05 17:17:05 +0100

    fix: allow cookiebot and gtm to appear when env var is set without a yarn build

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import { Noto_Serif, Noto_Sans } from 'next/font/google';
 import { ReactNode } from 'react';
 import { DefaultLayout } from '../components/layouts/default';
@@ -58,21 +57,23 @@ export default function MyApp({ Component, pageProps }: any) {
             }
           `,
         }} />
+        { config.cookiebotId &&
+          // eslint-disable-next-line @next/next/no-sync-scripts
+          <script id="Cookiebot"
+            src="https://consent.cookiebot.com/uc.js"
+            data-cbid={config.cookiebotId}></script>
+        }
+        { config.gtmId &&
+          // eslint-disable-next-line @next/next/next-script-for-ga
+          <script id="GTM" dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${config.gtmId}');`,
+          }}></script>
+        }
       </Head>
-      { config.cookiebotId &&
-      <Script id="Cookiebot"
-        src="https://consent.cookiebot.com/uc.js"
-        data-cbid={config.cookiebotId}></Script>
-    }
-      { config.gtmId &&
-      <Script id="GTM" dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${config.gtmId}');`,
-      }}></Script>
-    }
       <LayoutSelector>
         <Component {...pageProps} />
       </LayoutSelector>
