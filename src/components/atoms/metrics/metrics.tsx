0b7771a38726b8e40d938f9a7e4908a94c3231b4
Will Byrne <will.alex.byrne@gmail.com>
2024-04-10 14:31:29 +0100

    [#1045]: add downloads and citations to metrics component in definition list

--- src/components/atoms/metrics/metrics.tsx
+++ src/components/atoms/metrics/metrics.tsx
@@ -3,5 +3,12 @@ import './metrics.scss';
 
 export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section>
   <h1 id="metrics" className="metrics__title">Metrics</h1>
-  <div className="metrics__views">{metrics.views}</div>
+  <dl>
+    <dd>{metrics.views}</dd>
+    <dt>Views</dt>
+    <dd>{metrics.downloads}</dd>
+    <dt>Downloads</dt>
+    <dd>{metrics.citations}</dd>
+    <dt>Citations</dt>
+  </dl>
 </section>;
