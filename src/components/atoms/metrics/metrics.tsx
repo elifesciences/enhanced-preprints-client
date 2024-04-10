dc9429baca62a0739468bc90275e99babd78d17f
Will Byrne <will.alex.byrne@gmail.com>
2024-04-10 14:17:02 +0100

    [#1045]: Add metrics title

--- src/components/atoms/metrics/metrics.tsx
+++ src/components/atoms/metrics/metrics.tsx
@@ -1,5 +1,7 @@
 import { Metrics as MetricsType } from '../../../types/enhanced-article';
+import './metrics.scss';
 
-export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section id="metrics">
+export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section>
+  <h1 id="metrics" className="metrics__title">Metrics</h1>
   <div className="metrics__views">{metrics.views}</div>
 </section>;
