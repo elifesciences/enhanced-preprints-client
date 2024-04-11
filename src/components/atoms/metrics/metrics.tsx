7ce9a1487936944244c73fc2200f7c4555fbe977
Scott Aubrey <scott@aubrey.org.uk>
2024-04-11 10:33:15 +0100

    [#1045]: Handle singular and plural metrics better

--- src/components/atoms/metrics/metrics.tsx
+++ src/components/atoms/metrics/metrics.tsx
@@ -5,15 +5,15 @@ export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section>
   <h1 id="metrics" className="metrics__title">Metrics</h1>
   <dl className="metricsTable">
     <div className="metricsTable__group">
-      <dt className="metricsTable__label">Views</dt>
+      <dt className="metricsTable__label">{metrics.views === 1 ? 'View' : 'Views'}</dt>
       <dd className="metricsTable__value">{metrics.views}</dd>
     </div>
     <div className="metricsTable__group">
-      <dt className="metricsTable__label">Downloads</dt>
+      <dt className="metricsTable__label">{metrics.views === 1 ? 'Download' : 'Downloads'}</dt>
       <dd className="metricsTable__value">{metrics.downloads}</dd>
     </div>
     <div className="metricsTable__group">
-      <dt className="metricsTable__label">Citations</dt>
+      <dt className="metricsTable__label">{metrics.views === 1 ? 'Citation' : 'Citations'}</dt>
       <dd className="metricsTable__value">{metrics.citations}</dd>
     </div>
   </dl>
