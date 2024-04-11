161480a52e33452b1f7bf6300b9392e47f348c23
Scott Aubrey <scott@aubrey.org.uk>
2024-04-11 10:28:09 +0100

    [#1045]: style up the component to match journal styles, but utilising our mixins

--- src/components/atoms/metrics/metrics.tsx
+++ src/components/atoms/metrics/metrics.tsx
@@ -3,12 +3,18 @@ import './metrics.scss';
 
 export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section>
   <h1 id="metrics" className="metrics__title">Metrics</h1>
-  <dl>
-    <dd>{metrics.views}</dd>
-    <dt>Views</dt>
-    <dd>{metrics.downloads}</dd>
-    <dt>Downloads</dt>
-    <dd>{metrics.citations}</dd>
-    <dt>Citations</dt>
+  <dl className="metricsTable">
+    <div className="metricsTable__group">
+      <dt className="metricsTable__label">Views</dt>
+      <dd className="metricsTable__value">{metrics.views}</dd>
+    </div>
+    <div className="metricsTable__group">
+      <dt className="metricsTable__label">Downloads</dt>
+      <dd className="metricsTable__value">{metrics.downloads}</dd>
+    </div>
+    <div className="metricsTable__group">
+      <dt className="metricsTable__label">Citations</dt>
+      <dd className="metricsTable__value">{metrics.citations}</dd>
+    </div>
   </dl>
 </section>;
