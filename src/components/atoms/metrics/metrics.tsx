546fd1009cbeeab81d86651e75f7fbdd9a657dd9
aishakapan <a.kapan@elifesciences.org>
2024-04-11 15:18:45 +0100

    [#1045] Change the positioning of value and label in metrics

--- src/components/atoms/metrics/metrics.tsx
+++ src/components/atoms/metrics/metrics.tsx
@@ -4,14 +4,10 @@ import './metrics.scss';
 export const Metrics = ({ metrics }: { metrics: MetricsType }) => <section>
   <h1 id="metrics" className="metrics__title">Metrics</h1>
   <dl className="metricsTable">
-    <div className="metricsTable__group">
       <dt className="metricsTable__label">{metrics.views === 1 ? 'View' : 'Views'}</dt>
       <dd className="metricsTable__value">{metrics.views}</dd>
-    </div>
-    <div className="metricsTable__group">
       <dt className="metricsTable__label">{metrics.views === 1 ? 'Download' : 'Downloads'}</dt>
       <dd className="metricsTable__value">{metrics.downloads}</dd>
-    </div>
   </dl>
   <p>Views and downloads are accumulated for all versions if this paper published by eLife.</p>
   <dl>
