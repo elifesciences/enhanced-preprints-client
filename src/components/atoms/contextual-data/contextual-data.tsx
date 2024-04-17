36f9622c6f98ddfe2396c3be29c71b957e6c34f8
aishakapan <a.kapan@elifesciences.org>
2024-04-17 15:43:44 +0100

    [#1045]: Switch the order of items in contextual data to match design

--- src/components/atoms/contextual-data/contextual-data.tsx
+++ src/components/atoms/contextual-data/contextual-data.tsx
@@ -9,7 +9,7 @@ export type ContextualDataProps = {
 export const ContextualData = ({ views, citations, downloads }: ContextualDataProps) => (
   <ul className="contextual-data">
     <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{views}</span> {views === 1 ? 'view' : 'views'}</a></li>
-    <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{citations}</span> {citations === 1 ? 'citation' : 'citations'}</a></li>
     <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{downloads}</span> {downloads === 1 ? 'download' : 'downloads'}</a></li>
+    <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{citations}</span> {citations === 1 ? 'citation' : 'citations'}</a></li>
   </ul>
 );
