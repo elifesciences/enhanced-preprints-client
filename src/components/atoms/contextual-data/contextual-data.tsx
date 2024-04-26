317006465737acb4d4157438324755dd345d7d66
Will Byrne <will.alex.byrne@gmail.com>
2024-04-26 16:13:21 +0100

    make a change to trigger visual-regression

--- src/components/atoms/contextual-data/contextual-data.tsx
+++ src/components/atoms/contextual-data/contextual-data.tsx
@@ -9,8 +9,8 @@ export type ContextualDataProps = {
 
 export const ContextualData = ({ views, citations, downloads }: ContextualDataProps) => (
   <ul className="contextual-data">
-    <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{views}</span> {pluralize('view', views)}</a></li>
     <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{downloads}</span> {pluralize('download', downloads)}</a></li>
     <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{citations}</span> {pluralize('citation', citations)}</a></li>
+    <li className="contextual-data__item"><a className="contextual-data__item--link" href="#metrics"><span className="contextual-data__item--highlight">{views}</span> {pluralize('view', views)}</a></li>
   </ul>
 );
