e619c417b243ad126ad47f4357545c343fa652da
Scott Aubrey <scott@aubrey.org.uk>
2024-05-13 17:53:46 +0100

    add a attribute just for the sake of it

--- src/instrumentation.page.ts
+++ src/instrumentation.page.ts
@@ -1,5 +1,5 @@
 import { registerOTel } from '@vercel/otel';
 
 export function register() {
-  registerOTel({ serviceName: 'epp-client' });
+  registerOTel({ serviceName: 'epp-client', attributes: { experiments: ['otel'] } });
 }
