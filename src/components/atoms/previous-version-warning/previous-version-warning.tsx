3e5eb7652bb028ab151c992d170f4c1527c0596f
Nathan Lisgo <nathan@lisgo.co.uk>
2024-05-17 15:43:21 +0100

    feat(previous-version-warning): update UI and layout
    Modified the previous-version-warning component to enhance its visual appearance. Changes include wrapping the link in a span, adding padding, changing the background, and adjusting the typography. Additionally, updated the styling of the link to improve usability and accessibility.

--- src/components/atoms/previous-version-warning/previous-version-warning.tsx
+++ src/components/atoms/previous-version-warning/previous-version-warning.tsx
@@ -3,6 +3,8 @@ import './previous-version-warning.scss';
 export const PreviousVersionWarning = ({ url }: { url: string }) => (
   <div className="previous-version-warning">
     <span className="previous-version-warning__text">A newer version is available.</span>
-    <a className="previous-version-warning__link" href={encodeURI(url)}>Read the latest version.</a>
+    <span className="previous-version-warning__text">
+      <a className="previous-version-warning__link" href={encodeURI(url)}>Read the latest version</a>.
+    </span>
   </div>
 );
