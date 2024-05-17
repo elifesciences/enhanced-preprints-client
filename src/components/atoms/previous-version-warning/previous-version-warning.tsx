c26f7ec3a19dd3e342aa4ee7735f83ba8a0e82e4
Nathan Lisgo <nathan@lisgo.co.uk>
2024-05-17 16:15:36 +0100

    feat: enhance previous version warning component style
    The commit includes changes in both SCSS and TSX files of the previous
    version warning component. The component is now wrapped inside a
    'previous-version-warning-container' for better styling. Also, the
    background color is now set in the container, and display property is
    updated to 'flex'. The warning message is also now wrapped inside a
    'div' instead of a 'span'.

--- src/components/atoms/previous-version-warning/previous-version-warning.tsx
+++ src/components/atoms/previous-version-warning/previous-version-warning.tsx
@@ -1,10 +1,12 @@
 import './previous-version-warning.scss';
 
 export const PreviousVersionWarning = ({ url }: { url: string }) => (
-  <div className="previous-version-warning">
-    <span className="previous-version-warning__text">A newer version is available.</span>
-    <span className="previous-version-warning__text">
-      <a className="previous-version-warning__link" href={encodeURI(url)}>Read the latest version</a>.
-    </span>
+  <div className="previous-version-warning-container">
+    <div className="previous-version-warning">
+      <span className="previous-version-warning__text">A newer version is available.</span>
+      <span className="previous-version-warning__text">
+        <a className="previous-version-warning__link" href={encodeURI(url)}>Read the latest version</a>.
+      </span>
+    </div>
   </div>
 );
