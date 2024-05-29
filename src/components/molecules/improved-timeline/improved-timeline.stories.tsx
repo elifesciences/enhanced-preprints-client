429dd2d639d708d7258fbeb8a09cb1b2c5a00f57
Nathan Lisgo <nathan@lisgo.co.uk>
2024-05-29 08:33:10 +0100

    address linting issues

--- src/components/molecules/improved-timeline/improved-timeline.stories.tsx
+++ src/components/molecules/improved-timeline/improved-timeline.stories.tsx
@@ -2,15 +2,15 @@ import { StoryFn, Meta } from '@storybook/react';
 import { ImprovedTimeline } from './improved-timeline';
 
 export default {
-    title: 'Molecules/ImprovedTimeline',
-    component: ImprovedTimeline,
-  } as Meta<typeof ImprovedTimeline>;
-  
-  const Template: StoryFn<typeof ImprovedTimeline> = (args) => <ImprovedTimeline {...args} />;
-  
-  export const EventTimeline = Template.bind({});
-  EventTimeline.args = {
-    items: [
-      { name: 'Reviewed preprint', date: 'March 18, 2023' },
-    ],
-  };
+  title: 'Molecules/ImprovedTimeline',
+  component: ImprovedTimeline,
+} as Meta<typeof ImprovedTimeline>;
+
+const Template: StoryFn<typeof ImprovedTimeline> = (args) => <ImprovedTimeline {...args} />;
+
+export const EventTimeline = Template.bind({});
+EventTimeline.args = {
+  items: [
+    { name: 'Reviewed preprint', date: 'March 18, 2023' },
+  ],
+};
