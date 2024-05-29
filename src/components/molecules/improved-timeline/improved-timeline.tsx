954a98e045c8978d4c96058ab5854dd055152966
Nathan Lisgo <n.lisgo@elifesciences.org>
2024-05-29 12:48:15 +0100

    [#1097]: Introduce improved timeline component with initial styles (#1935)
    Co-authored-by: aishakapan <a.kapan@elifesciences.org>

---
+++ src/components/molecules/improved-timeline/improved-timeline.tsx
@@ -0,0 +1,33 @@
+import { Fragment } from 'react';
+import './improved-timeline.scss';
+
+type ImprovedTimelineItem = {
+  version: number,
+  date: string,
+};
+
+type ImprovedTimelineProps = {
+  items: Array<ImprovedTimelineItem>,
+};
+
+const formatDate = (date: string): string => new Date(date).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
+export const ImprovedTimeline = ({ items }: ImprovedTimelineProps) => (
+  <dl className="improved-review-timeline">
+    {
+      items.map((item, index) => {
+        const typeClass = `improved-review-timeline__${item.version > 1 ? 'revised' : 'reviewed'}`;
+        return (
+          <Fragment key={index}>
+            <dt className={`improved-review-timeline__event ${typeClass}`}>Reviewed Preprint</dt>
+            <dd className={`improved-review-timeline__detail ${typeClass}`}>
+              <span className="improved-review-timeline__version">{`v${item.version}`}</span>
+              <time className="improved-review-timeline__date" dateTime={item.date.toString()}>{formatDate(item.date)}</time>
+              <span className="improved-review-timeline__description">{item.version > 1 ? 'Revised by authors' : 'Not revised'}</span>
+            </dd>
+          </Fragment>
+        );
+      })
+    }
+  </dl>
+);
