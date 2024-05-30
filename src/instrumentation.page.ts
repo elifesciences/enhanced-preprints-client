392b89c038e8df04262ac155a679cdb71138a5e3
Scott Aubrey <scott@aubrey.org.uk>
2024-05-30 13:20:48 +0100

    Revert "add a attribute just for the sake of it"
    This reverts commit e619c417b243ad126ad47f4357545c343fa652da.

--- src/instrumentation.page.ts
+++ src/instrumentation.page.ts
@@ -1,5 +1,5 @@
 import { registerOTel } from '@vercel/otel';
 
 export function register() {
-  registerOTel({ serviceName: 'epp-client', attributes: { experiments: ['otel'] } });
+  registerOTel({ serviceName: 'epp-client' });
 }
