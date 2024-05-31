db7ea1db4265fcab0050cae0b28f7196c1de270c
Nathan Lisgo <nathan@lisgo.co.uk>
2024-05-31 15:10:00 +0100

    [#1099]: update expansion text in improved-timeline component

--- src/components/molecules/improved-timeline/improved-timeline.tsx
+++ src/components/molecules/improved-timeline/improved-timeline.tsx
@@ -23,7 +23,7 @@ export const ImprovedTimeline = ({ current, events }: ImprovedTimelineProps) => 
 
   useEffect(() => setExpanded(false), []);
   const displayEvents = events.filter((event, index) => expanded || (current && event.version === current) || (!current && index === 0));
-  const expansionText = expanded ? 'Hide all versions' : 'Show all versions';
+  const expansionText = `${expanded ? 'Hide' : 'Show'} ${events.length > 1 && events[0].version === current ? `previous version${events.length > 2 ? 's' : ''}` : 'all versions'}`;
   const { t } = useTranslation();
   return (
     <dl className="improved-review-timeline">
