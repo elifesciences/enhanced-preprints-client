6e85e0f7b6c6137cc9f79658288e5da8078b183e
Will Byrne <will.alex.byrne@gmail.com>
2024-06-04 17:19:21 +0100

    [#1101] add hardcoded 85111 data to version-history component

--- src/components/atoms/version-history/version-history.tsx
+++ src/components/atoms/version-history/version-history.tsx
@@ -1,10 +1,33 @@
+import './version-history.scss';
+
 export const VersionHistory = () => (
-  <>
-    <h4>Versions</h4>
+  <section className="version-history">
+    <h3>Versions</h3>
     <ul>
       <li>
-        banana
+        <span>Preprint posted: </span>
+        <a href="https://doi.org/10.1101/2022.11.08.515698"><time>November 22, 2022</time> (view preprint)</a>
+      </li>
+      <li>
+        <span>Recieved: </span>
+        <time>November 23, 2022</time>
+      </li>
+      <li>
+        <span>Sent for peer review: </span>
+        <time>November 28, 2022</time>
+      </li>
+      <li>
+        <span>Preprint posted: </span>
+        <a href="https://doi.org/10.7554/eLife.85111.1"><time>January 25, 2023</time> (view preprint)</a>
+      </li>
+      <li>
+        <span>Preprint posted: </span>
+        <a href="https://doi.org/10.7554/eLife.85111.2"><time>May 10, 2023</time> (view preprint)</a>
+      </li>
+      <li>
+        <span>Version of Record published: </span>
+        <a href="https://elifesciences.org/articles/85111"><time>June 7, 2023</time> (version 1)?</a>
       </li>
     </ul>
-  </>
+  </section>
 );
