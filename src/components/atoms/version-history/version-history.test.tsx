9f3c7f5cfa702db969fbb8b57862ba110f166498
aishakapan <a.kapan@elifesciences.org>
2024-06-05 12:31:50 +0100

    [#1101]: Remove redundant label test + add test for correct order display

--- src/components/atoms/version-history/version-history.test.tsx
+++ src/components/atoms/version-history/version-history.test.tsx
@@ -46,6 +46,23 @@ describe('Version History', () => {
     render(<VersionHistory versions={versions}/>);
     expect(document.querySelector('A')).toHaveAttribute('href', 'www.elifesciences.org');
   });
-  it.todo('has the correct link label when set');
-  it.todo('displays the versions in the expected order');
+
+  it.failing('displays the versions in the expected order', () => {
+    const versions = [
+      {
+        label: 'Label',
+        date: new Date('2024-06-05'),
+      },
+      {
+        label: 'Label',
+        date: new Date('2023-06-05'),
+      },
+    ];
+
+    render(<VersionHistory versions={versions}/>);
+    const dates = Array.from(document.querySelectorAll('TIME')).map((node) => node.innerHTML);
+
+    expect(dates[0]).toStrictEqual('June 5, 2023');
+    expect(dates[1]).toStrictEqual('June 5, 2024');
+  });
 });
