c72f0309dc1ce46322e21fc549689d69e4c688ce
Nathan Lisgo <nathan@lisgo.co.uk>
2024-06-05 12:17:05 +0100

    [#1101]: improve readability of version history list

--- src/components/atoms/version-history/version-history.tsx
+++ src/components/atoms/version-history/version-history.tsx
@@ -13,28 +13,22 @@ export const VersionHistory = ({ versions }: Props) => (
     <h3>Versions</h3>
     <ul>
       <li>
-        <a href="https://doi.org/10.1101/2022.11.08.515698"><span>Preprint posted: </span></a>
-        <time>November 22, 2022</time>
+        <a href="https://doi.org/10.1101/2022.11.08.515698">Preprint posted</a>: <time>November 22, 2022</time>
       </li>
       <li>
-        <span>Sent for peer review: </span>
-        <time>November 28, 2022</time>
+        Sent for peer review: <time>November 28, 2022</time>
       </li>
       <li>
-        <a href="https://doi.org/10.7554/eLife.85111.1"><span>Reviewed Preprint version 1: </span></a>
-        <time>January 25, 2023</time>
+        <a href="https://doi.org/10.7554/eLife.85111.1">Reviewed Preprint version 1</a>: <time>January 25, 2023</time>
       </li>
       <li>
-        <a href="https://doi.org/10.7554/eLife.85111.2"><span>Reviewed Preprint version 2: </span></a>
-        <time>May 10, 2023</time>
+        <a href="https://doi.org/10.7554/eLife.85111.2">Reviewed Preprint version 2</a>: <time>May 10, 2023</time>
       </li>
       <li>
-        <a href="https://elifesciences.org/articles/85111"><span>Version of Record published: </span></a>
-        <time>June 7, 2023</time>
+        <a href="https://elifesciences.org/articles/85111">Version of Record published</a>: <time>June 7, 2023</time>
       </li>
       <li>
-        <a href="https://elifesciences.org/articles/85111"><span>Version of Record updated: </span></a>
-        <time>June 15, 2023</time>
+        <a href="https://elifesciences.org/articles/85111">Version of Record updated</a>: <time>June 15, 2023</time>
       </li>
     </ul>
   </section>
