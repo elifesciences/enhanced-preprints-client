32903d26827d37c128cbe1df31b33e06083f31a3
Will Byrne <will.alex.byrne@gmail.com>
2024-06-10 10:36:18 +0100

    merge master into branch

--- src/utils/generators/generate-author-id.test.ts
+++ src/utils/generators/generate-author-id.test.ts
@@ -1,15 +1,15 @@
-import { createAuthorId } from './create-author-id';
-import { authors } from './mocks';
+import { generateAuthorId } from './generate-author-id';
+import { authors } from '../mocks';
 
 describe('CreateAuthorId', () => {
   it('should return a string starting with an x', () => {
-    const id = createAuthorId(authors[0]);
+    const id = generateAuthorId(authors[0]);
 
     expect(id.at(0)).toStrictEqual('x');
   });
 
   it('should return a string starting with the correct id for the author', () => {
-    const id = createAuthorId(authors[0]);
+    const id = generateAuthorId(authors[0]);
 
     expect(id).toStrictEqual('x-666072093');
   });
