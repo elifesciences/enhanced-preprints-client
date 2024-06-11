736b0eb7bcade6ae726f113e88ced3748e7ff4d9
Nathan Lisgo <nathan@lisgo.co.uk>
2024-06-11 19:48:08 +0100

    modify hidden condition in timeline component [elifesciences/enhanced-preprints-issues#1112]

--- src/components/molecules/timeline/timeline.tsx
+++ src/components/molecules/timeline/timeline.tsx
@@ -22,7 +22,7 @@ export const Timeline = ({ current, events }: TimelineProps) => {
       {
         sortedEvents.map((event, index) => {
           const typeClass = (sortedEvents.length === 1 || (current && current === event.version)) ? (` review-timeline__event--${event.version > 1 ? 'revised' : 'reviewed'}`) : '';
-          const hidden = (current && current !== event.version && !expanded);
+          const hidden = (current && current !== event.version && expanded === false);
           const eventName = event.name ?? 'Reviewed Preprint';
           return (
             <Fragment key={index}>
