ddd13f29cb886b521d28fc40949fea1689de25e4
Nathan Lisgo <nathan@lisgo.co.uk>
2024-06-11 19:14:53 +0100

    add expanded state styling to review timeline [elifesciences/enhanced-preprints-issues#1112]
    The review timeline component has been updated to include expanded state
    styling. This includes a new class for expanded state in the timeline.tsx
    file and modified margin in the timeline.scss file for the timeline event
    when in an expanded state.

--- src/components/molecules/timeline/timeline.tsx
+++ src/components/molecules/timeline/timeline.tsx
@@ -18,7 +18,7 @@ export const Timeline = ({ current, events }: TimelineProps) => {
   const expansionText = `${expanded ? 'Hide' : 'Show'} ${sortedEvents.length > 1 && sortedEvents[0].version === current ? `previous version${sortedEvents.length > 2 ? 's' : ''}` : 'all versions'}`;
   const { t } = useTranslation();
   return (
-    <dl className="review-timeline" id="review-timeline">
+    <dl className={`review-timeline${expanded !== false ? ' review-timeline--expanded' : ''}`} id="review-timeline">
       {
         sortedEvents.map((event, index) => {
           const typeClass = (sortedEvents.length === 1 || (current && current === event.version)) ? (` review-timeline__event--${event.version > 1 ? 'revised' : 'reviewed'}`) : '';
