68b668c3a65fecc3f7ff9b8e0bfb199a95e4adac
Scott Aubrey <scott@aubrey.org.uk>
2024-06-11 16:43:09 +0100

    feat: Add type converter for automatically change all Date types to strings

---
+++ src/utils/type-converters.ts
@@ -0,0 +1,8 @@
+// utility type to turn a PageProps defined with Dates into one defined with strings
+export type DatesToStrings<T> = T extends Date
+  ? string
+  : T extends Array<infer U>
+    ? DatesToStrings<U>[]
+    : T extends object
+      ? { [K in keyof T]: DatesToStrings<T[K]> }
+      : T;
