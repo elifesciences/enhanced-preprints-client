1095e6daa5c909cd32d46316d299cf87772e7512
aishakapan <a.kapan@elifesciences.org>
2024-06-12 17:17:01 +0100

    Add some text styling [elifesciences/enhanced-preprints-issues#1114]

--- src/components/atoms/review-process/review-process.tsx
+++ src/components/atoms/review-process/review-process.tsx
@@ -1,7 +1,11 @@
+import './review-process.scss'
+
 export const ReviewProcess = () => (
-  <div>
-    <h2>Peer review process</h2>
-    <p> <b>Not revised:</b> This Reviewed Preprint includes the authors’ original preprint (without revision), an eLife assessment, public reviews, and a response from the authors (if available).</p>
-    <a href="#">Read more about eLife’s peer review process.</a>
-  </div>
+  <section className="review-process">
+  <h2 className="review-process__header">Peer review process</h2>
+    <div className="review-process__container">
+      <p className="review-process__body"><b>Not revised:</b> This Reviewed Preprint includes the authors’ original preprint (without revision), an eLife assessment, public reviews, and a response from the authors (if available).</p>
+      <a className="review-process__link" href="#">Read more about eLife’s peer review process.</a>
+    </div>
+  </section>
 );
