48b76609df7b8a7987e3f8424e80df89df5a78f0
aishakapan <a.kapan@elifesciences.org>
2024-06-12 17:40:51 +0100

    add new classes and generalise the colour coding across timeline and review process [elifesciences/enhanced-preprints-issues#1114]

--- src/components/atoms/review-process/review-process.tsx
+++ src/components/atoms/review-process/review-process.tsx
@@ -5,7 +5,7 @@ export const ReviewProcess = () => (
   <h2 className="review-process__header">Peer review process</h2>
     <div className="review-process__container">
       <p className="review-process__body"><b>Not revised:</b> This Reviewed Preprint includes the authors’ original preprint (without revision), an eLife assessment, public reviews, and a response from the authors (if available).</p>
-      <a className="review-process__link" href="#">Read more about eLife’s peer review process.</a>
+      <a className="review-process__link link-reviewed" href="#">Read more about eLife’s peer review process.</a>
     </div>
   </section>
 );
