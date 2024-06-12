4139d4bd8f8bd6a6dd5f17ef2562992f575524d3
Nathan Lisgo <nathan@lisgo.co.uk>
2024-06-12 18:31:30 +0100

    Update review process component [elifesciences/enhanced-preprints-issues#1114]
    Update review process component to include correct syntax and test description. Also, ensure correct line ending in SCSS file.

--- src/components/atoms/review-process/review-process.test.tsx
+++ src/components/atoms/review-process/review-process.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { ReviewProcess } from './review-process';
 
 describe('Review Process', () => {
-  it('renders review process description in the peer review tab', () => {
+  it('renders review process', () => {
     render(<ReviewProcess />);
 
     const firstItem = screen.getByText('Peer review process');
--- src/components/atoms/review-process/review-process.tsx
+++ src/components/atoms/review-process/review-process.tsx
@@ -1,8 +1,8 @@
-import './review-process.scss'
+import './review-process.scss';
 
 export const ReviewProcess = () => (
   <section className="review-process">
-  <h2 className="review-process__header">Peer review process</h2>
+    <h2 className="review-process__header">Peer review process</h2>
     <div className="review-process__container">
       <p className="review-process__body"><b>Not revised:</b> This Reviewed Preprint includes the authors’ original preprint (without revision), an eLife assessment, public reviews, and a response from the authors (if available).</p>
       <a className="review-process__link link-reviewed" href="#">Read more about eLife’s peer review process.</a>
