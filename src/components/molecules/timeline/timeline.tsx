1f073a1bb674bc0030ebfdc153702c8435e170e2
Nathan Lisgo <nathan@lisgo.co.uk>
2024-06-13 22:14:27 +0100

    Wrap review timeline in a container [elifesciences/enhanced-preprints-issues#1113]
    Added a div container to wrap the review timeline for better layout control. Also updated the related CSS class from '.review-timeline' to '.review-timeline-container'.

--- src/components/molecules/timeline/timeline.tsx
+++ src/components/molecules/timeline/timeline.tsx
@@ -18,34 +18,40 @@ export const Timeline = ({ current, events }: TimelineProps) => {
   const expansionText = `${expanded ? 'Hide' : 'Show'} ${sortedEvents.length > 1 && sortedEvents[0].version === current ? `previous version${sortedEvents.length > 2 ? 's' : ''}` : 'all versions'}`;
   const { t } = useTranslation();
   return (
-    <dl className={`review-timeline${expanded !== false ? ' review-timeline--expanded' : ''}`} id="review-timeline">
-      {
-        sortedEvents.map((event, index) => {
-          const typeClass = (sortedEvents.length === 1 || (current && current === event.version)) ? (` review-timeline__event--${event.version > 1 ? 'revised' : 'reviewed'}`) : '';
-          const hidden = (current && current !== event.version && expanded === false);
-          const eventName = event.name ?? 'Reviewed Preprint';
-          return (
-            <Fragment key={index}>
-              <dt className={`review-timeline__event${typeClass}`}{...(hidden ? { style: { display: 'none' } } : {})}>
-                {(current && current !== event.version) ? (
-                  <a href={event.url} className="review-timeline__event-link">
-                    {eventName}
-                  </a>
-                ) : (
-                  eventName
-                )}
-              </dt>
-              <dd className={`review-timeline__detail${typeClass}`} style={hidden ? { display: 'none' } : {}}>
-                {event.versionIndicator && <span className="review-timeline__version">{event.versionIndicator}</span>}
-                <time className="review-timeline__date" dateTime={event.date.toISOString()}>{formatDate(event.date)}</time>
-                {typeClass && <a className="review-timeline__link" href={t('process_url')}>{event.version > 1 ? 'Revised by authors' : 'Not revised'}</a>}
-              </dd>
-            </Fragment>
-          );
-        })
-      }
-      {(sortedEvents.length > 1 && expanded !== null) &&
-      <button aria-controls="review-timeline" aria-expanded={expanded} className={`review-timeline__expansion${expanded ? ' review-timeline__expansion--expanded' : ''}`} onClick={() => setExpanded(!expanded)}>{expansionText}</button>}
-    </dl>
+    <div className="review-timeline-container">
+      <dl className={`review-timeline${expanded !== false ? ' review-timeline--expanded' : ''}`} id="review-timeline">
+        {
+          sortedEvents.map((event, index) => {
+            const typeClass = (sortedEvents.length === 1 || (current && current === event.version)) ? (` review-timeline__event--${event.version > 1 ? 'revised' : 'reviewed'}`) : '';
+            const hidden = (current && current !== event.version && expanded === false);
+            const eventName = event.name ?? 'Reviewed Preprint';
+            return (
+              <Fragment key={index}>
+                <dt className={`review-timeline__event${typeClass}`}{...(hidden ? { style: { display: 'none' } } : {})}>
+                  {(current && current !== event.version) ? (
+                    <a href={event.url} className="review-timeline__event-link">
+                      {eventName}
+                    </a>
+                  ) : (
+                    eventName
+                  )}
+                </dt>
+                <dd className={`review-timeline__detail${typeClass}`} style={hidden ? { display: 'none' } : {}}>
+                  {event.versionIndicator && <span className="review-timeline__version">{event.versionIndicator}</span>}
+                  <time className="review-timeline__date"
+                    dateTime={event.date.toISOString()}>{formatDate(event.date)}</time>
+                  {typeClass && <a className="review-timeline__link"
+                    href={t('process_url')}>{event.version > 1 ? 'Revised by authors' : 'Not revised'}</a>}
+                </dd>
+              </Fragment>
+            );
+          })
+        }
+        {(sortedEvents.length > 1 && expanded !== null) &&
+        <button aria-controls="review-timeline" aria-expanded={expanded}
+          className={`review-timeline__expansion${expanded ? ' review-timeline__expansion--expanded' : ''}`}
+          onClick={() => setExpanded(!expanded)}>{expansionText}</button>}
+      </dl>
+    </div>
   );
 };
