930300469dd2b268e36854a92d4ed91d54839262
Will Byrne <will.alex.byrne@gmail.com>
2024-06-17 12:24:40 +0100

    wip to fix an issue with array processing [elifesciences/enhanced-preprints-issues#1106]

--- src/utils/type-converters.test.ts
+++ src/utils/type-converters.test.ts
@@ -12,4 +12,25 @@ describe('Type Converters', () => {
 
     expect(output.foo).toEqual(expect.any(Date));
   });
+
+  it('converts dates in arrays', () => {
+    type Bar = {
+      bar: { bar: Date }[],
+      bars: Date[],
+    };
+    const input: DatesToStrings<Bar> = {
+      bar: [{ bar: '2022-01-13' }, { bar: '2022-02-13' }],
+      bars: ['2022-01-13', '2022-01-13'],
+    };
+    const output = stringsToDates<Bar>(input);
+
+    expect(output.bar[0].bar).toEqual(expect.any(Date));
+    expect(output.bar[0].bar).toStrictEqual(new Date('2022-01-13'));
+    expect(output.bar[1].bar).toEqual(expect.any(Date));
+    expect(output.bar[1].bar).toStrictEqual(new Date('2022-02-13'));
+    expect(output.bars[0]).toEqual(expect.any(Date));
+    expect(output.bars[0]).toStrictEqual(new Date('2022-01-13'));
+    expect(output.bars[1]).toEqual(expect.any(Date));
+    expect(output.bars[1]).toStrictEqual(new Date('2022-02-13'));
+  });
 });
--- src/utils/type-converters.ts
+++ src/utils/type-converters.ts
@@ -6,18 +6,26 @@ export type DatesToStrings<T> = T extends Date
       ? { [K in keyof T]: DatesToStrings<T[K]> }
       : T;
 
-export const stringsToDates = <T extends object>(input: DatesToStrings<T>): T => {
+export const stringsToDates = <T extends object>(input: DatesToStrings<T>): T extends Array<T> ? T[] : T => {
   // @ts-ignore
-  const output = { ...input };
-  // eslint-disable-next-line no-restricted-syntax
-  for (const [key, val] of Object.entries(output as T)) {
-    if (typeof val === 'string' && !Number.isNaN(new Date(val).getTime())) {
-      output[key] = new Date(val);
-    } else if (Array.isArray(output[key])) {
-      output[key] = val.map(stringsToDates);
-    } else if (typeof val === 'object') {
-      output[key] = stringsToDates(val);
+  let output;
+  if (Array.isArray(input)) {
+    output = input.map(stringsToDates);
+  } else if (typeof input === 'object') {
+    output = { ...input };
+    // eslint-disable-next-line no-restricted-syntax
+    for (const [key, val] of Object.entries(output as T)) {
+      if (typeof val === 'string' && !Number.isNaN(new Date(val).getTime())) {
+        console.log(val);
+        output[key] = new Date(val);
+      } else if (Array.isArray(output[key])) {
+        output[key] = val.map(stringsToDates);
+      } else if (typeof val === 'object') {
+        output[key] = stringsToDates(val);
+      }
     }
+  } else if (typeof input === 'string' && !Number.isNaN(new Date(input).getTime())) {
+    return new Date(input);
   }
 
   return output as T;
