0185b0c5c0d2fe2cd70dcf5fd6e78acb7b6ded94
Will Byrne <will.alex.byrne@gmail.com>
2024-07-01 10:46:54 +0100

    fix broken timeline test

--- src/components/molecules/timeline/timeline.stories.tsx
+++ src/components/molecules/timeline/timeline.stories.tsx
@@ -136,6 +136,6 @@ export const EventTimelineWithMultipleVOR: Story = {
     ],
   },
   play: async ({ canvasElement }) => {
-    await timeLineTest(canvasElement, 'Show all versions', 'Hide all versions', 5);
+    await timeLineTest(canvasElement, 'Show all versions', 'Hide all versions', 4);
   },
 };
