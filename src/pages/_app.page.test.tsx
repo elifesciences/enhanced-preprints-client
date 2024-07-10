321d947ec48a2e3dd1065c27597a6e59527a41f2
Nathan Lisgo <nathan@lisgo.co.uk>
2024-07-10 14:34:45 +0100

    Update MyApp component tests to check layout rendering based on siteName property [elifesciences/enhanced-preprints-issues#1138]

--- src/pages/_app.page.test.tsx
+++ src/pages/_app.page.test.tsx
@@ -1,4 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import MyApp from './_app.page';
+
 describe('MyApp', () => {
-  it.todo('renders default layout');
-  it.todo('renders alternative layout when siteName property is set');
+  it.each([
+    {
+      name: 'siteName is null',
+      siteName: null,
+    },
+    {
+      name: 'siteName is unknown',
+      siteName: 'unknown',
+    },
+  ])('renders default layout ($name)', ({ siteName }) => {
+    render(<MyApp Component={() => <div>Test</div>} pageProps={{ siteName }} />);
+    expect(screen.getByAltText('eLife logo')).toBeInTheDocument();
+  });
+
+  it('renders alternative layout when siteName property is set', () => {
+    render(<MyApp Component={() => <div>Test</div>} pageProps={{ siteName: 'biophysics-colab' }} />);
+    expect(screen.getByAltText('Biophysics Colab logo')).toBeInTheDocument();
+  });
 });
