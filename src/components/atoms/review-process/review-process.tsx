16a14a1bde94fcfbcbab08b4867da9a2727319d7
Nathan Lisgo <nathan@lisgo.co.uk>
2024-07-11 23:00:51 +0100

    Fix setting of class in review-process section [elifesciences/enhanced-preprints-issues#1140]

--- src/components/atoms/review-process/review-process.tsx
+++ src/components/atoms/review-process/review-process.tsx
@@ -10,12 +10,12 @@ type ReviewProcessProps = {
 
 export const ReviewProcess = ({ current, authorResponse }: ReviewProcessProps) => {
   const { t } = useTranslation();
-  const type = current && current > 1 ? 'revised' : `reviewed${authorResponse === true ? '_with_author_response' : ''}`;
+  const type = current && current > 1 ? 'revised' : 'reviewed';
   return (
     <section className={`review-process review-process--${type}`}>
       <h2 className="review-process__header" id="review-process">Peer review process</h2>
       <div className="review-process__container">
-        <p className="review-process__body">{parse(t(`review_process_${type}`))}</p>
+        <p className="review-process__body">{parse(t(`review_process_${type}${type === 'reviewed' && authorResponse === true ? '_with_author_response' : ''}`))}</p>
         <a className="review-process__link" href={t('process_url')}>Read more about eLife’s peer review process.</a>
       </div>
     </section>
