af5dd03d13c80a6388c77e4324d3ea462ad3aaf7
aishakapan <a.kapan@elifesciences.org>
2024-09-03 10:01:23 +0100

    Add term description above the list

--- src/components/atoms/terms-list/terms-list.tsx
+++ src/components/atoms/terms-list/terms-list.tsx
@@ -1,10 +1,13 @@
 export const TermsList = () => {
   const terms = ['Landmark', 'Fundamental', 'Important', 'Valuable', 'Useful'];
   return (
-    <div className="terms-list">
-      {terms.map((term) => (
-        <span className="term" key={term}>{term}</span>
-      ))}
-    </div>
+    <>
+      <p><b>Valuable</b>: Contains findings that have theoretical or practical implications for a subfield.</p>
+      <div className="terms-list">
+        {terms.map((term) => (
+          <span className="term" key={term}>{term}</span>
+        ))}
+      </div>
+    </>
   );
 };
