ffa5f6e7418a7b4608e99ee8a37325e2b1dde3ae
aishakapan <a.kapan@elifesciences.org>
2024-09-03 12:06:27 +0100

    Add some styling and html strcuture changes

--- src/components/atoms/terms-list/terms-list.tsx
+++ src/components/atoms/terms-list/terms-list.tsx
@@ -1,13 +1,17 @@
+import './terms-list.scss';
+
 export const TermsList = () => {
   const terms = ['Landmark', 'Fundamental', 'Important', 'Valuable', 'Useful'];
   return (
-    <>
-      <p><b>Valuable</b>: Contains findings that have theoretical or practical implications for a subfield.</p>
+    <div className='terms-container'>
+      <p className='term-description'>
+        <b>Valuable</b>: Contains findings that have theoretical or practical implications for a subfield.
+      </p>
       <div className="terms-list">
         {terms.map((term) => (
           <span className="term" key={term}>{term}</span>
         ))}
       </div>
-    </>
+    </div>
   );
 };
