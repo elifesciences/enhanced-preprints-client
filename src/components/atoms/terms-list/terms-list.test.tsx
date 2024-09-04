2dbc372788822aae298e0454ef14ec34610a3ccb
Will Byrne <will.alex.byrne@gmail.com>
2024-09-04 11:54:01 +0100

    add test for term-list component

--- src/components/atoms/terms-list/terms-list.test.tsx
+++ src/components/atoms/terms-list/terms-list.test.tsx
@@ -1,3 +1,26 @@
+import { render } from '@testing-library/react';
+import { TermsList } from './terms-list';
+
 describe('TermsList', () => {
-  it.todo('write tests');
+  it('renders the selected term and its description', () => {
+    render(<TermsList terms={['solid', 'two', 'three']} selectedTerm={['solid']} />);
+
+    expect(document.querySelector('.term-description b')).toHaveTextContent('solid');
+    expect(document.querySelector('.term-description')).toHaveTextContent('solid: Methods, data and analyses broadly support the claims with only minor weaknesses');
+  });
+
+  it('renders a list of terms passed in', () => {
+    render(<TermsList terms={['one', 'two', 'three']} selectedTerm={['one']} />);
+
+    expect(document.querySelector('.term:first-child')).toHaveTextContent('one');
+    expect(document.querySelector('.term:nth-child(2)')).toHaveTextContent('two');
+    expect(document.querySelector('.term:nth-child(3)')).toHaveTextContent('three');
+  });
+
+  it('highlights the terms that are passed in', () => {
+    render(<TermsList terms={['one', 'two', 'three']} selectedTerm={['one', 'two']} />);
+
+    expect(document.querySelector('.term__highlighted:first-child')).toHaveTextContent('one');
+    expect(document.querySelector('.term__highlighted:nth-child(2)')).toHaveTextContent('two');
+  });
 });
