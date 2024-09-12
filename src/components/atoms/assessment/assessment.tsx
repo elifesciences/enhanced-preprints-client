f856d265df3695c8dd60b273e47f33e7b1bc26be
Will Byrne <will.alex.byrne@gmail.com>
2024-09-12 10:55:20 +0100

    remove capitalised A on assessment in fixed text

--- src/components/atoms/assessment/assessment.tsx
+++ src/components/atoms/assessment/assessment.tsx
@@ -23,7 +23,7 @@ export const Assessment = ({ content }: Props) => {
         { (significance && significance.length > 0) && <TermsList title="Significance of findings" terms={significanceTerms} selectedTerm={significance} /> }
         { (strength && strength.length > 0) && <TermsList title="Strength of evidence" terms={strengthTerms} selectedTerm={findTerms(content).strength || []} /> }
         {/* eslint-disable-next-line max-len */}
-        <p className="assessment__fixed_text">During the peer-review process the editor and reviewers write an eLife Assessment that summarises the significance of the findings reported in the article (on a scale ranging from landmark to useful) and the strength of the evidence (on a scale ranging from exceptional to inadequate). <a href="https://elifesciences.org/inside-elife/db24dd46">Learn more about eLife Assessments</a></p>
+        <p className="assessment__fixed_text">During the peer-review process the editor and reviewers write an eLife assessment that summarises the significance of the findings reported in the article (on a scale ranging from landmark to useful) and the strength of the evidence (on a scale ranging from exceptional to inadequate). <a href="https://elifesciences.org/inside-elife/db24dd46">Learn more about eLife assessments</a></p>
       </div>
       <span aria-expanded={isToggled} className={`explanation_link ${isToggled ? 'explanation_link__expanded' : ''}`} onClick={toggleText}> {isToggled ? 'Show less' : 'Read more about this assessment'}</span>
     </section>
