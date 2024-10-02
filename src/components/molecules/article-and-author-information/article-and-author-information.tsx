c6dacb017200a8f5efc8b5a433d36e1e8e54b025
Will Byrne <will.alex.byrne@gmail.com>
2024-10-02 15:18:10 +0100

    add generic footnotes and a class in the sass file

--- src/components/molecules/article-and-author-information/article-and-author-information.tsx
+++ src/components/molecules/article-and-author-information/article-and-author-information.tsx
@@ -14,6 +14,14 @@ export const ArticleAndAuthorInformation = ({
   <section>
     <h1 id="article-and-author-information" className="article-and-author-information__title">Article and author information</h1>
     { authors.length > 0 && <AuthorList authors={authors} authorNotes={authorNotes} /> }
+    { authorNotes.length > 0 && (
+        <div className="author-notes">
+          <h2 className="author-notes__title">Author Notes</h2>
+          <ul className="author-notes__list">
+            { authorNotes.filter(({ id }) => id === undefined).map(({ text }, index) => <p key={index} className="author-notes__list_item">{text}</p>)}
+          </ul>
+        </div>
+      )}
     { versions.length > 0 && <VersionHistory versions={versions} /> }
     { license && <Copyright license={license} publishedYear={publishedYear} authors={authors} /> }
   </section>
