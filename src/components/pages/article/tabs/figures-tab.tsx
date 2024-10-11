dea4e8de2e34718739f5b721971df0beea719eb7
Nathan Lisgo <n.lisgo@elifesciences.org>
2024-10-11 12:40:05 +0100

    Move eLife assessment above abstract [elifesciences/enhanced-preprints-issues#1165] (#2157)
    --------
    
    Co-authored-by: Will Byrne <will.alex.byrne@gmail.com>

--- src/components/pages/article/tabs/figures-tab.tsx
+++ src/components/pages/article/tabs/figures-tab.tsx
@@ -4,7 +4,7 @@ import { ArticleContent } from '../../../atoms/article-content/article-content';
 import { JSXContent } from '../../../../utils/content';
 
 export const ArticleFiguresTab = ({ content }: { content: JSXContent }) => (
-  <div className="tabbed-navigation__content">
+  <div className="tabbed-navigation__content tabbed-navigation__content--figures">
     <div className="menu-spacer"/>
     <div className="article-body-container">
       <Heading id="figures" headingLevel={2} content="Figures and data" />
