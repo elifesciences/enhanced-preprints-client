31f6142eb6686c096482f0a669e59e4a2cbe2dc9
Scott Aubrey <scott@aubrey.org.uk>
2024-11-04 08:11:30 +0000

    render all review-process stories with elife i18n set

--- src/components/atoms/review-process/review-process.stories.tsx
+++ src/components/atoms/review-process/review-process.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { ReviewProcess } from './review-process';
 import '../../../i18n';
+import { I18nextProvider } from 'react-i18next';
+import { i18n } from '../../../i18n';
 
 const meta: Meta<typeof ReviewProcess> = {
   title: 'Atoms/Review Process',
@@ -14,6 +16,7 @@ export const NotRevisedReviewProcess: Story = {
   args: {
     current: 1,
   },
+  render: (args) => <I18nextProvider i18n={i18n} defaultNS="elife"><ReviewProcess {...args} /></I18nextProvider>,
 };
 
 export const NotRevisedReviewProcessWithAuthorResponse: Story = {
@@ -21,10 +24,12 @@ export const NotRevisedReviewProcessWithAuthorResponse: Story = {
     current: 1,
     authorResponse: true,
   },
+  render: (args) => <I18nextProvider i18n={i18n} defaultNS="elife"><ReviewProcess {...args} /></I18nextProvider>,
 };
 
 export const RevisedReviewProcess: Story = {
   args: {
     current: 2,
   },
+  render: (args) => <I18nextProvider i18n={i18n} defaultNS="elife"><ReviewProcess {...args} /></I18nextProvider>,
 };
