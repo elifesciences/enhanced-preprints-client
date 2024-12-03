a9cb0ebb763609221d4ac1cd9fa344a645e7ada1
Will Byrne <will.alex.byrne@gmail.com>
2024-12-03 15:53:45 +0000

    lint fix

--- src/components/molecules/timeline/timeline.tsx
+++ src/components/molecules/timeline/timeline.tsx
@@ -1,8 +1,8 @@
 import { Fragment, useEffect, useState } from 'react';
 import './timeline.scss';
+import { useTranslation } from 'react-i18next';
 import { formatDate } from '../../../utils/formatters';
 import { TimelineEvent } from '../../../types';
-import { useTranslation } from 'react-i18next';
 
 export type TimelineProps = {
   current?: number;
