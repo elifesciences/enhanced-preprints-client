00eb9dc95a82b3cca3705b2408cd0469fb61d7fb
Will Byrne <will.alex.byrne@gmail.com>
2025-01-27 15:19:24 +0000

    fix tests pointing to wrong parent element

--- src/components/atoms/reference/reference.test.tsx
+++ src/components/atoms/reference/reference.test.tsx
@@ -11,7 +11,7 @@ describe('Reference', () => {
     expect(screen.getByText('Afshari FS')).toBeInTheDocument();
     expect(screen.getByText('1847')).toBeInTheDocument();
     expect(screen.getByText('J. Neurophysiol')).toBeInTheDocument();
-    expect(screen.getByText('Resurgent Na currents in four classes of neurons of the cerebellum').parentElement?.id).toStrictEqual('c1');
+    expect(screen.getByText('Resurgent Na currents in four classes of neurons of the cerebellum').parentElement?.parentElement?.id).toStrictEqual('c1');
     expect(screen.getByText('2843', { exact: false })).toBeInTheDocument();
   });
 
@@ -31,7 +31,7 @@ describe('Reference', () => {
   it('should be wrapped in a div if isReferenceList is false', () => {
     render(<Reference reference={references[0]} isReferenceList={false} />);
 
-    expect(screen.getByText(references[0].title).parentElement?.tagName).toStrictEqual('DIV');
+    expect(screen.getByText(references[0].title).parentElement?.parentElement?.tagName).toStrictEqual('DIV');
   });
 
   it('should not render the label if the reference has one', () => {
@@ -98,7 +98,7 @@ describe('Reference', () => {
     it('should be wrapped in an li if isReferenceList is true', () => {
       render(<Reference reference={references[0]} isReferenceList={true} />);
 
-      expect(screen.getByText(references[0].title).parentElement?.tagName).toStrictEqual('LI');
+      expect(screen.getByText(references[0].title).parentElement?.parentElement?.tagName).toStrictEqual('LI');
     });
 
     it('should wrap doi in link if isReferenceList is true', () => {
