c1a968629e8f3bb11f0f43baf8448affaae825dd
aishakapan <a.kapan@elifesciences.org>
2025-05-12 11:29:51 +0100

    Begin to create a required footer for Biophysics Colab
    elifesciences/enhanced-preprints-issues/issues/1249

--- src/components/layouts/biophysics-colab.tsx
+++ src/components/layouts/biophysics-colab.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 import './biophysics-colab.scss';
+import Image from 'next/image';
+import logo from '../../../public/science-colab-logo.png';
 import { SiteHeaderBiophysicsColab } from '../molecules/site-header/site-header-biophysics-colab';
 
 type Props = {
@@ -14,5 +16,14 @@ export const BiophysicsColabLayout = ({ children }: Props) => (
       </div>
       {children}
     </div>
+    <footer>
+      <p>Part of</p>
+      <a href="https://www.sciencecolab.org/">
+        <Image
+          src={logo}
+          alt="Science Colab logo"
+          />
+      </a>
+    </footer>
   </>
 );
