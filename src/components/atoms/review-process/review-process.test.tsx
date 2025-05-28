f1d913708e216be22539726f1f990be76ef681f7
LinaKind <l.kinduryte@elifesciences.org>
2025-05-28 17:38:11 +0300

    Set version of record class in review process
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1257

--- src/components/atoms/review-process/review-process.test.tsx
+++ src/components/atoms/review-process/review-process.test.tsx
@@ -41,7 +41,7 @@ describe('Review Process', () => {
     expect(document.querySelectorAll('.review-process--with-evaluation-summary')).toHaveLength(1);
   });
 
-  it.failing('shows the description with a version of record class', () => {
+  it('shows the description with a version of record class', () => {
     render(<ReviewProcess current={1} versionOfRecord={true} />);
 
     expect(document.querySelectorAll('.review-process--version-of-record')).toHaveLength(1);
--- src/components/atoms/review-process/review-process.tsx
+++ src/components/atoms/review-process/review-process.tsx
@@ -10,13 +10,14 @@ type ReviewProcessProps = {
 };
 
 export const ReviewProcess = ({
-  current, evaluationSummary, authorResponse,
+  current, evaluationSummary, authorResponse, versionOfRecord,
 }: ReviewProcessProps) => {
   const { t } = useTranslation();
   const type = current && current > 1 ? 'revised' : 'reviewed';
   const evaluationSummaryClass = evaluationSummary ? ' review-process--with-evaluation-summary' : '';
+  const versionOfRecordClass = versionOfRecord ? ' review-process--version-of-record' : '';
   return (
-    <section className={`review-process review-process--${type}${evaluationSummaryClass}`}>
+    <section className={`review-process review-process--${type}${evaluationSummaryClass}${versionOfRecordClass}`}>
       <h2 className="review-process__header" id="review-process">Peer review process</h2>
       <div className="review-process__container">
         <p className="review-process__body">{parse(t(`review_process_${type}${type === 'reviewed' && authorResponse === true ? '_with_author_response' : ''}`))}</p>
