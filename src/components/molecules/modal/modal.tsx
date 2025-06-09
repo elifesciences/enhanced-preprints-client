d999b902706aaacc0aaade1d946379edc0ed2885
aishakapan <a.kapan@elifesciences.org>
2025-06-09 13:00:19 +0100

    Add accessibilty features to the modal component for better screen reading experience
    https://github.com/elifesciences/enhanced-preprints-issues/issues/965

--- src/components/molecules/modal/modal.tsx
+++ src/components/molecules/modal/modal.tsx
@@ -27,13 +27,13 @@ export const Modal = ({
   return (
     <>
       <div onClick={(event) => { if (onModalClose !== undefined && clickDetectedOutsideOfModal(event)) { onModalClose(); } }} className={`modal-container${open ? ' modal-content__show' : ''} `}>
-        <dialog ref={contentRef} className={`modal-content ${modalLayout ? ` modal-content__${modalLayout}` : ''}`}>
+        <dialog ref={contentRef} role="alertdialog" aria-modal="true" aria-labelledby="modal_title" aria-describedby="modal_content" className={`modal-content ${modalLayout ? ` modal-content__${modalLayout}` : ''}`}>
           <div className="modal-content__block">
             <div className="modal-content__top">
-              <h6 className="modal-content__title">{modalTitle}</h6>
+              <h6 id="modal_title" className="modal-content__title">{modalTitle}</h6>
               {onModalClose !== undefined ? (<button className="modal-content__close-button" onClick={onModalClose}>Close</button>) : ''}
             </div>
-            {children !== undefined && open ? (<div className="modal-content__body">{children}</div>) : ''}
+            {children !== undefined && open ? (<div id="modal_content" className="modal-content__body">{children}</div>) : ''}
             {modalWarning && <span className="modal-content__warning">{modalWarning}</span>}
           </div>
         </dialog>
