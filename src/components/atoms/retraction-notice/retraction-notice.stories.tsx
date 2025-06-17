27f150b95bcc88e86c3356e3c0e1ba83923b43ad
Scott Aubrey <scott@aubrey.org.uk>
2025-06-17 14:29:45 +0100

    Improve feedback cycle with a story for RetractionNotice
    https://github.com/elifesciences/issues/issues/9315

---
+++ src/components/atoms/retraction-notice/retraction-notice.stories.tsx
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { RetractionNotice } from './retraction-notice';
+
+const meta: Meta<typeof RetractionNotice> = {
+  title: 'Atoms/Retraction Notice',
+  component: RetractionNotice,
+};
+
+export default meta;
+type Story = StoryObj<typeof RetractionNotice>;
+
+export const RetractionNoticeComponent: Story = {
+  args: {
+    url: 'www.google.com',
+  },
+};
