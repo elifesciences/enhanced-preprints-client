5b0d45bf7283b9b4d4d56d518aa5be80e42b9309
Scott Aubrey <scott@aubrey.org.uk>
2025-06-18 09:27:34 +0100

    Style the layout of the text on different widths
    https://github.com/elifesciences/issues/issues/9315

--- src/components/atoms/retraction-notice/retraction-notice.tsx
+++ src/components/atoms/retraction-notice/retraction-notice.tsx
@@ -1,6 +1,8 @@
 import './retraction-notice.scss';
 
-export const RetractionNotice = ({ url }:{ url: string }) => <div className="retraction-notice">
-  <p>This article has been retracted.</p>
-  <p><a href={url}>Read the retraction notice</a>.</p>
-</div>;
+export const RetractionNotice = ({ url }:{ url: string }) => (
+  <div className="retraction-notice">
+    <p>This article has been retracted.</p>
+    <p><a href={url}>Read the retraction notice</a>.</p>
+  </div>
+);
