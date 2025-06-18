e0ef02f565f93f2f32a5027640c610f5c2d5f328
Scott Aubrey <scott@aubrey.org.uk>
2025-06-18 14:26:26 +0100

    Further style RetractionNotice
    https://github.com/elifesciences/issues/issues/9315

--- src/components/atoms/retraction-notice/retraction-notice.tsx
+++ src/components/atoms/retraction-notice/retraction-notice.tsx
@@ -2,7 +2,7 @@ import './retraction-notice.scss';
 
 export const RetractionNotice = ({ url }:{ url: string }) => (
   <div className="retraction-notice">
-    <p>This article has been retracted.</p>
+    <p>This article is retracted.</p>
     <p><a href={url}>Read the retraction notice</a>.</p>
   </div>
 );
