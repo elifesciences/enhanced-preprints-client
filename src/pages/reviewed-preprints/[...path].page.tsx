99cf8c50091f5febd0e6d2ff244473d1c8604d94
Nathan Lisgo <nathan@lisgo.co.uk>
2025-06-20 13:39:25 +0100

    Resolve compilation error

--- src/pages/reviewed-preprints/[...path].page.tsx
+++ src/pages/reviewed-preprints/[...path].page.tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 import { JSX, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { config } from '../../config';
