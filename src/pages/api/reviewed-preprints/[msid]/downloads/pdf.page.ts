2e9c24c384328ceeaf4f5ed5b9a59eea014f9231
aishakapan <a.kapan@elifesciences.org>
2025-07-09 14:56:06 +0100

    Create a new endpoint for pdf redirect
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1270

---
+++ src/pages/api/reviewed-preprints/[msid]/downloads/pdf.page.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { fetchVersion } from '../../../../../utils/data-fetch';
+import { redirect } from 'next/navigation';
+import { errorNotFoundRequest } from '../../../reviewed-preprints.page';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const msid = (Array.isArray(req.query.msid) ? req.query.msid[0] : req.query.msid) ?? '';
+
+  const version = await fetchVersion(msid);
+  const pdfUrl = version?.article.pdfUrl;
+
+  if (pdfUrl) {
+    redirect(pdfUrl);
+  } else {
+    errorNotFoundRequest(res);
+    return
+  }
+};
