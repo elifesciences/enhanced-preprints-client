7986ff54830baf00e617e262788118f44f54bfa0
aishakapan <a.kapan@elifesciences.org>
2025-09-09 15:33:46 +0100

    Migrate to the new flat config as per eslint v9 updates

---
+++ eslint.config.js
@@ -0,0 +1,95 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const noOnlyTests = require("eslint-plugin-no-only-tests");
+const { fixupPluginRules } = require("@eslint/compat");
+const deprecationPlugin = require('eslint-plugin-deprecation');
+const nextPlugin = require("@next/eslint-plugin-next");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        "no-only-tests": noOnlyTests,
+        "deprecation": fixupPluginRules(deprecationPlugin),
+        "next": fixupPluginRules(nextPlugin),
+
+    },
+    extends: compat.extends(
+        "airbnb/base",
+        "airbnb-typescript/base",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:storybook/recommended",
+        "plugin:json/recommended",
+        "plugin:@next/next/recommended",
+        "plugin:@next/next/core-web-vitals",
+    ),
+    rules: {
+        "eol-last": ["error", "always"],
+        "deprecation/deprecation": "warn",
+        "import/prefer-default-export": 0,
+
+        "max-len": ["error", {
+            "code": 240,
+        }],
+
+        "import/extensions": 0,
+
+        "import/no-extraneous-dependencies": ["error", {
+            "devDependencies": [
+                "**/*.stories.*",
+                "**/.storybook/**/*.*",
+                "**/*.test.*",
+                "**/browser-tests/**/*.*",
+            ],
+
+            "peerDependencies": true,
+        }],
+
+        "operator-linebreak": 0,
+
+        "no-only-tests/no-only-tests": ["error", {
+            "focus": ["only"],
+        }],
+
+        "react/jsx-indent": ["error", 2],
+        "react/jsx-indent-props": ["error", 2],
+        "@typescript-eslint/no-non-null-assertion": "warn",
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+}, globalIgnores([
+    "**/.eslintrc.js",
+    "**/next.config.js",
+    "**/stylelint.config.js",
+    "**/next-env.d.ts",
+    "**/playwright.config.ts",
+    "**/jest.config.js",
+])]);
