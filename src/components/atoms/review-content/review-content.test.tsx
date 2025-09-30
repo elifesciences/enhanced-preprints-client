bdb252fe4c423478280617aebf8a60433c5ce619
Daniel Ecer <de-code@users.noreply.github.com>
2025-09-30 17:38:02 +0100

    Describe intended behaviour for proxying png imgur images with a failing test
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1288

--- src/components/atoms/review-content/review-content.test.tsx
+++ src/components/atoms/review-content/review-content.test.tsx
@@ -21,7 +21,13 @@ describe('ReviewContent', () => {
     expect(screen.getByText('I am an em').tagName).toStrictEqual('EM');
   });
 
-  it('adds https://proxy.duckduckgo.com/iu/?u= prefix to https://i.imgur.com/ img uris', () => {
+  it.failing('adds https://proxy.duckduckgo.com/iu/?u= prefix to https://i.imgur.com/ png img uris', () => {
+    const { container } = render(<ReviewContent content={'<p><a href="https://imgur.com/wiUoXWs"><img src="https://i.imgur.com/wiUoXWs.png" title="source: imgur.com" /></a></p>'}/>);
+
+    expect(container.querySelector('img[src="https://proxy.duckduckgo.com/iu/?u=https://i.imgur.com/wiUoXWs.png"]')).toBeInTheDocument();
+  });
+
+  it('adds https://proxy.duckduckgo.com/iu/?u= prefix to https://i.imgur.com/ jpg img uris', () => {
     const { container } = render(<ReviewContent content={'<p><a href="https://imgur.com/wiUoXWs"><img src="https://i.imgur.com/wiUoXWs.jpg" title="source: imgur.com" /></a></p>'}/>);
 
     expect(container.querySelector('img[src="https://proxy.duckduckgo.com/iu/?u=https://i.imgur.com/wiUoXWs.jpg"]')).toBeInTheDocument();
