732050a79890ab33b05f0c10d0c5093fd0a40cd6
David Moulton <d.moulton@elifesciences.org>
2025-11-12 12:02:08 +0000

    Deduplicate test input and parametrise the expectations
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/get-pdf-url.test.ts
+++ src/utils/get-pdf-url.test.ts
@@ -1,23 +1,23 @@
 import { getPdfUrl } from './get-pdf-url';
 
 describe('getPdfUrl', () => {
+  const msid = '12345';
+
   describe('when the article version is not a version of record', () => {
     it('should use the "/reviewed-preprints" prefix', () => {
-      const msid = '12345';
       const isVor = false;
       const url = getPdfUrl(msid, isVor);
 
-      expect(url).toBe('/reviewed-preprints/12345.pdf');
+      expect(url).toBe(`/reviewed-preprints/${msid}.pdf`);
     });
   });
 
   describe('when the article version is a version of record', () => {
     it('should use the "/articles" prefix', () => {
-      const msid = '12345';
       const isVor = true;
       const url = getPdfUrl(msid, isVor);
 
-      expect(url).toBe('/articles/12345.pdf');
+      expect(url).toBe(`/articles/${msid}.pdf`);
     });
   });
 });
