c283a9b42b20091e8308d09a80e2b0e4a89a637b
Daniel Ecer <de-code@users.noreply.github.com>
2025-11-12 10:23:13 +0000

    Define required behaviour with todo tests
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/get-pdf-url.test.ts
+++ src/utils/get-pdf-url.test.ts
@@ -8,4 +8,12 @@ describe('getPdfUrl', () => {
 
     expect(url).toBe('/reviewed-preprints/12345.pdf');
   });
+
+  describe('when the article version is a reviewed preprint', () => {
+    it.todo('should use the "/reviewed-preprints" prefix');
+  });
+
+  describe('when the article version is a verson of record', () => {
+    it.todo('should use the "/articles" prefix');
+  });
 });
