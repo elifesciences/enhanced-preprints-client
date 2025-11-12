1a5e9a510dabb602277748f1cf1264d56f12832f
David Moulton <d.moulton@elifesciences.org>
2025-11-12 13:42:45 +0000

    Describe current and intended behaviour in a set of todo tests
    https://github.com/elifesciences/issues/issues/9432

---
+++ src/utils/get-canonical-url.test.ts
@@ -0,0 +1,9 @@
+describe('getCanonicalUrl', () => {
+  describe('when the article version is not a version of record', () => {
+    it.todo('should use the "/reviewed-preprints" prefix');
+  });
+
+  describe('when the article version is a version of record', () => {
+    it.todo('should use the "/articles" prefix');
+  });
+});
