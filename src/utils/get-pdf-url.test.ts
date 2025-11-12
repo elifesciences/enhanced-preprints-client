02833398568e18ca06f0e6f76d799e65b807f7f8
David Moulton <d.moulton@elifesciences.org>
2025-11-12 11:49:52 +0000

    Remove a duplicate test
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/get-pdf-url.test.ts
+++ src/utils/get-pdf-url.test.ts
@@ -1,14 +1,6 @@
 import { getPdfUrl } from './get-pdf-url';
 
 describe('getPdfUrl', () => {
-  it('should return pdf_url with msid replaced', () => {
-    const msid = '12345';
-    const isVor = false;
-    const url = getPdfUrl(msid, isVor);
-
-    expect(url).toBe('/reviewed-preprints/12345.pdf');
-  });
-
   describe('when the article version is not a version of record', () => {
     it('should use the "/reviewed-preprints" prefix', () => {
       const msid = '12345';
