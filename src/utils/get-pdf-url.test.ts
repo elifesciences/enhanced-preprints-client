2b7fd94773876e8e541b585ba9b199328b2ca1f1
Scott Aubrey <scott@aubrey.org.uk>
2025-11-13 11:04:58 +0000

    test both types of URL prefixes
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/get-pdf-url.test.ts
+++ src/utils/get-pdf-url.test.ts
@@ -2,44 +2,47 @@ import { getPdfUrl } from './get-pdf-url';
 
 describe('getPdfUrl', () => {
   const msid = '12345';
+  const domain = 'https://elifesciences.org';
 
   describe('when the article version is not a version of record', () => {
     const isVor = false;
+    const url = getPdfUrl(msid, isVor);
     it('uses the "/reviewed-preprints" prefix', () => {
-      const url = getPdfUrl(msid, isVor);
-
       expect(url.startsWith('/reviewed-preprints')).toBe(true);
     });
 
     it('uses the manuscript ID in the basename of the PDF URL', () => {
-      const notVorUrl = getPdfUrl(msid, isVor);
+      expect(url.endsWith(`${msid}.pdf`)).toBe(true);
+    });
 
-      expect(notVorUrl.endsWith(`${msid}.pdf`)).toBe(true);
+    describe('when the canonical domain is configured', () => {
+      const expectedUrl = `https://elifesciences.org/reviewed-preprints/${msid}.pdf`;
+      it('should return a full url with protocol and domain', () => {
+        const canonicalUrl = getPdfUrl(msid, isVor, domain);
+
+        expect(canonicalUrl).toBe(expectedUrl);
+      });
     });
   });
 
   describe('when the article version is a version of record', () => {
     const isVor = true;
-    const domain = 'https://elifesciences.org';
+    const url = getPdfUrl(msid, isVor);
 
     it('uses the "/articles" prefix', () => {
-      const url = getPdfUrl(msid, isVor);
-
       expect(url.startsWith('/articles')).toBe(true);
     });
 
     it('uses the manuscript ID in the basename of the PDF URL', () => {
-      const vorUrl = getPdfUrl(msid, isVor);
-
-      expect(vorUrl.endsWith(`${msid}.pdf`)).toBe(true);
+      expect(url.endsWith(`${msid}.pdf`)).toBe(true);
     });
 
     describe('when the canonical domain is configured', () => {
       const expectedUrl = `https://elifesciences.org/articles/${msid}.pdf`;
       it('should return a full url with protocol and domain', () => {
-        const url = getPdfUrl(msid, isVor, domain);
+        const canonicalUrl = getPdfUrl(msid, isVor, domain);
 
-        expect(url).toBe(expectedUrl);
+        expect(canonicalUrl).toBe(expectedUrl);
       });
     });
   });
