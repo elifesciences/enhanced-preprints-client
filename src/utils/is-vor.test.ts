45a793a3c33bba544f513e4779dae16923f43c4b
Scott Aubrey <scott@aubrey.org.uk>
2025-11-14 12:12:54 +0000

    Write passing test
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/is-vor.test.ts
+++ src/utils/is-vor.test.ts
@@ -9,6 +9,25 @@ describe('isVor', () => {
     });
   });
   describe('Given a version that is a Version of Record', () => {
-    it.todo('should return true');
+    it('should return true', () => {
+      const articleVersionVor = {
+        article: mock85111.article,
+        versions: {
+          '85111v1': {
+            doi: '10.7554/eLife.85111.1',
+            id: '85111v1',
+            msid: '85111',
+            preprintDoi: '10.1101/2022.11.08.515698',
+            preprintUrl: 'https://www.biorxiv.org/content/10.1101/2022.11.08.515698v2',
+            published: new Date('2023-01-25T14:00:00.000Z'),
+            sentForReview: new Date('2022-11-29T14:20:30.000Z'),
+            versionDoi: '10.7554/eLife.85111.1',
+            umbrellaDoi: '10.7554/eLife.85111',
+            versionIdentifier: '1',
+          },
+        },
+      };
+      expect(isVor(articleVersionVor)).toBe(true);
+    });
   });
 });
