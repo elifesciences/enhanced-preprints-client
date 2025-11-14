7891fd0de829027f9713888ab04acb7afc9bd7f3
Scott Aubrey <scott@aubrey.org.uk>
2025-11-14 11:44:02 +0000

    Write a todo list for characterisation tests
    https://github.com/elifesciences/issues/issues/9432

---
+++ src/utils/is-vor.test.ts
@@ -0,0 +1,8 @@
+describe('isVor', () => {
+  describe('Given a version that is not a Version of Record', () => {
+    it.todo('should return false');
+  });
+  describe('Given a version that is a Version of Record', () => {
+    it.todo('should return true');
+  });
+});
