6bc0e872d9e0331f2c72fd3d8213e5a99eefa846
nlisgo <n.lisgo@elifesciences.org>
2025-11-14 11:58:40 +0000

    Use existing fixture in test
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/is-vor.test.ts
+++ src/utils/is-vor.test.ts
@@ -1,24 +1,10 @@
-import { type EnhancedArticleWithVersions } from '../types';
 import { isVor } from './is-vor';
+import { mock85111 } from './mocks/enhanced-article-with-versions';
 
 describe('isVor', () => {
   describe('Given a version that is not a Version of Record', () => {
-    const notVor = {
-      article: {
-        msid: '12345',
-        versionIdentifier: '1',
-      },
-      versions: {
-        // eslint-disable-next-line quote-props
-        '1': {
-          versionIdentifier: '1',
-          preprintPosted: '2022-01-01',
-        },
-      },
-    } as unknown as EnhancedArticleWithVersions;
-
     it('should return false', () => {
-      expect(isVor(notVor)).toBe(false);
+      expect(isVor(mock85111)).toBe(false);
     });
   });
   describe('Given a version that is a Version of Record', () => {
