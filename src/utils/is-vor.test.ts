041090bbe67e8c372815c46379c678b33eaaeef4
nlisgo <n.lisgo@elifesciences.org>
2025-11-14 12:17:51 +0000

    Create a new fixture from test case example
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/is-vor.test.ts
+++ src/utils/is-vor.test.ts
@@ -1,33 +1,15 @@
 import { isVor } from './is-vor';
-import { mock85111 } from './mocks/enhanced-article-with-versions';
+import { mock85111 as articleVersionNotVor, vorArticleVersion } from './mocks/enhanced-article-with-versions';
 
 describe('isVor', () => {
   describe('Given a version that is not a Version of Record', () => {
     it('should return false', () => {
-      const articleVersionNotVor = mock85111;
       expect(isVor(articleVersionNotVor)).toBe(false);
     });
   });
   describe('Given a version that is a Version of Record', () => {
     it('should return true', () => {
-      const articleVersionVor = {
-        article: mock85111.article,
-        versions: {
-          '85111v1': {
-            doi: '10.7554/eLife.85111.1',
-            id: '85111v1',
-            msid: '85111',
-            preprintDoi: '10.1101/2022.11.08.515698',
-            preprintUrl: 'https://www.biorxiv.org/content/10.1101/2022.11.08.515698v2',
-            published: new Date('2023-01-25T14:00:00.000Z'),
-            sentForReview: new Date('2022-11-29T14:20:30.000Z'),
-            versionDoi: '10.7554/eLife.85111.1',
-            umbrellaDoi: '10.7554/eLife.85111',
-            versionIdentifier: '1',
-          },
-        },
-      };
-      expect(isVor(articleVersionVor)).toBe(true);
+      expect(isVor(vorArticleVersion)).toBe(true);
     });
   });
 });
--- src/utils/mocks/enhanced-article-with-versions.ts
+++ src/utils/mocks/enhanced-article-with-versions.ts
@@ -81,3 +81,21 @@ export const mock85111: EnhancedArticleWithVersions = {
     citations: 6,
   },
 };
+
+export const vorArticleVersion = {
+  article: mock85111.article,
+  versions: {
+    '85111v1': {
+      doi: '10.7554/eLife.85111.1',
+      id: '85111v1',
+      msid: '85111',
+      preprintDoi: '10.1101/2022.11.08.515698',
+      preprintUrl: 'https://www.biorxiv.org/content/10.1101/2022.11.08.515698v2',
+      published: new Date('2023-01-25T14:00:00.000Z'),
+      sentForReview: new Date('2022-11-29T14:20:30.000Z'),
+      versionDoi: '10.7554/eLife.85111.1',
+      umbrellaDoi: '10.7554/eLife.85111',
+      versionIdentifier: '1',
+    },
+  },
+};
