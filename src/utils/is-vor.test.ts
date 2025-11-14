454e86cc716262c26475568ea6b07896508deed4
LinaKind <l.kinduryte@elifesciences.org>
2025-11-14 13:53:19 +0200

    Write a passing test
    https://github.com/elifesciences/issues/issues/9432

--- src/utils/is-vor.test.ts
+++ src/utils/is-vor.test.ts
@@ -1,6 +1,25 @@
+import { type EnhancedArticleWithVersions } from '../types';
+import { isVor } from './is-vor';
+
 describe('isVor', () => {
   describe('Given a version that is not a Version of Record', () => {
-    it.todo('should return false');
+    const notVor = {
+      article: {
+        msid: '12345',
+        versionIdentifier: '1',
+      },
+      versions: {
+        // eslint-disable-next-line quote-props
+        '1': {
+          versionIdentifier: '1',
+          preprintPosted: '2022-01-01',
+        },
+      },
+    } as unknown as EnhancedArticleWithVersions;
+
+    it('should return false', () => {
+      expect(isVor(notVor)).toBe(false);
+    });
   });
   describe('Given a version that is a Version of Record', () => {
     it.todo('should return true');
