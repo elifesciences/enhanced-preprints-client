bc2dee786631ab97d021df78dfbc0b6f8ddb0b30
Scott Aubrey <scott@aubrey.org.uk>
2025-11-18 11:41:52 +0000

    Add one characterisation test for the extracted proxy
    https://github.com/elifesciences/issues/issues/9442

--- src/utils/proxy-url-to-response.test.ts
+++ src/utils/proxy-url-to-response.test.ts
@@ -1,3 +1,9 @@
+import EventEmitter from 'events';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createRequest, createResponse } from 'node-mocks-http';
+import { ReadableStream } from 'stream/web';
+import { proxyUrlToResponse } from './proxy-url-to-response';
+
 describe('proxyUrlToResponse', () => {
   describe('when the url returns status 200', () => {
     it.todo('copies appropriate request headers related to client caching to the upstream request');
@@ -12,6 +18,33 @@ describe('proxyUrlToResponse', () => {
   });
 
   describe('when given the filename to apply to the downloaded file', () => {
-    it.todo('sets the content-disposition header on the response');
+    let req: NextApiRequest;
+    let res: NextApiResponse & ReturnType<typeof createResponse>;
+
+    beforeAll(() => {
+      global.fetch = jest.fn();
+    });
+
+    beforeEach(() => {
+      req = createRequest();
+      res = createResponse({
+        eventEmitter: EventEmitter,
+      });
+    });
+
+    it('sets the content-disposition header on the response', async () => {
+      (fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+        body: ReadableStream.from(['']),
+        headers: new Headers(),
+      });
+
+      const filename = 'filename.txt';
+      const contentDispositionHeader = `attachment; filename="${filename}"`;
+
+      await proxyUrlToResponse('arbitrary url', res, req, filename, 'arbitrary string');
+
+      expect(res.getHeader('content-disposition')).toBe(contentDispositionHeader);
+    });
   });
 });
