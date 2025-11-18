b1c88385dbfa13f178d85fa9e28a933c748b5468
David Moulton <d.moulton@elifesciences.org>
2025-11-18 11:53:36 +0000

    Add a second characterisation test for the extracted proxy
    https://github.com/elifesciences/issues/issues/9442

--- src/utils/proxy-url-to-response.test.ts
+++ src/utils/proxy-url-to-response.test.ts
@@ -5,6 +5,26 @@ import { ReadableStream } from 'stream/web';
 import { proxyUrlToResponse } from './proxy-url-to-response';
 
 describe('proxyUrlToResponse', () => {
+  let req: NextApiRequest;
+  let res: NextApiResponse & ReturnType<typeof createResponse>;
+
+  beforeAll(() => {
+    global.fetch = jest.fn();
+  });
+
+  beforeEach(() => {
+    req = createRequest();
+    res = createResponse({
+      eventEmitter: EventEmitter,
+    });
+
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      body: ReadableStream.from(['']),
+      headers: new Headers(),
+    });
+  });
+
   describe('when the url returns status 200', () => {
     it.todo('copies appropriate request headers related to client caching to the upstream request');
     it.todo('does not copy request headers unrelated to client caching to the upstream request');
@@ -14,31 +34,16 @@ describe('proxyUrlToResponse', () => {
   });
 
   describe('when given a canonical URL', () => {
-    it.todo('sets the link header with canonical-url on the response');
+    it('sets the link header with canonical-url on the response', async () => {
+      const canonicalUrl = 'canonical url';
+      await proxyUrlToResponse('arbitrary url', res, req, 'arbitrary filename', canonicalUrl);
+
+      expect(res.getHeader('link')).toBe(`<${canonicalUrl}>; rel="canonical"`);
+    });
   });
 
   describe('when given the filename to apply to the downloaded file', () => {
-    let req: NextApiRequest;
-    let res: NextApiResponse & ReturnType<typeof createResponse>;
-
-    beforeAll(() => {
-      global.fetch = jest.fn();
-    });
-
-    beforeEach(() => {
-      req = createRequest();
-      res = createResponse({
-        eventEmitter: EventEmitter,
-      });
-    });
-
     it('sets the content-disposition header on the response', async () => {
-      (fetch as jest.Mock).mockResolvedValueOnce({
-        ok: true,
-        body: ReadableStream.from(['']),
-        headers: new Headers(),
-      });
-
       const filename = 'filename.txt';
       const contentDispositionHeader = `attachment; filename="${filename}"`;
 
