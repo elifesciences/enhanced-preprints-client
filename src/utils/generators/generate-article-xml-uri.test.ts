bda07c8c94a1e4a276bd8e8f6ced0f5396cf5e33
nlisgo <n.lisgo@elifesciences.org>
2025-11-24 10:41:38 +0000

    Write a characterisation test for the XML location on the files API
    https://github.com/elifesciences/issues/issues/9442

--- src/utils/generators/generate-article-xml-uri.test.ts
+++ src/utils/generators/generate-article-xml-uri.test.ts
@@ -10,4 +10,7 @@ describe('generateArticleXmlUri', () => {
   it('includes article version', () => {
     expect(articleXmlUri).toContain(versionIdentifier);
   });
+  it('the basename is article-transformed.xml', () => {
+    expect(articleXmlUri.endsWith('article-transformed.xml')).toBe(true);
+  });
 });
