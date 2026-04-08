1b752585a2230872302ce0e5f9a3417f381ebfda
LinaKind <l.kinduryte@elifesciences.org>
2026-04-08 19:00:02 +0300

    Improve types and implementation of the oxa heading node parsing

--- src/utils/oxa/oxa-to-jsx.tsx
+++ src/utils/oxa/oxa-to-jsx.tsx
@@ -1,4 +1,5 @@
 import { type JSX } from 'react';
+import { Heading } from '../../components/atoms/heading/heading';
 
 type JSXContentPart = string | JSX.Element | Array<JSXContentPart>;
 export type JSXContent = JSXContentPart | Array<JSXContentPart>;
@@ -17,10 +18,11 @@ type OxaNode = {
   classes?: Array<string> // Optional: Styling or semantic classes
   // biome-ignore lint/suspicious/noExplicitAny: We don't know all K/V
   data?: Record<string, any> // Optional: Arbitrary metadata
-  children?: Array<OxaNode | TextOxaNode> // Optional: Nested content nodes
+  children?: Array<OxaNode> // Optional: Nested content nodes
 };
 
-type InlineOxaNode = OxaNode & {
+type InlineOxaNode = Omit<OxaNode, 'type'> & {
+  type: 'Text'
   value: string
 }
 
@@ -28,17 +30,22 @@ type TextOxaNode = InlineOxaNode & {
   type: 'Text'
 }
 
-type HeadingOxaNode = OxaNode & {
+type HeadingOxaNode = Omit<OxaNode, 'children'> & {
   type: 'Heading'
   level: number
+  children: Array<InlineOxaNode>
 };
 
-export const oxaToJsx = (content?: TextOxaNode | HeadingOxaNode, options?: Options, index?: number): JSXContent => {
+export const oxaToJsx = (content?: TextOxaNode | HeadingOxaNode | InlineOxaNode, options?: Options, index?: number): JSXContent => {
   if (typeof content === 'undefined') {
     return '';
   }
   if (content.type === 'Text') {
     return content.value;
+  }
+  if (content.type === 'Heading') {
+    const level = content.level > 6 ? 6 : content.level < 1 ? 1 : content.level as 1 | 2 | 3 | 4 | 5 | 6;
+    return <Heading id={content.id ?? ''} headingLevel={level} content={content.children.map((child) => oxaToJsx(child))}></Heading>
   }
   return '';
 };
