fe9e07b94d9a8f5eb1444cf07f49f5c084c98abd
nlisgo <n.lisgo@elifesciences.org>
2026-05-19 16:25:25 +0100

    Implement hardcoded paths to pdf for elife staging

--- src/pages/api/downloads/[msid]/pdf.page.ts
+++ src/pages/api/downloads/[msid]/pdf.page.ts
@@ -21,12 +21,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       return;
     }
 
-    const pdfUrl = articleWithVersions.article?.pdfUrl;
+    const downloadFilename = `${articleWithVersions.article.msid}-v${articleWithVersions.article.versionIdentifier}.pdf`;
+    const pdfUrl = config.tenantDomain === 'https://staging--epp.elifesciences.org' && config.filesApiPath
+      ? `${config.filesApiPath}/${articleWithVersions.article.msid}/v${articleWithVersions.article.versionIdentifier}/content/elife-preprint-${downloadFilename}`
+      : articleWithVersions.article?.pdfUrl;
     if (!pdfUrl) {
       res.status(404).end();
-      return;
+      return; 
     }
-    const downloadFilename = `${articleWithVersions.article.msid}-v${articleWithVersions.article.versionIdentifier}.pdf`;
     const canonicalUrl = getCanonicalUrl(articleWithVersions.article.msid, isVor(articleWithVersions), config.tenantDomain);
 
     await proxyUrlToResponse(pdfUrl, req, res, downloadFilename, canonicalUrl);
