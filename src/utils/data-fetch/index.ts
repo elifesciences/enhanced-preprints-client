ca5d62d12b16a22af34f0f8db7a21c5647c7b030
nlisgo <n.lisgo@elifesciences.org>
2026-07-01 11:41:47 +0100

    Increase encapsulation by removing unnecessary exports
    https://github.com/eLifePathways/issues/issues/3

--- src/utils/data-fetch/index.ts
+++ src/utils/data-fetch/index.ts
@@ -1,3 +1,2 @@
 export { fetchVersion, fetchVersions, fetchVersionsNoContent } from './fetch-data';
 export { getLatestVersionWarningUrl } from './get-latest-version-warning-url';
-export { jsonFetch, jsonFetchOrNull } from './json-fetch';
