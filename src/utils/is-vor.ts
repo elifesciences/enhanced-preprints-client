d0fca964c8b321c09e902b76b149334e9a47d87f
LinaKind <l.kinduryte@elifesciences.org>
2026-07-09 18:16:17 +0300

    Begin to provide explicit return types for exported modules
    https://github.com/eLifePathways/issues/issues/3

--- src/utils/is-vor.ts
+++ src/utils/is-vor.ts
@@ -1,7 +1,7 @@
 import { isVORVersionSummary } from './type-guards';
 import { type EnhancedArticleWithVersions } from '../types';
 
-export const isVor = (articleWithVersions: EnhancedArticleWithVersions) => {
+export const isVor = (articleWithVersions: EnhancedArticleWithVersions): boolean => {
   const versions = Object.values(articleWithVersions.versions);
   return Object.values(versions).some((version) => version.versionIdentifier === articleWithVersions.article.versionIdentifier && isVORVersionSummary(version));
 };
--- src/utils/isPdfRoute.ts
+++ src/utils/isPdfRoute.ts
@@ -1,6 +1,6 @@
 import { usePathname } from 'next/navigation';
 
-const isPdfRoute = () => {
+const isPdfRoute = (): boolean => {
   const pathName = usePathname();
   return pathName.endsWith('/pdf');
 };
