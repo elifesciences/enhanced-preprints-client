d32da1cc7dca496cdcaf9201a58a0df65b9d0dfe
LinaKind <l.kinduryte@elifesciences.org>
2026-07-22 16:28:31 +0300

    Ensure styling is pulled through by providing a class
    https://github.com/eLifePathways/issues/issues/3

--- src/components/pages/article/tabs/figures-tab.tsx
+++ src/components/pages/article/tabs/figures-tab.tsx
@@ -7,7 +7,7 @@ export const ArticleFiguresTab = ({ content }: { content: JSXContent }): JSX.Ele
   <div className="tabbed-navigation__content tabbed-navigation__content--figures">
     <div className="menu-spacer"/>
     <div className="article-body-container">
-      <h2 id="figures">Figures and data</h2>
+      <h2 id="figures" className="heading-2">Figures and data</h2>
       <ArticleContent content={content} />
     </div>
   </div>
