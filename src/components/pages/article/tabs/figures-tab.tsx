73f0e569b21994a332154ef5dec57fdc1574c937
nlisgo <n.lisgo@elifesciences.org>
2026-07-22 14:24:09 +0100

    Inline heading
    https://github.com/eLifePathways/issues/issues/3

--- src/components/pages/article/tabs/figures-tab.tsx
+++ src/components/pages/article/tabs/figures-tab.tsx
@@ -2,13 +2,12 @@ import { type JSX } from 'react';
 import '../article-page.scss';
 import { type JSXContent } from '../../../../utils/content';
 import { ArticleContent } from '../../../atoms/article-content/article-content';
-import { Heading } from '../../../atoms/heading/heading';
 
 export const ArticleFiguresTab = ({ content }: { content: JSXContent }): JSX.Element => (
   <div className="tabbed-navigation__content tabbed-navigation__content--figures">
     <div className="menu-spacer"/>
     <div className="article-body-container">
-      <Heading id="figures" headingLevel={2} content="Figures and data" />
+      <h2 id="figures">Figures and data</h2>
       <ArticleContent content={content} />
     </div>
   </div>
