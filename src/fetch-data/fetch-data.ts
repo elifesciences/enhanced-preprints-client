43a182bc07a0f2e5245a2d7b8627722c6216aa76
David Moulton <d.moulton@elifesciences.org>
2026-07-30 13:41:13 +0100

    Begin to build schema for the ProcessedArticle input shape
    https://github.com/eLifePathways/issues/issues/3

--- src/fetch-data/fetch-data.ts
+++ src/fetch-data/fetch-data.ts
@@ -14,7 +14,27 @@ import {
 
 const ToDoSchema = z.any();
 
-const ProcessedArticleSchema = ToDoSchema;
+const ContentSchema = ToDoSchema;
+const AuthorSchema = ToDoSchema;
+const LicenseSchema = ToDoSchema;
+const ReferenceSchema = ToDoSchema;
+
+const ProcessedArticleSchema = z.object({
+  title: ContentSchema,
+  authors: z.array(AuthorSchema).optional(),
+  abstract: ContentSchema,
+  licenses: z.array(LicenseSchema),
+  content: ContentSchema,
+  references: z.array(ReferenceSchema),
+  meta: z.object({
+    authorNotes: z.array(z.object({
+      type: z.string(),
+      id: z.string().optional(),
+      text: z.string(),
+      label: z.string().optional(),
+    })).optional(),
+  }).optional(),
+});
 
 const EnhancedArticleSchema = z.object({
   id: z.string(),
