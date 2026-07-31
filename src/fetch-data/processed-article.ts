b35a9e48ab09ac71b9ddf4c1447dfbb243fd6ef8
LinaKind <l.kinduryte@elifesciences.org>
2026-07-31 12:50:25 +0300

    Improve encapsulation of the schema for a Reference type
    https://github.com/eLifePathways/issues/issues/3

--- src/fetch-data/processed-article.ts
+++ src/fetch-data/processed-article.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
-import {AuthorSchema} from './author';
+import { AuthorSchema } from './author';
+import { ReferenceSchema } from './reference';
 
 const ToDoSchema = z.any();
 
@@ -9,61 +10,6 @@ const LicenseSchema = z.object({
   type: z.string(),
   url: z.string().optional(),
   content: ContentSchema.optional(),
-});
-
-interface PublicationShape {
-  type: 'PublicationVolume' | 'Periodical' | 'CreativeWork' | 'PublicationIssue';
-  name?: string;
-  volumeNumber?: number | string;
-  issueNumber?: number;
-  isPartOf?: PublicationShape;
-}
-const PublicationSchema: z.ZodType<PublicationShape> = z.object({
-  type: z.union([z.literal('PublicationVolume'), z.literal('Periodical'), z.literal('CreativeWork'), z.literal('PublicationIssue')]),
-  name: z.string().optional(),
-  volumeNumber: z.union([z.number(), z.string()]).optional(),
-  issueNumber: z.number().optional(),
-  isPartOf: z.lazy(() => PublicationSchema).optional(),
-});
-
-const PublisherSchema = z.object({
-  type: z.literal('Organization'),
-  name: z.string(),
-  address: z.object({
-    type: z.literal('PostalAddress'),
-    addressLocality: z.string(),
-  }).optional(),
-});
-
-const ReferenceSchema = z.object({
-  type: z.literal('Article'),
-  id: z.string(),
-  title: z.string(),
-  url: z.string().optional(),
-  pageEnd: z.union([z.number(), z.string()]).optional(),
-  pageStart: z.union([z.number(), z.string()]).optional(),
-  authors: z.array(AuthorSchema),
-  datePublished: z.union([z.string(), z.object({
-    type: z.literal('Date'),
-    value: z.string(),
-  })]).optional(),
-  isPartOf: PublicationSchema.optional(),
-  publisher: PublisherSchema.optional(),
-  identifiers: z.array(z.object({
-    type: z.string(),
-    name: z.string(),
-    propertyID: z.string().optional(),
-    value: z.string(),
-  })).optional(),
-  comments: z.array(z.object({
-    type: z.literal('Comment'),
-    commentAspect: z.string(),
-  })).optional(),
-  meta: z.object({
-    yearPublished: z.string().optional(),
-    label: z.string().optional(),
-    publicationType: z.string().optional(),
-  }).optional(),
 });
 
 export const ProcessedArticleSchema = z.object({
---
+++ src/fetch-data/reference.ts
@@ -0,0 +1,57 @@
+import { z } from 'zod';
+import { AuthorSchema } from './author';
+
+interface PublicationShape {
+  type: 'PublicationVolume' | 'Periodical' | 'CreativeWork' | 'PublicationIssue';
+  name?: string;
+  volumeNumber?: number | string;
+  issueNumber?: number;
+  isPartOf?: PublicationShape;
+}
+const PublicationSchema: z.ZodType<PublicationShape> = z.object({
+  type: z.union([z.literal('PublicationVolume'), z.literal('Periodical'), z.literal('CreativeWork'), z.literal('PublicationIssue')]),
+  name: z.string().optional(),
+  volumeNumber: z.union([z.number(), z.string()]).optional(),
+  issueNumber: z.number().optional(),
+  isPartOf: z.lazy(() => PublicationSchema).optional(),
+});
+
+const PublisherSchema = z.object({
+  type: z.literal('Organization'),
+  name: z.string(),
+  address: z.object({
+    type: z.literal('PostalAddress'),
+    addressLocality: z.string(),
+  }).optional(),
+});
+
+export const ReferenceSchema = z.object({
+  type: z.literal('Article'),
+  id: z.string(),
+  title: z.string(),
+  url: z.string().optional(),
+  pageEnd: z.union([z.number(), z.string()]).optional(),
+  pageStart: z.union([z.number(), z.string()]).optional(),
+  authors: z.array(AuthorSchema),
+  datePublished: z.union([z.string(), z.object({
+    type: z.literal('Date'),
+    value: z.string(),
+  })]).optional(),
+  isPartOf: PublicationSchema.optional(),
+  publisher: PublisherSchema.optional(),
+  identifiers: z.array(z.object({
+    type: z.string(),
+    name: z.string(),
+    propertyID: z.string().optional(),
+    value: z.string(),
+  })).optional(),
+  comments: z.array(z.object({
+    type: z.literal('Comment'),
+    commentAspect: z.string(),
+  })).optional(),
+  meta: z.object({
+    yearPublished: z.string().optional(),
+    label: z.string().optional(),
+    publicationType: z.string().optional(),
+  }).optional(),
+});
