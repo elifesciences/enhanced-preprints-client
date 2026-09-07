1315155341c2681da2013a07c57ac7121f1cf3ed
LinaKind <l.kinduryte@elifesciences.org>
2026-09-07 18:20:46 +0300

    Allow pages to provide their own custom layout
    https://github.com/eLifePathways/issues/issues/9

--- src/pages/test-custom-layout/test-custom-layout.page.tsx
+++ src/pages/test-custom-layout/test-custom-layout.page.tsx
@@ -1,5 +1,13 @@
+import { type ReactNode } from 'react';
+import { BlankLayout } from '../../components/layouts/blank-layout';
 
 const Page = () => <h1>Test custom layout</h1>;
+
+Page.getLayout = function getLayout(page: ReactNode) {
+  return (
+    <BlankLayout>{page}</BlankLayout>
+  );
+};
 
 // ts-unused-exports:disable-next-line
 export default Page;
