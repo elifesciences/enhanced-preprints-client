e3406d758ce953b78e14a3fe92b675aef6f1c57b
LinaKind <l.kinduryte@elifesciences.org>
2026-09-09 12:37:38 +0300

    Begin defining classes for styles
    https://github.com/eLifePathways/issues/issues/10

--- src/pages/api/static/105391-claims-tree.page.ts
+++ src/pages/api/static/105391-claims-tree.page.ts
@@ -19,17 +19,17 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
       <ul role="list">
 
         <li role="listitem">
-          <a href="#" draggable="false">
-            <header>Question 1 (<span class="visuallyhidden">comprises </span>3 claims)</header>
+          <a href="#" draggable="false" class="card">
+            <header class="card__header">Question 1 (<span class="visuallyhidden">comprises </span>3 claims)</header>
             <p><span class="visuallyhidden">The question: </span>What are the neural mechanisms of interpersonal guilt and responsibility during social decisions under risk?</p>
           </a>
           <section>
           
             <header class="visuallyhidden">There are 3 claims for this question:</header>
             
-            <a href="#" draggable="false">
+            <a href="#" draggable="false" class="card">
               <article>
-                <header>Claim 1 (question 1)</header>
+                <header class="card__header">Claim 1 (question 1)</header>
                 <p>The guilt effect is associated with increased BOLD signal in the left anterior insula.</p>            
                 <div><span class="visuallyhidden">The following is related to this claim: </span><span>Figure</span><span class="visuallyhidden">, </span><span>Study 2</span></div>
 
