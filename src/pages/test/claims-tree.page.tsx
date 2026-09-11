54c838e50ceb2e0104d6a52731f4b85ab7a8bc8a
LinaKind <l.kinduryte@elifesciences.org>
2026-09-11 16:45:22 +0300

    Introduce Head nextjs element with title property
    https://github.com/eLifePathways/issues/issues/11

--- src/pages/test/claims-tree.page.tsx
+++ src/pages/test/claims-tree.page.tsx
@@ -1,8 +1,16 @@
+import Head from 'next/head';
 import { type ReactNode } from 'react';
 import { BlankLayout } from '../../components/layouts/blank';
 import { ClaimsTreePage } from '../../components/pages/mira/claims-tree/claims-tree-page';
 
-const Page = () => <><ClaimsTreePage></ClaimsTreePage></>;
+const Page = () => (
+  <>
+    <Head>
+      <title>Claims Tree</title>
+    </Head>
+    <ClaimsTreePage></ClaimsTreePage>
+  </>
+);
 
 Page.getLayout = function getLayout(page: ReactNode) {
   return (
