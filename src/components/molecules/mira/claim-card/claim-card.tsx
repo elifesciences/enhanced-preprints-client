c9df32f62b9b69f53054532edec9985890d517b4
David Moulton <d.moulton@elifesciences.org>
2026-09-16 14:46:00 +0100

    Add missing file
    https://github.com/eLifePathways/issues/issues/12

---
+++ src/components/molecules/mira/claim-card/claim-card.tsx
@@ -0,0 +1,29 @@
+import type {JSX} from 'react';
+
+type ClaimCardProps = {
+  questionNumber: string,
+  header: string,
+  title: string,
+  related: Array<string>,
+};
+
+export const ClaimCard = (props: ClaimCardProps): JSX.Element => (
+  <>
+    <a href="#" draggable="false" className="card">
+      <article>
+        <header>Claim {props.header} <span className="supplementary">(question&nbsp;{props.questionNumber})</span></header>
+        <p>{props.title}</p>
+        <div className="related">
+          <span className="visuallyhidden">The following is related to this claim: </span>
+          <span className="related-item">
+            <span>{props.related[0]}</span>
+          </span>
+          <span className="visuallyhidden">, </span>
+          <span className="related-item">
+            <span>{props.related[1]}</span>
+          </span>
+        </div>
+      </article>
+    </a>
+  </>
+);
