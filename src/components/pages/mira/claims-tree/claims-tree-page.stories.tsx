4225f17c7319803422bd9f026c7533c0f4a5fa48
David Moulton <d.moulton@elifesciences.org>
2026-09-16 10:49:41 +0100

    Introduce visual regression coverage for the claims tree page
    https://github.com/eLifePathways/issues/issues/12

---
+++ src/components/pages/mira/claims-tree/claims-tree-page.stories.tsx
@@ -0,0 +1,35 @@
+import { type Meta, type StoryObj } from '@storybook/nextjs';
+import { ClaimsTreePage } from './claims-tree-page';
+import { BlankLayout } from '../../../layouts/blank';
+
+const meta: Meta<typeof ClaimsTreePage> = {
+  title: 'Pages/Claims Tree',
+  parameters: {
+    chromatic: {
+      modes: {
+        small: {
+          viewport: 'small',
+        },
+        medium: {
+          viewport: 'medium',
+        },
+        large: {
+          viewport: 'large',
+        },
+        extraLarge: {
+          viewport: 'extraLarge',
+        },
+      },
+    },
+  },
+  render: () => (
+    <BlankLayout>
+      <ClaimsTreePage />
+    </BlankLayout>
+  ),
+};
+
+export default meta;
+type Story = StoryObj<typeof ClaimsTreePage>;
+
+export const ClaimsTree: Story = {};
