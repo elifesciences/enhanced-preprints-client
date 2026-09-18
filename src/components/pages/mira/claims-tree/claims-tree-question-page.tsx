d6e07e929f881f7f957f323112746057d0faaeb3
LinaKind <l.kinduryte@elifesciences.org>
2026-09-18 12:56:38 +0300

    Begin to build a question page component
    https://github.com/eLifePathways/issues/issues/13

--- src/components/pages/mira/claims-tree/claims-tree-question-page.tsx
+++ src/components/pages/mira/claims-tree/claims-tree-question-page.tsx
@@ -1,7 +1,7 @@
 import { DM_Sans, Poppins } from 'next/font/google';
 import { type JSX } from 'react';
 import './common.scss';
-import './claims-tree-page.scss';
+import './claims-tree-question-page.scss';
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
@@ -21,6 +21,13 @@ export const ClaimsTreeQuestionPage = (): JSX.Element => (
   <>
     <main className={`page-wrapper ${dmSans.variable} ${poppins.variable}`}>
       <h1>Question 1</h1>
+      <div className="card">
+        <header>Question 1
+          <span aria-hidden="true" className="dot">&nbsp;&#x2022;&nbsp;</span>
+          <span className="visuallyhidden">comprises </span>3 claims<span className="visuallyhidden">.</span>
+        </header>
+        <p><span className="visuallyhidden">The question: </span>What are the neural mechanisms of interpersonal guilt and responsibility during social decisions under risk?</p>
+      </div>
     </main>
 
   </>
